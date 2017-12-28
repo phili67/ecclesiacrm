00b3d76a270661a5927744f81607a3dc0023358c
Philippe Logel <philippelogel@free.fr>
2017-12-28 23:28:06 +0100

    some uprgade

--- src/mysql/upgrade/2.10.4-delete-ChurchCRM-folder.php
+++ src/mysql/upgrade/2.10.4-delete-ChurchCRM-folder.php
@@ -1,5 +1,15 @@
 <?php 
-function delTree($dir) { 
+
+  use Propel\Runtime\Propel;
+  use EcclesiaCRM\Utils\LoggerUtils;
+  use EcclesiaCRM\dto\SystemURLs;
+
+  $connection = Propel::getConnection();
+  $logger = LoggerUtils::getAppLogger();
+
+  $logger->info("Start to delete : ChurchCRM folder");
+
+  function delTree($dir) { 
    $files = array_diff(scandir($dir), array('.','..')); 
     foreach ($files as $file) { 
       (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
@@ -7,5 +17,7 @@ function delTree($dir) {
     return rmdir($dir); 
   } 
 
-  delTree("ChurchCRM");
+  delTree(SystemURLs::getDocumentRoot()."/ChurchCRM");
+  
+  $logger->info("Delete the old ChurchCRM folder");
 ?>
