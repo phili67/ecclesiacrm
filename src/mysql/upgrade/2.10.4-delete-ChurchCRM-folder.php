0cefc36fea13da6c4e4edba292edbecb61bf975d
Philippe Logel <philippelogel@free.fr>
2017-12-28 19:01:06 +0100

    Start 2.10.4 upgrade

---
+++ src/mysql/upgrade/2.10.4-delete-ChurchCRM-folder.php
@@ -0,0 +1,12 @@
+<?php 
+function delTree($dir) { 
+   $files = array_diff(scandir($dir), array('.','..')); 
+    foreach ($files as $file) { 
+      (is_dir("$dir/$file")) ? delTree("$dir/$file") : unlink("$dir/$file"); 
+    } 
+    return rmdir($dir); 
+  } 
+
+delTree("ChurchCRM");
+
+?>
