3cab746cdcad018e6df6b77cf14bac45e5d5cbb5
Philippe Logel <philippelogel@free.fr>
2017-12-29 18:25:31 +0100

    The last modifcation

--- src/Include/Functions.php
+++ src/Include/Functions.php
@@ -1408,10 +1408,6 @@ function formatNumber($iNumber, $sMode = 'integer')
   }
 }
 
-<<<<<<< HEAD
-
-=======
->>>>>>> ff5adb5d4d7f2cc6a84706c08ce1c76686b06a82
 function FilenameToFontname($filename, $family)
 {
     if ($filename == $family) {
