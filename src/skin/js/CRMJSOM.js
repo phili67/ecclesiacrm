347c530397e2bc1005e23971ee55bd3c8bdaf882
Philippe Logel <philippelogel@free.fr>
2017-12-31 19:34:30 +0100

    Bug

--- src/skin/js/CRMJSOM.js
+++ src/skin/js/CRMJSOM.js
@@ -15,7 +15,7 @@
 
     window.CRM.DisplayErrorMessage = function(endpoint, error) {
       if (endpoint.indexOf("/api/dashboard") !== -1) {// we are in the case, we're logout
-        location.reload();
+        //location.reload();
         return;
       }
 
