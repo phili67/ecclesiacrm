20526c757ea00f3a4d9cfc7fef012978026b76ba
Philippe Logel <philippelogel@free.fr>
2018-01-03 22:05:29 +0100

    the last modifications

--- src/Include/Header-function.php
+++ src/Include/Header-function.php
@@ -56,55 +56,59 @@ function Header_modals()
         <div class="modal-dialog">
             <!-- Modal content-->
             <div class="modal-content">
-            <div id="submitDiaglogStart">
-                <form name="issueReport">
-                    <input type="hidden" name="pageName" value="<?= $_SERVER['SCRIPT_NAME'] ?>"/>
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title"><?= gettext('Issue Report!') ?></h4>
-                    </div>
-                    <div class="modal-body">
-                        <div class="container-fluid">
-                            <div class="row">
-                                <div class="col-xl-3">
-                                    <label
-                                            for="issueTitle"><?= gettext('Enter a Title for your bug / feature report') ?>
-                                        : </label>
-                                </div>
-                                <div class="col-xl-3">
-                                    <input type="text" name="issueTitle"  style="min-width: 100%;max-width: 100%;">
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xl-3">
-                                    <label
-                                            for="issueDescription"><?= gettext('What were you doing when you noticed the bug / feature opportunity?') ?></label>
-                                </div>
-                                <div class="col-xl-3">
-                                    <textarea rows="10" name="issueDescription" style="min-width: 100%;max-width: 100%;"></textarea>
-                                </div>
-                            </div>
-                        </div>
-                        <ul>
-                            <li><?= gettext("When you click \"submit,\" an error report will be posted to the EcclesiaCRM GitHub Issue tracker.") ?></li>
-                            <li><?= gettext('Please do not include any confidential information.') ?></li>
-                            <li><?= gettext('Some general information about your system will be submitted along with the request such as Server version and browser headers.') ?></li>
-                            <li><?= gettext('No personally identifiable information will be submitted unless you purposefully include it.') ?></li>
-                        </ul>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" id="submitIssue"><?= gettext('Submit') ?></button>
-                    </div>
-                </form>
-            </div>
-            <div id="submitDiaglogFinish">
-              <div class="modal-body"><h2><?= _("Successfully submitted Issue") ?> <span id="issueSubmitSucces"></span></h2>
-              <a href="" target="_blank" id="issueSubmitSuccesLink"><?= _("View Issue on GitHub")." : #" ?> <span id="issueSubmitSuccesLinkText"></span></a>
-              <div class="modal-footer">
-                        <button type="button" class="btn btn-primary" id="submitIssueDone"><?= gettext('OK') ?></button>
+              <div id="submitDiaglogStart">
+                  <form name="issueReport">
+                      <input type="hidden" name="pageName" value="<?= $_SERVER['SCRIPT_NAME'] ?>"/>
+                      <div class="modal-header">
+                          <button type="button" class="close" data-dismiss="modal">&times;</button>
+                          <h4 class="modal-title"><?= gettext('Issue Report!') ?></h4>
+                      </div>
+                      <div class="modal-body">
+                          <div class="container-fluid">
+                              <div class="row">
+                                  <div class="col-xl-3">
+                                      <label
+                                              for="issueTitle"><?= gettext('Enter a Title for your bug / feature report') ?>
+                                          : </label>
+                                  </div>
+                                  <div class="col-xl-3">
+                                      <input type="text" name="issueTitle"  style="min-width: 100%;max-width: 100%;">
+                                  </div>
+                              </div>
+                              <div class="row">
+                                  <div class="col-xl-3">
+                                      <label
+                                              for="issueDescription"><?= gettext('What were you doing when you noticed the bug / feature opportunity?') ?></label>
+                                  </div>
+                                  <div class="col-xl-3">
+                                      <textarea rows="10" name="issueDescription" style="min-width: 100%;max-width: 100%;"></textarea>
+                                  </div>
+                              </div>
+                          </div>
+                          <ul>
+                              <li><?= gettext("When you click \"submit,\" an error report will be posted to the EcclesiaCRM GitHub Issue tracker.") ?></li>
+                              <li><?= gettext('Please do not include any confidential information.') ?></li>
+                              <li><?= gettext('Some general information about your system will be submitted along with the request such as Server version and browser headers.') ?></li>
+                              <li><?= gettext('No personally identifiable information will be submitted unless you purposefully include it.') ?></li>
+                          </ul>
+                      </div>
+                      <div class="modal-footer">
+                          <button type="button" class="btn btn-primary" id="submitIssue"><?= gettext('Submit') ?></button>
+                      </div>
+                  </form>
               </div>
-              </div>              
-            </div>
+              <div id="submitDiaglogFinish">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    <h4 class="modal-title"><?= gettext('Issue Report done!') ?></h4>
+                </div>
+                <div class="modal-body"><h2><?= _("Successfully submitted Issue") ?> <span id="issueSubmitSucces"></span></h2>
+                <a href="" target="_blank" id="issueSubmitSuccesLink"><?= _("View Issue on GitHub")." : #" ?> <span id="issueSubmitSuccesLinkText"></span></a>
+                <div class="modal-footer">
+                          <button type="button" class="btn btn-primary" id="submitIssueDone"><?= gettext('OK') ?></button>
+                </div>
+                </div>              
+              </div>
             </div>
 
         </div>
--- src/skin/js/IssueReporter.js
+++ src/skin/js/IssueReporter.js
@@ -1,41 +1,58 @@
+$(document).ready(function(){
+  $("#submitDiaglogStart" ).show();
+  $("#submitDiaglogFinish" ).hide();
+});
+
+
 $("#submitIssue").click(function () {
-  var postData = {
-    "issueTitle": $("input:text[name=issueTitle]").val(),
-    "issueDescription": $("textarea[name=issueDescription]").val(),
-    "pageName" : $("input[name=pageName]").val(),
-    "screenSize": {
-        "height":screen.height,
-        "width":screen.width
-    },
-    "windowSize":{
-        "height":$(window).height(),
-        "width":$(window).width()
-    },
-    "pageSize" : {
-        "height" : $(document).height(),
-        "width":$(document).width()
-    }
-  };
-  $.ajax({
-    method: "POST",
-    url: window.CRM.root + "/api/issues",
-    data: JSON.stringify(postData),
-    contentType: "application/json; charset=utf-8",
-    dataType: "json"
-  }).done(function (data) {
-    $("#submitDiaglogStart" ).hide();
-    $("#submitDiaglogFinish" ).show();
-    
-    //console.log(data);
-    
-    $("#issueSubmitSucces" ).text(data.number);
-    $("#issueSubmitSuccesLink").prop("href", data.url);
-    $("#issueSubmitSuccesLinkText" ).text(data.number);
-  });
+  if ($("input:text[name=issueTitle]").val() && $("textarea[name=issueDescription]").val()) {
+    var postData = {
+      "issueTitle": $("input:text[name=issueTitle]").val(),
+      "issueDescription": $("textarea[name=issueDescription]").val(),
+      "pageName" : $("input[name=pageName]").val(),
+      "screenSize": {
+          "height":screen.height,
+          "width":screen.width
+      },
+      "windowSize":{
+          "height":$(window).height(),
+          "width":$(window).width()
+      },
+      "pageSize" : {
+          "height" : $(document).height(),
+          "width":$(document).width()
+      }
+    };
+    $.ajax({
+      method: "POST",
+      url: window.CRM.root + "/api/issues",
+      data: JSON.stringify(postData),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json"
+    }).done(function (data) {
+      console.log(data);
+      
+      $("#issueSubmitSucces" ).text(data.number);
+      $("#issueSubmitSuccesLink").prop("href", data.url);
+      $("#issueSubmitSuccesLinkText" ).text(data.number);
+      
+      $("#submitDiaglogStart" ).hide();
+      $("#submitDiaglogFinish" ).show();    
+    });
+  } else {
+    var box = bootbox.dialog({title: "<span style='color: red;'>"+i18next.t("Error")+"</span>",message : i18next.t("You have to set a Title and a Description for your issue.")});
+                
+    setTimeout(function() {
+        // be careful not to call box.hide() here, which will invoke jQuery's hide method
+        box.modal('hide');
+    }, 3000);
+  
+    return false;
+  }
 });
 
 $("#submitIssueDone").click(function () {
-    $("#IssueReportModal").modal('toggle');;
+    $("#IssueReportModal").modal('toggle');
     $("#submitDiaglogStart" ).show();
     $("#submitDiaglogFinish" ).hide();   
     $("input:text[name=issueTitle]").val(""); 
@@ -43,7 +60,10 @@ $("#submitIssueDone").click(function () {
 });
 
 
-$(document).ready(function(){
+$("#IssueReportModal .close").click(function(){
   $("#submitDiaglogStart" ).show();
   $("#submitDiaglogFinish" ).hide();
-});
+  
+  $("input:text[name=issueTitle]").val(""); 
+  $("textarea[name=issueDescription]").val(""); 
+})
