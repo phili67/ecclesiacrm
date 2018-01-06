0587246f96c1e0b1f252aaea443bfa7a35e6f308
Philippe Logel <philippelogel@free.fr>
2018-01-06 01:00:25 +0100

    The last modification

--- src/api/routes/properties.php
+++ src/api/routes/properties.php
@@ -120,4 +120,32 @@ $app->group('/properties', function() {
 
         return $response->withJson(['success' => true, 'msg' => gettext('The menu is successfully assigned.')]);
     });
+    
+    $this->post('/sundayschoolmenu/unassign', function($request, $response, $args) {
+        if (!$_SESSION['user']->isAdmin()) {
+            return $response->withStatus(401);
+        }
+ 
+        $data = (object)$request->getParsedBody();
+        
+        $groupID = $data->groupID;
+        $propertyID = $data->propertyID;
+
+        $group = GroupQuery::create()->findPk($groupID);
+        $property = PropertyQuery::create()->findPk($propertyID);
+        if (!$group || !$property) {
+            return $response->withStatus(404, gettext('The record could not be found.'));
+        }
+        
+        $groupProperty = Record2propertyR2pQuery::create()// we loop to find the good record
+            ->filterByR2pRecordId($groupID)
+            ->filterByR2pProId($propertyID)
+            ->findOne();
+            
+        if ($groupProperty) { // we can delete the last property a sunday group menu is only affected to one group
+            $groupProperty->delete();
+        }
+
+        return $response->withJson(['success' => true, 'msg' => gettext('The menu is successfully unassigned.')]);
+    });
 });
