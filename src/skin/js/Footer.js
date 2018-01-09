6b5167ccbdf0a0b2d9a653add6a753bd67fa60a1
Philippe Logel <philippelogel@free.fr>
2018-01-09 21:32:14 +0100

    The changes

--- src/skin/js/Footer.js
+++ src/skin/js/Footer.js
@@ -314,6 +314,10 @@ $("document").ready(function(){
 
 $(document).on("click", "#deleteCart", function (e) {
   window.CRM.cart.delete(function(data) {
+    var path = location.href;
+    path = path.substring(path.lastIndexOf("/") + 1);
+    path = path.split("?")[0].split("#")[0]; 
+
     if (data.status == "failure")
     {
       var box = window.CRM.DisplayAlert("Error text",data.message);
@@ -321,17 +325,16 @@ $(document).on("click", "#deleteCart", function (e) {
       setTimeout(function() {
         // be careful not to call box.hide() here, which will invoke jQuery's hide method
         box.modal('hide');
-        var path = location.href;
-        path = path.substring(path.lastIndexOf("/") + 1);
-        path = path.split("?")[0].split("#")[0];
-  
+        
         if (path == "PersonView.php" && data != 'nothing was done') {
           location.reload();
         }
       }, 7000);
-    }
-    
-    
+    } else {
+      if (path == "PersonView.php" && data != 'nothing was done') {
+          location.reload();
+      }
+    }    
   });
 });
 
