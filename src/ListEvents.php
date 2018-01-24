71ee72431f2e1fecc9b7d4a43cc3de9270f51003
Philippe Logel <philippelogel@free.fr>
2018-01-24 17:26:51 +0100

    The last improvements

--- src/ListEvents.php
+++ src/ListEvents.php
@@ -78,6 +78,13 @@ $rsOpps = RunQuery($sSQL);
 $numRows = mysqli_num_rows($rsOpps);
 
 ?>
+<div class='text-center'>
+  <a href="EventEditor.php" class='btn btn-primary'>
+    <i class='fa fa-ticket'></i>
+    <?= gettext('Add New Event') ?>
+  </a>
+</div>
+
 <table cellpadding="1" align="center" cellspacing="0" class='table'>
 <tr>
 <td align="center" width="50%"><p><strong><?= gettext('Select Event Types To Display') ?></strong></p>
@@ -283,8 +290,8 @@ foreach ($allMonths as $mKey => $mVal) {
                     $cvSQL = "SELECT * FROM eventcounts_evtcnt WHERE evtcnt_eventid='$aEventID[$row]' ORDER BY evtcnt_countid ASC";
                     $cvOpps = RunQuery($cvSQL);
                     $aNumCounts = mysqli_num_rows($cvOpps);
-
-                    if ($aNumCounts) {
+                    
+                    if ($aNumCounts > 0 && $attNumRows[$row] == 0) {
                         for ($c = 0; $c < $aNumCounts; $c++) {
                             $cRow = mysqli_fetch_array($cvOpps, MYSQLI_BOTH);
                             extract($cRow);
@@ -327,15 +334,12 @@ foreach ($allMonths as $mKey => $mVal) {
                 <tr>
                    <td colspan="3">
                      <center>
-                     <?php
-                       if ($attNumRows[$row]-$attCheckOut[$row] > 0) {
-                    ?>
                       <table>
                       <tr>
                       <td>
                       <form name="EditAttendees" action="EditEventAttendees.php" method="POST">
                         <input type="hidden" name="EID" value="<?= $aEventID[$row] ?>">
-                         <input type="hidden" name="EName" value="<?= $aEventTitle[$row] ?>">
+                        <input type="hidden" name="EName" value="<?= $aEventTitle[$row] ?>">
                         <input type="hidden" name="EDesc" value="<?= $aEventDesc[$row] ?>">
                         <input type="hidden" name="EDate" value="<?= OutputUtils::FormatDate($aEventStartDateTime[$row], 1) ?>">
                         <input type="submit" name="Action" value="<?= gettext('Attendees').'('.$attNumRows[$row].')' ?>" class="btn btn-info btn-sm" >
@@ -344,25 +348,13 @@ foreach ($allMonths as $mKey => $mVal) {
                       <td>
                       <form action="<?= SystemURLs::getRootPath() ?>/Checkin.php" method="POST">
                         <input type="hidden" name="EventID" value="<?= $aEventID[$row] ?>">
-                        <button type="submit" name="Action" title="<?=gettext('Make Check-out') ?>" data-tooltip value="<?=gettext('Make Check-out') ?>" class="btn btn-success btn-sm">
+                        <button type="submit" name="Action" title="<?=gettext('Make Check-out') ?>" data-tooltip value="<?=gettext('Make Check-out') ?>" class="btn btn-<?= ($attNumRows[$row]-$attCheckOut[$row] > 0)?"success":"default disabled" ?> btn-sm">
                           <i class='fa fa-check-circle'></i> <?=gettext('Make Check-out') ?>
                         </button>                      
                        </form>
                        </td>
                        </tr>
                        </table>
-                     <?php } else { ?>
-                     <form name="EditAttendees" action="EditEventAttendees.php" method="POST">
-                        <input type="hidden" name="EID" value="<?= $aEventID[$row] ?>">
-                         <input type="hidden" name="EName" value="<?= $aEventTitle[$row] ?>">
-                        <input type="hidden" name="EDesc" value="<?= $aEventDesc[$row] ?>">
-                        <input type="hidden" name="EDate" value="<?= OutputUtils::FormatDate($aEventStartDateTime[$row], 1) ?>">
-                        <input type="submit" name="Action" value="<?= gettext('Attendees').'('.$attNumRows[$row].')' ?>" class="btn btn-info btn-sm" >
-                      </form>
-                      <button type="submit" name="Action" title="<?=gettext('Make Check-out') ?>" data-tooltip value="<?=gettext('Make Check-out') ?>" class="btn btn-success disabled">
-                         <i class='fa fa-check-circle'></i> <?=gettext('Make Check-out') ?>
-                      </button>                     
-                     <?php } ?>
                      </center>
                    </td>
                 </tr>
@@ -591,10 +583,10 @@ foreach ($allMonths as $mKey => $mVal) {
 } // end for-each month loop
 ?>
 
-<div class='text-center'>
-  <a href="EventEditor.php" class='btn btn-primary'>
-    <i class='fa fa-ticket'></i>
-    <?= gettext('Add New Event') ?>
+<div>
+  <a href="calendar.php" class='btn btn-default'>
+    <i class='fa fa-chevron-left'></i>
+    <?= gettext('Return to Calendar') ?>
   </a>
 </div>
 
