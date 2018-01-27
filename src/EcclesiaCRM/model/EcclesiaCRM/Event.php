d8eb9c1067e7f58af077e772d9bc91c3b5e8d473
Philippe Logel <philippelogel@free.fr>
2018-01-27 17:41:48 +0100

    A little bug

--- src/EcclesiaCRM/model/EcclesiaCRM/Event.php
+++ src/EcclesiaCRM/model/EcclesiaCRM/Event.php
@@ -55,7 +55,7 @@ class Event extends BaseEvent
   public function getEventURI()
   {
     if($_SESSION['bAddEvent'] || $_SESSION['bAdmin'])
-      return SystemURLs::getRootPath()."EventEditor.php?calendarAction=".$this->getID();
+      return SystemURLs::getRootPath()."/EventEditor.php?calendarAction=".$this->getID();
     else 
       return '';
   }
