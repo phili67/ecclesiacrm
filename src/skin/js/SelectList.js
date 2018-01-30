3899a11744c9c83dcc79a541ab9c634be17392e8
Philippe Logel <philippelogel@free.fr>
2018-01-30 23:22:57 +0100

    The bug resolution with cart

--- src/skin/js/SelectList.js
+++ src/skin/js/SelectList.js
@@ -6,4 +6,62 @@ $(document).ready(function () {
    $("#RemoveAllFromCart").click(function(){
     window.CRM.cart.removePerson(listPeople);
   });
+  
+  $(document).on("click",".AddToPeopleCart", function(){
+      clickedButton = $(this);
+      window.CRM.cart.addPerson([clickedButton.data("cartpersonid")],function()
+      {
+        $(clickedButton).addClass("RemoveFromPeopleCart");
+        $(clickedButton).removeClass("AddToPeopleCart");
+        $('span i:nth-child(2)',clickedButton).addClass("fa-remove");
+        $('span i:nth-child(2)',clickedButton).removeClass("fa-cart-plus");
+      });
+  });
+  
+  $(document).on("click",".RemoveFromPeopleCart", function(){
+      clickedButton = $(this);
+      window.CRM.cart.removePerson([clickedButton.data("cartpersonid")],function()
+      {
+        $(clickedButton).addClass("AddToPeopleCart");
+        $(clickedButton).removeClass("RemoveFromPeopleCart");
+        $('span i:nth-child(2)',clickedButton).removeClass("fa-remove");
+        $('span i:nth-child(2)',clickedButton).addClass("fa-cart-plus");
+      });
+    });
+    
+      // newMessage event subscribers : Listener CRJSOM.js
+    $(document).on("updateCartMessage", updateLittleButtons);
+    
+    function updateLittleButtons(e) {
+        var cartPeople = e.people;
+        
+        personButtons = $("a[data-cartpersonid]");
+        $(personButtons).each(function(index,personButton){
+          personID = $(personButton).data("cartpersonid")
+          if (cartPeople.includes(personID)) {
+            personPresent = true;
+            $(personButton).addClass("RemoveFromPeopleCart");
+            $(personButton).removeClass("AddToPeopleCart");
+            fa = $(personButton).find("i.fa.fa-inverse");
+            $(fa).addClass("fa-remove");
+            $(fa).removeClass("fa-cart-plus");
+            text = $(personButton).find("span.cartActionDescription")
+            if(text){
+              $(text).text(i18next.t("Remove from Cart"));
+            }
+          } else {
+            $(personButton).addClass("AddToPeopleCart");
+            $(personButton).removeClass("RemoveFromPeopleCart");
+            fa = $(personButton).find("i.fa.fa-inverse");
+            
+            $(fa).removeClass("fa-remove");
+            $(fa).addClass("fa-cart-plus");
+            text = $(personButton).find("span.cartActionDescription")
+            if(text){
+              $(text).text(i18next.t("Add to Cart"));
+            }
+          }
+        });
+    }
+
 });
