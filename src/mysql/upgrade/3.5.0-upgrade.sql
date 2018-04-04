8e88eae20288a3ac04131e9987f21af76e8d9407
Philippe Logel <philippelogel@free.fr>
2018-04-04 21:09:35 +0200

    The changes for the new release

--- src/mysql/upgrade/3.5.0-upgrade.sql
+++ src/mysql/upgrade/3.5.0-upgrade.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `user_usr` ADD `usr_HomeDir` varchar(500) default NULL;
+ALTER TABLE `user_usr` ADD `usr_HomeDir` varchar(500) DEFAULT NULL;
+ALTER TABLE `note_nte` ADD `nte_Info` varchar(500) DEFAULT NULL;
