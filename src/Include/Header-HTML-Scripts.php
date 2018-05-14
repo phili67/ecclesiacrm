3501f751a6cf804b0b1ed9ec66b7e1a23c9a05d8
Philippe Logel <philippelogel@free.fr>
2018-05-14 19:32:21 +0200

    The new Icon

--- src/Include/Header-HTML-Scripts.php
+++ src/Include/Header-HTML-Scripts.php
@@ -16,3 +16,42 @@ use EcclesiaCRM\dto\SystemURLs;
 <script src="<?= SystemURLs::getRootPath() ?>/skin/external/jquery-ui/jquery-ui.min.js"></script>
 
 <script src="<?= SystemURLs::getRootPath() ?>/skin/external/moment/moment-with-locales.min.js"></script>
+
+<!-- For old IEs -->
+<link rel="shortcut icon" href="favicon.ico" />
+
+<!-- For new browsers - multisize ico  -->
+<link rel="icon" type="image/x-icon" sizes="16x16 32x32" href="Favicons/favicon.ico">
+
+<!-- For iPad with high-resolution Retina display running iOS ≥ 7: -->
+<link rel="apple-touch-icon" sizes="152x152" href="Favicons/favicon-152-precomposed.png">
+
+<!-- For iPad with high-resolution Retina display running iOS ≤ 6: -->
+<link rel="apple-touch-icon" sizes="144x144" href="Favicons/favicon-144-precomposed.png">
+
+<!-- For iPhone with high-resolution Retina display running iOS ≥ 7: -->
+<link rel="apple-touch-icon" sizes="120x120" href="Favicons/favicon-120-precomposed.png">
+
+<!-- For iPhone with high-resolution Retina display running iOS ≤ 6: -->
+<link rel="apple-touch-icon" sizes="114x114" href="Favicons/favicon-114-precomposed.png">
+
+<!-- For iPhone 6+ -->
+<link rel="apple-touch-icon" sizes="180x180" href="Favicons/favicon-180-precomposed.png">
+
+<!-- For first- and second-generation iPad: -->
+<link rel="apple-touch-icon" sizes="72x72" href="Favicons/favicon-72-precomposed.png">
+
+<!-- For non-Retina iPhone, iPod Touch, and Android 2.1+ devices: -->
+<link rel="apple-touch-icon" href="Favicons/favicon-57.png">
+
+<!-- For Old Chrome -->
+<link rel="icon" href="Favicons/favicon-32.png" sizes="32x32">
+
+<!-- For IE10 Metro -->
+<meta name="msapplication-TileColor" content="#FFFFFF">
+<meta name="msapplication-TileImage" content="favicon-144.png">
+<meta name="theme-color" content="#ffffff">
+
+<!-- Chrome for Android -->
+<link rel="manifest" href="Favicons/manifest.json">
+<link rel="icon" sizes="192x192" href="Favicons/favicon-192.png">
