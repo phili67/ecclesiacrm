ddef7ffd43b4a52644680becae3e6bf0e89fc582
ecclesia <philippelogel@free.fr>
2018-05-21 09:25:21 +0200

    Revert "The new 4.1 version"

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -1580,25 +1580,3 @@ CREATE TABLE note_nte_share (
       REFERENCES family_fam(fam_ID)
       ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `group_manager_person`
---
-
-
-CREATE TABLE group_manager_person (
-    `grp_mgr_per_id` mediumint(9) unsigned  NOT NULL AUTO_INCREMENT,
-    `grp_mgr_per_person_ID` mediumint(9) unsigned NOT NULL,
-    `grp_mgr_per_group_ID` mediumint(9) unsigned NOT NULL,
-    PRIMARY KEY(grp_mgr_per_id),
-    CONSTRAINT fk_grp_mgr_per_person_ID
-      FOREIGN KEY (grp_mgr_per_person_ID) 
-      REFERENCES person_per(per_ID)
-      ON DELETE CASCADE,
-    CONSTRAINT fk_grp_mgr_per_group_ID
-      FOREIGN KEY (grp_mgr_per_person_ID) 
-      REFERENCES group_grp(grp_ID)
-      ON DELETE CASCADE
-) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci;
