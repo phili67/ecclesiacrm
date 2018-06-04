086e40575fddf26c333207a32ce8fb32b6292224
Philippe Logel <philippelogel@free.fr>
2018-06-04 11:08:48 +0200

    The php script update for the 4.1 upgrade

---
+++ src/mysql/upgrade/4.1.0-upgrade.php
@@ -0,0 +1,14 @@
+<?php 
+  use Propel\Runtime\Propel;
+  use EcclesiaCRM\Utils\LoggerUtils;
+  use EcclesiaCRM\dto\SystemURLs;
+
+  $connection = Propel::getConnection();
+  $logger = LoggerUtils::getAppLogger();
+
+  $logger->info("Start to delete : old unuseful files");
+
+  unlink(SystemURLs::getDocumentRoot()."/mysql/upgrade/rebuild_nav_menus.sql");
+  
+  $logger->info("End Delete the old unuseful files");
+?>
