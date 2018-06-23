24acbd6331b6c6867178669c5642cab988918f29
Philippe Logel <philippelogel@free.fr>
2018-06-23 05:56:13 +0200

    Copyright upgrade

--- src/EcclesiaCRM/sabre/CalDavPDO.php
+++ src/EcclesiaCRM/sabre/CalDavPDO.php
@@ -1,5 +1,13 @@
 <?php
 
+//
+//  This code is under copyright not under MIT Licence
+//  copyright   : 2018 Philippe Logel all right reserved not MIT licence
+//                This code can't be incoprorated in another software without any authorizaion
+//
+//  Updated : 2018/06/23
+//
+
 namespace EcclesiaCRM\MyPDO;
 
 use Sabre\CalDAV;
