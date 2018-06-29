28f81d2871895687cf00683245c414932a04dc0b
Philippe Logel <philippelogel@free.fr>
2018-06-29 09:40:29 +0200

    IconPicker is a full independant class

--- src/skin/js/IconPicker.js
+++ src/skin/js/IconPicker.js
@@ -3,14 +3,33 @@
 //  copyright   : 2018 Philippe Logel all right reserved not MIT licence
 //  Updated     : 2018/06/28
 //
+
+// usage :
+// modal = createImagePickerWindow ({
+//    title:"A title Name",
+//    firstLabel:"First Label",
+//    message: "Select your icon below",
+//    label:"name",
+//    directory:"/your_path"
+//  },
+//  function(selectedName) {
+//     console.log(selectedName);
+//  });  
+  
   
     var selectedName = '';
 
-    function BootboxContent(){          
-       var frm_str = '<h3 style="margin-top:-5px">'+i18next.t("Choose a Map Icon")+'</h3>'
+    function BootboxContent(title,firstLabel,label,message){          
+       var frm_str = '<h3 style="margin-top:-5px">'+title+'</h3>'
           + '<div>'
             +'<div class="row div-title">'
-              +'<div class="col-md-9"><div id="here_table"></div></div>'
+              +'<div class="col-md-5"><label>'+firstLabel+' : </label></div><div class="col-md-7">"'+label+'"</div></div>'
+            +'</div>'
+            +'<div class="row div-title">'
+              +'<div class="col-md-12">'+message+'</div>'
+            +'</div>'
+            +'<div class="row">'
+              +'<div class="col-md-12"><div id="here_table"></div></div>'
             +'</div>'
           +'</div>'
 
@@ -19,64 +38,32 @@
         return object
     }
     
-    function createImagePickerWindow (lstID,lstOptionID) // dialogType : createEvent or modifyEvent, eventID is when you modify and event
-    {      
-      var modal = bootbox.dialog({
-         message: BootboxContent(),
-         buttons: [
-          {
-           label: i18next.t("Close"),
-           className: "btn btn-default",
-          },
-          {
-           label: i18next.t("Save"),
-           className: "btn btn-primary",
-           callback: function() {
-                                                            
-                  window.CRM.APIRequest({
-                      method: 'POST',
-                      path: 'mapicons/setIconName',
-                      data: JSON.stringify({"name":selectedName,"lstID":lstID,"lstOptionID":lstOptionID})
-                  }).done(function(data) {
-                     location.reload();
-                  });
-
-                  return true;  
-            }
-          }
-         ],
-         onEscape: function() {
-            modal.modal("hide");
-         }
-       });
-              
-       return modal;
-    }
-    
     $('body').on('click','.imgCollection', function(){ 
       var name = $(this).data("name");
       
       selectedName = name;
     
       $( ".imgCollection" ).each(function( index ) {
-            $(this).css('border', "solid 0px red"); 
+            $(this).css('border', "solid 1px white"); 
       });
 
-      $(this).css('border', "solid 1px red"); 
+      $(this).css('border', "solid 1px blue"); 
     });
 
     
-    function AddIcons() {
+    function AddIcons(directory) {
       window.CRM.APIRequest({
           method: 'POST',
           path: 'mapicons/getall',
       }).done(function(data) {
         var len = data.length;
     
-        $('#here_table').append('<table></table>');
+        $('#here_table').append('<table width=100%></table>');
+        
         var table = $('#here_table').children();
+        
         for(i=0;i<len;i++){
-         if (i%17 == 0) {
+         if (i%9 == 0) {
              if (i==0) {
                var buff = '<tr>';
              } else {
@@ -84,12 +71,12 @@
                var buff = '<tr>';
              }
           } else {
-            buff += '<td><img src="' + window.CRM.root+'/skin/icons/markers/'+data[i] + '" class="imgCollection" data-name="'+data[i]+'" border="0"></td>';
+            buff += '<td><img src="' + directory+data[i] + '" class="imgCollection" data-name="'+data[i]+'" style="border:solid 1px white"></td>';
           }
         }
     
         if (buff != '') {
-          len = len%15;
+          len = len%9;
           for (i=0;i<len;i++) {
             buff += '<td></td>';
           }
@@ -97,28 +84,35 @@
         }
       });
     }
-
-
-$('.RemoveImage').click('focus', function (e) {
-  var lstID = $(this).data('id');
-  var lstOptionID = $(this).data('optionid');
-  
-  window.CRM.APIRequest({
-      method: 'POST',
-      path: 'mapicons/removeIcon',
-      data: JSON.stringify({"lstID":lstID,"lstOptionID":lstOptionID})
-  }).done(function(data) {
-     location.reload();
-  });
-});
-
-
-$('.AddImage').click('focus', function (e) {
-  var lstID = $(this).data('id');
-  var lstOptionID = $(this).data('optionid');
-  
-  selectedName = '';
-  modal = createImagePickerWindow (lstID,lstOptionID);  
-  AddIcons();
-  modal.modal("show");
-});
+    
+    
+    function createImagePickerWindow (options,callback) // dialogType : createEvent or modifyEvent, eventID is when you modify and event
+    {      
+      var diag = bootbox.dialog({
+         message: BootboxContent(options.title,options.firstLabel,options.label,options.message,options.directory),
+         size: "small",
+         buttons: [
+          {
+           label: i18next.t("Cancel"),
+           className: "btn btn-default",
+          },
+          {
+           label: i18next.t("Validate"),
+           className: "btn btn-primary",
+           callback: function() {
+              if (callback) {
+                  return callback(selectedName);
+              }
+            }
+          }
+         ],
+         onEscape: function() {
+            modal.modal("hide");
+         }
+       });
+               
+       AddIcons(options.directory);
+       diag.modal("show");
+              
+       return diag;
+    }
