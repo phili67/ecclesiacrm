43500969baa26f72ea41298157a35f8a88cca9bb
Philippe Logel <philippelogel@free.fr>
2018-07-18 06:40:41 +0200

    Some lost files

---
+++ src/EcclesiaCRM/model/EcclesiaCRM/ListOptionIcon.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EcclesiaCRM;
+
+use EcclesiaCRM\Base\ListOptionIcon as BaseListOptionIcon;
+
+/**
+ * Skeleton subclass for representing a row from the 'list_icon' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ListOptionIcon extends BaseListOptionIcon
+{
+
+}
---
+++ src/EcclesiaCRM/model/EcclesiaCRM/ListOptionIconQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace EcclesiaCRM;
+
+use EcclesiaCRM\Base\ListOptionIconQuery as BaseListOptionIconQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'list_icon' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class ListOptionIconQuery extends BaseListOptionIconQuery
+{
+
+}
