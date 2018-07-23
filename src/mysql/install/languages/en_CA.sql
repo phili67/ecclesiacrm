03c589df80f7caf842ba6e61a04ef5ac0d5f0aff
Philippe Logel <philippelogel@free.fr>
2018-07-23 22:06:05 +0200

    The languages for installation

---
+++ src/mysql/install/languages/en_CA.sql
@@ -0,0 +1,9 @@
+INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`) VALUES
+(39, 'sLanguage', 'en_CA'),
+(64, 'sDistanceUnit', 'miles'),
+(100, 'sPhoneFormat', '999 999 9999'),
+(101, 'sPhoneFormatWithExt', '999 999 9999'),
+(111, 'sPhoneFormatCell', '999 999 9999'),
+(112, 'sTimeFormat', '%H:%M'),
+(2051, 'sCurrency', '$')
+ON DUPLICATE KEY UPDATE cfg_name=VALUES(cfg_name),cfg_value=VALUES(cfg_value);
