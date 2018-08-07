11a52bf4dfd4dc1c1d7a3f832e1be9fb48a55a8a
Philippe Logel <philippelogel@free.fr>
2018-08-07 20:30:14 +0200

    Update localisation

--- src/mysql/install/languages/en_CA.sql
+++ src/mysql/install/languages/en_CA.sql
@@ -4,5 +4,6 @@ INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`) VALUES
 (101, 'sPhoneFormatWithExt', '999 999 9999'),
 (111, 'sPhoneFormatCell', '999 999 9999'),
 (112, 'sTimeFormat', '%H:%M'),
+(2050, 'bStateUnusefull', '1'),
 (2051, 'sCurrency', '$')
 ON DUPLICATE KEY UPDATE cfg_name=VALUES(cfg_name),cfg_value=VALUES(cfg_value);
--- src/mysql/install/languages/es_ES.sql
+++ src/mysql/install/languages/es_ES.sql
@@ -4,6 +4,7 @@ INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`) VALUES
 (101, 'sPhoneFormatWithExt', '999 999 999'),
 (111, 'sPhoneFormatCell', '999 999 999'),
 (112, 'sTimeFormat', '%H:%M'),
+(2050, 'bStateUnusefull', '0'),
 (2051, 'sCurrency', '€')
 ON DUPLICATE KEY UPDATE cfg_name=VALUES(cfg_name),cfg_value=VALUES(cfg_value);
 
