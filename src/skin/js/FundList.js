089f55482ff5480039086767c277b79fd697186a
Philippe Logel <philippelogel@free.fr>
2018-08-25 19:51:41 +0200

    DataTable coherences

--- src/skin/js/FundList.js
+++ src/skin/js/FundList.js
@@ -20,6 +20,14 @@ $(document).ready(function () {
       },
       {
         width: 'auto',
+        title:i18next.t('Actions'),
+        data:'Id',
+        render: function(data, type, full, meta) {
+          return '<a class="edit-fund" data-id="'+data+'"><i class="fa fa-pencil" aria-hidden="true"></i></a><a class="delete-fund" data-id="'+data+'">&nbsp;&nbsp;&nbsp;<i class="fa fa-trash-o" aria-hidden="true"></i></a>';
+        }
+      },      
+      {
+        width: 'auto',
         title:i18next.t('Active'),
         data:'Active',
         render: function(data, type, full, meta) {
@@ -40,22 +48,6 @@ $(document).ready(function () {
         data:'Description',
         render: function(data, type, full, meta) {
           return data;
-        }
-      },
-      {
-        width: 'auto',
-        title:i18next.t('Edit'),
-        data:'Id',
-        render: function(data, type, full, meta) {        
-          return '<button class="btn btn-success edit-fund" data-id="'+data+'" >'+i18next.t('Edit')+'</button>';
-        }
-      },
-      {
-        width: 'auto',
-        title:i18next.t('Delete'),
-        data:'Id',
-        render: function(data, type, full, meta) {
-          return '<button class="btn btn-danger delete-fund" data-id="'+data+'" >'+i18next.t('Delete')+'</button>';
         }
       }
     ],
