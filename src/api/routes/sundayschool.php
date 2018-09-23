ad378b078dcde5dc7b5d94c03726ffcf7cf84c0c
Philippe Logel <philippelogel@free.fr>
2018-09-23 21:30:02 +0200

    SundaySchoolView.php speed update

--- src/api/routes/sundayschool.php
+++ src/api/routes/sundayschool.php
@@ -21,10 +21,37 @@ $app->group('/sundayschool', function () {
     
     $thisClassChildren = $sundaySchoolService->getKidsFullDetails($args['groupId']);
     
-    
-    foreach ($thisClassChildren as $child) {
-    }
-
     echo "{\"ClassroomStudents\":".json_encode($thisClassChildren)."}";
+  });
+  
+  $this->post('/getAllGendersForDonut/{groupId:[0-9]+}',function($request,$response,$args) {
+    $sundaySchoolService = new SundaySchoolService();
+    
+    $genderChartArray = [];
+    foreach ($sundaySchoolService->getKidsGender($args['groupId']) as $gender => $kidsCount) {
+        array_push($genderChartArray, ["label" => gettext($gender), "data" => $kidsCount]);
+    }
+    return $response->withJson($genderChartArray);
+  });
+
+  $this->post('/getAllStudentsForChart/{groupId:[0-9]+}',function($request,$response,$args) {
+    $sundaySchoolService = new SundaySchoolService();
+    
+    $birthDayMonthChartArray = [];
+		foreach ($sundaySchoolService->getKidsBirthdayMonth($args['groupId']) as $birthDayMonth => $kidsCount) {
+		    $res[0] = gettext($birthDayMonth);
+		    $res[1] = $kidsCount;
+		    
+				$birthDayMonthChartArray[] = $res;
+		}
+    
+    return  $response->withJson($birthDayMonthChartArray);
+    
+    /*$birthDayMonthChartArray = "";
+		/*foreach ($sundaySchoolService->getKidsBirthdayMonth($args['groupId']) as $birthDayMonth => $kidsCount) {
+				$birthDayMonthChartArray .= "['".gettext($birthDayMonth)."',".$kidsCount."].";
+		}*/
+
+    //return  $response->withJson(["cocuou"=>"toto"]);
   });
 });
