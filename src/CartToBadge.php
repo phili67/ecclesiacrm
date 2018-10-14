d94102d05af14fd0a4edecb9816def3631ee834e
Philippe Logel <philippelogel@free.fr>
2018-10-14 21:44:14 +0200

    The last changes

--- src/CartToBadge.php
+++ src/CartToBadge.php
@@ -190,12 +190,16 @@ require 'Include/Footer.php';
     $(".delete-file").click(function () {
       var name = $(this).data("name");
       
-      window.CRM.APIRequest({
-        method: 'POST',
-        path: 'system/deletefile',
-        data: JSON.stringify({"name": name, "path" : '/Images/background/'})
-      }).done(function(data) {
-        location.reload();
+      bootbox.confirm(i18next.t("Are you sure, you want to delete this image ?"), function(result){
+        if (result) {
+          window.CRM.APIRequest({
+            method: 'POST',
+            path: 'system/deletefile',
+            data: JSON.stringify({"name": name, "path" : '/Images/background/'})
+          }).done(function(data) {
+            location.reload();
+          });
+        }
       });
     });
 
