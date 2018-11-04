a2b8989b8522a97bb450fce565a945aa257512d2
Philippe Logel <philippelogel@free.fr>
2018-11-04 21:21:45 +0100

    C32019 PDF_Label add

--- src/EcclesiaCRM/Reports/PDF_Label.php
+++ src/EcclesiaCRM/Reports/PDF_Label.php
@@ -46,17 +46,17 @@ class PDF_Label extends ChurchInfoReport
     // Private properties
     public $_Avery_Name = '';        // Name of format
     public $_Margin_Left = 0;        // Left margin of labels
-    public $_Margin_Top = 0;        // Top margin of labels
-    public $_X_Space = 0;        // Horizontal space between 2 labels
-    public $_Y_Space = 0;        // Vertical space between 2 labels
-    public $_X_Number = 0;        // Number of labels horizontally
-    public $_Y_Number = 0;        // Number of labels vertically
-    public $_Width = 0;        // Width of label
-    public $_Height = 0;        // Height of label
-    public $_Char_Size = 10;        // Character size
-    public $_Line_Height = 10;        // Default line height
-    public $_Metric = 'mm';        // Type of metric.. Will help to calculate good values
-    public $_Metric_Doc = 'mm';        // Type of metric for the doc..
+    public $_Margin_Top = 0;         // Top margin of labels
+    public $_X_Space = 0;            // Horizontal space between 2 labels
+    public $_Y_Space = 0;            // Vertical space between 2 labels
+    public $_X_Number = 0;           // Number of labels horizontally
+    public $_Y_Number = 0;           // Number of labels vertically
+    public $_Width = 0;              // Width of label
+    public $_Height = 0;             // Height of label
+    public $_Char_Size = 10;         // Character size
+    public $_Line_Height = 10;       // Default line height
+    public $_Metric = 'mm';          // Type of metric.. Will help to calculate good values
+    public $_Metric_Doc = 'mm';      // Type of metric for the doc..
 
     public $_COUNTX = 1;
     public $_COUNTY = 1;
@@ -64,15 +64,16 @@ class PDF_Label extends ChurchInfoReport
     // List of all Avery formats
     public $_Avery_Labels = [
         'Tractor'=> ['name'=>'Tractor', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>6.5, 'marginTop'=>5, 'NX'=>1, 'NY'=>10, 'SpaceX'=>3.175, 'SpaceY'=>0, 'width'=>120, 'height'=>26.5, 'font-size'=>12],
-        'Badge'  => ['name'=>'Badge', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>1, 'marginTop'=>11.5, 'NX'=>3, 'NY'=>6, 'SpaceX'=>2, 'SpaceY'=>1, 'width'=>70, 'height'=>40, 'font-size'=>11],
-        '5160'   => ['name'=>'5160', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>4, 'marginTop'=>11.5, 'NX'=>3, 'NY'=>10, 'SpaceX'=>3.175, 'SpaceY'=>0, 'width'=>66.675, 'height'=>25.4, 'font-size'=>11],
-        '5161'   => ['name'=>'5161', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>1, 'marginTop'=>10.7, 'NX'=>2, 'NY'=>10, 'SpaceX'=>3.967, 'SpaceY'=>0, 'width'=>101.6, 'height'=>25.4, 'font-size'=>11],
-        '5162'   => ['name'=>'5162', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>4, 'marginTop'=>20.224, 'NX'=>2, 'NY'=>7, 'SpaceX'=>4.762, 'SpaceY'=>0, 'width'=>100.807, 'height'=>34, 'font-size'=>12],
-        '5163'   => ['name'=>'5163', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>1.762, 'marginTop'=>10.7, 'NX'=>2, 'NY'=>5, 'SpaceX'=>3.175, 'SpaceY'=>0, 'width'=>101.6, 'height'=>50.8, 'font-size'=>8],
-        '5164'   => ['name'=>'5164', 'paper-size'=>'letter', 'metric'=>'in', 'marginLeft'=>0.148, 'marginTop'=>0.5, 'NX'=>2, 'NY'=>3, 'SpaceX'=>0.2031, 'SpaceY'=>0, 'width'=>4.0, 'height'=>3.33, 'font-size'=>12],
-        '8600'   => ['name'=>'8600', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>7.1, 'marginTop'=>19, 'NX'=>3, 'NY'=>10, 'SpaceX'=>9.5, 'SpaceY'=>3.1, 'width'=>66.6, 'height'=>25.4, 'font-size'=>8],
+        'Badge'  => ['name'=>'Badge',   'paper-size'=>'A4',     'metric'=>'mm', 'marginLeft'=>1, 'marginTop'=>11.5, 'NX'=>3, 'NY'=>6, 'SpaceX'=>2, 'SpaceY'=>1, 'width'=>70, 'height'=>40, 'font-size'=>11],
+        '5160'   => ['name'=>'5160',    'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>4, 'marginTop'=>11.5, 'NX'=>3, 'NY'=>10, 'SpaceX'=>3.175, 'SpaceY'=>0, 'width'=>66.675, 'height'=>25.4, 'font-size'=>11],
+        '5161'   => ['name'=>'5161',    'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>1, 'marginTop'=>10.7, 'NX'=>2, 'NY'=>10, 'SpaceX'=>3.967, 'SpaceY'=>0, 'width'=>101.6, 'height'=>25.4, 'font-size'=>11],
+        '5162'   => ['name'=>'5162',    'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>4, 'marginTop'=>20.224, 'NX'=>2, 'NY'=>7, 'SpaceX'=>4.762, 'SpaceY'=>0, 'width'=>100.807, 'height'=>34, 'font-size'=>12],
+        '5163'   => ['name'=>'5163',    'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>1.762, 'marginTop'=>10.7, 'NX'=>2, 'NY'=>5, 'SpaceX'=>3.175, 'SpaceY'=>0, 'width'=>101.6, 'height'=>50.8, 'font-size'=>8],
+        '5164'   => ['name'=>'5164',    'paper-size'=>'letter', 'metric'=>'in', 'marginLeft'=>0.148, 'marginTop'=>0.5, 'NX'=>2, 'NY'=>3, 'SpaceX'=>0.2031, 'SpaceY'=>0, 'width'=>4.0, 'height'=>3.33, 'font-size'=>12],
+        '8600'   => ['name'=>'8600',    'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>7.1, 'marginTop'=>19, 'NX'=>3, 'NY'=>10, 'SpaceX'=>9.5, 'SpaceY'=>3.1, 'width'=>66.6, 'height'=>25.4, 'font-size'=>8],
         '74536'  => ['name'=>'74536(name tags)', 'paper-size'=>'letter', 'metric'=>'mm', 'marginLeft'=>7.0, 'marginTop'=>25, 'NX'=>2, 'NY'=>3, 'SpaceX'=>0, 'SpaceY'=>0, 'width'=>102, 'height'=>76, 'font-size'=>18],
-        'L7163'  => ['name'=>'L7163', 'paper-size'=>'A4', 'metric'=>'mm', 'marginLeft'=>5, 'marginTop'=>15, 'NX'=>2, 'NY'=>7, 'SpaceX'=>2.5, 'SpaceY'=>0, 'width'=>99.1, 'height'=>38.1, 'font-size'=>10],
+        'L7163'  => ['name'=>'L7163', 'paper-size'=>'A4',      'metric'=>'mm', 'marginLeft'=>5, 'marginTop'=>15, 'NX'=>2, 'NY'=>7, 'SpaceX'=>2.5, 'SpaceY'=>0, 'width'=>99.1, 'height'=>38.1, 'font-size'=>10],
+        'C32019' => ['name'=>'C32019', 'paper-size'=>'A4',     'metric'=>'mm', 'marginLeft'=>14, 'marginTop'=>13.5, 'NX'=>2, 'NY'=>5, 'SpaceX'=>10.5, 'SpaceY'=>0, 'width'=>85, 'height'=>54, 'font-size'=>11],
     ];
 
     // convert units (in to mm, mm to in)
