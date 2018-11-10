b4706a39d3de7b1cc7090ba3182dc2ec961dea74
Philippe Logel <philippelogel@free.fr>
2018-11-10 19:37:30 +0100

    New update in the sql code and in CardDavPDO.php

--- src/mysql/upgrade/5.2.0-upgrade.sql
+++ src/mysql/upgrade/5.2.0-upgrade.sql
@@ -8,6 +8,18 @@ CREATE TABLE IF NOT EXISTS addressbooks (
     UNIQUE(principaluri(100), uri(100))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+CREATE TABLE IF NOT EXISTS addressbookshare (
+    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    addressbooksid INTEGER UNSIGNED NOT NULL  DEFAULT '0',
+    principaluri VARBINARY(255),
+    displayname VARCHAR(255),
+    description TEXT,
+    href VARBINARY(100),
+    access TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 = read, 2 = readwrite',
+    UNIQUE(principaluri(100), uri(100)),
+    UNIQUE(addressbooksid, principaluri)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE IF NOT EXISTS cards (
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     addressbookid INT(11) UNSIGNED NOT NULL,
