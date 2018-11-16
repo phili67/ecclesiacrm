c66cfd26020ef9ea1782efa57917ca0f901cdc96
Philippe Logel <philippelogel@free.fr>
2018-11-16 22:33:46 +0100

    A lost file

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -868,6 +868,7 @@ CREATE TABLE `property_pro` (
   `pro_Name` varchar(200) NOT NULL default '0',
   `pro_Description` text NOT NULL,
   `pro_Prompt` varchar(255) default NULL,
+  `pro_Comment` text NOT NULL default '' COMMENT 'comment for GDPR',
   PRIMARY KEY  (`pro_ID`),
   UNIQUE KEY `pro_ID` (`pro_ID`),
   KEY `pro_ID_2` (`pro_ID`)
@@ -1733,3 +1734,16 @@ INSERT INTO `gdpr_infos` (`gdpr_info_About`, `gdpr_info_Name`, `gdpr_info_Type`,
 --
 -- Dumping data for table `gdpr_infos`
 --
+
+
+-- method for dup emails
+
+CREATE VIEW email_list AS
+    SELECT fam_Email AS email, 'family' AS type, fam_id AS id FROM family_fam WHERE fam_email IS NOT NULL AND fam_email != ''
+    UNION
+    SELECT per_email AS email, 'person_home' AS type, per_id AS id FROM person_per WHERE per_email IS NOT NULL AND per_email != ''
+    UNION
+    SELECT per_WorkEmail AS email, 'person_work' AS type, per_id AS id FROM person_per WHERE per_WorkEmail IS NOT NULL AND per_WorkEmail != '';
+
+CREATE VIEW email_count AS
+    SELECT email, COUNT(*) AS total FROM email_list group by email;
