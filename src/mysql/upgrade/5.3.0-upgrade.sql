4916601a69a51eae0c763acb556a14b440b6c98a
Philippe Logel <philippelogel@free.fr>
2018-12-12 22:38:42 +0100

    The changes

--- src/mysql/upgrade/5.3.0-upgrade.sql
+++ src/mysql/upgrade/5.3.0-upgrade.sql
@@ -1,13 +1,3 @@
-CREATE TABLE IF NOT EXISTS addressbooks (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    principaluri VARBINARY(255),
-    displayname VARCHAR(255),
-    uri VARBINARY(200),
-    description TEXT,
-    synctoken INT(11) UNSIGNED NOT NULL DEFAULT '1',
-    UNIQUE(principaluri(100), uri(100))
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
 CREATE TABLE IF NOT EXISTS addressbookshare (
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     addressbooksid INTEGER UNSIGNED NOT NULL  DEFAULT '0',
@@ -18,23 +8,4 @@ CREATE TABLE IF NOT EXISTS addressbookshare (
     access TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 = owner, 2 = read, 3 = readwrite',
     UNIQUE(principaluri(100)),
     UNIQUE(addressbooksid, principaluri)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS cards (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    addressbookid INT(11) UNSIGNED NOT NULL,
-    carddata MEDIUMBLOB,
-    uri VARBINARY(200),
-    lastmodified INT(11) UNSIGNED,
-    etag VARBINARY(32),
-    size INT(11) UNSIGNED NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-CREATE TABLE IF NOT EXISTS addressbookchanges (
-    id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    uri VARBINARY(200) NOT NULL,
-    synctoken INT(11) UNSIGNED NOT NULL,
-    addressbookid INT(11) UNSIGNED NOT NULL,
-    operation TINYINT(1) NOT NULL,
-    INDEX addressbookid_synctoken (addressbookid, synctoken)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
