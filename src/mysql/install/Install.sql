71fe40d14db04cb3bc578e1396288d1635901c87
Philippe Logel <philippelogel@free.fr>
2018-12-13 09:58:27 +0100

    The schema.xml update

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -8,22 +8,24 @@ CREATE TABLE addressbooks (
     uri VARBINARY(200),
     description TEXT,
     synctoken INT(11) UNSIGNED NOT NULL DEFAULT '1',
+    groupId mediumint(8) NOT NULL default -1 COMMENT '-1 personal addressbook, >1 for a group in the CRM',
     UNIQUE(principaluri(100), uri(100))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Table structure for table `addressbooks`
 --
-CREATE TABLE addressbookshare (
+CREATE TABLE IF NOT EXISTS addressbookshare (
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    addressbooksid INTEGER UNSIGNED NOT NULL  DEFAULT '0',
+    addressbooksid INT(11) UNSIGNED NOT NULL,
     principaluri VARBINARY(255),
     displayname VARCHAR(255),
     description TEXT,
     href VARBINARY(100),
     access TINYINT(1) NOT NULL DEFAULT '1' COMMENT '1 = owner, 2 = read, 3 = readwrite',
     UNIQUE(principaluri(100)),
-    UNIQUE(addressbooksid, principaluri)
+    UNIQUE(addressbooksid, principaluri),
+    CONSTRAINT fk_addressbooksid FOREIGN KEY (addressbooksid) REFERENCES addressbooks(id) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -36,6 +38,7 @@ CREATE TABLE cards (
     uri VARBINARY(200),
     lastmodified INT(11) UNSIGNED,
     etag VARBINARY(32),
+    personId mediumint(9) NOT NULL default -1 COMMENT '-1 personal cards, >1 for a real person in the CRM',
     size INT(11) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
