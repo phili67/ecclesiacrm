1e9699c1240e3a4ec8ba7f5c4a9b16bd0b62ff26
Philippe Logel <philippelogel@free.fr>
2018-12-20 22:27:24 +0100

    User code cleanup

--- src/api/routes/users.php
+++ src/api/routes/users.php
@@ -1,6 +1,9 @@
 <?php
 
 // Users APIs
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 use EcclesiaCRM\UserQuery;
 use EcclesiaCRM\User;
 use EcclesiaCRM\UserConfigQuery;
@@ -13,151 +16,161 @@ use EcclesiaCRM\Family;
 use EcclesiaCRM\Note;
 use Propel\Runtime\ActiveQuery\Criteria;
 use EcclesiaCRM\SessionUser;
+use EcclesiaCRM\Emails\UpdateAccountEmail;
 
 
 $app->group('/users', function () {
-
-    $this->post('/{userId:[0-9]+}/password/reset', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        $user = UserQuery::create()->findPk($args['userId']);
-        if (!is_null($user)) {
-            $password = $user->resetPasswordToRandom();
-            $user->save();
-            $user->createTimeLineNote("password-reset");
-            $email = new ResetPasswordEmail($user, $password);
-            if ($email->send()) {
-                return $response->withStatus(200)->withJson(['status' => "success"]);
-            } else {
-                $this->Logger->error($email->getError());
-                throw new \Exception($email->getError());
-            }
-        } else {
-            return $response->withStatus(404);
-        }
-    });
-        
-     $this->post('/applyrole', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        
-        $params = (object)$request->getParsedBody();
-          
-        if (isset ($params->userID) && isset ($params->roleID)) {
-          $user = UserQuery::create()->findPk($params->userID);
-           
-          if (!is_null($user)) {
-             $user->ApplyRole($params->roleID);
-
-             return $response->withJson(['success' => true,'userID' => $params->userID]);
-          }
-        }
-            
-        return $response->withJson(['success' => false]);
-    });
-    
-    
-    
-    $this->post('/webdavKey', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        
-        $params = (object)$request->getParsedBody();
-          
-        if (isset ($params->userID)) {
-        
-          $user = UserQuery::create()->findPk($params->userID);
-          if (!is_null($user)) {
-            return $response->withJson(['status' => "success", "token" => $user->getWebdavkey()]);
-          }
-        }
-        
-        return $response->withJson(['status' => "failed"]);
-    });
-    
-    $this->post('/lockunlock', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        
-        $params = (object)$request->getParsedBody();
-          
-        if (isset ($params->userID)) {
-        
-          $user = UserQuery::create()->findPk($params->userID);
-          
-          if (!is_null($user) && $user->getPersonId() != 1) {            
-            $newStatus = (empty($user->getIsDeactivated()) ? true : false);
-
-            //update only if the value is different
-            if ($newStatus) {
-                $user->setIsDeactivated(true);
-            } else {
-                $user->setIsDeactivated(false);
-            }
-            
-            $user->save();
-        
-            //Create a note to record the status change
-            $note = new Note();
-            $note->setPerId($user->getPersonId());
-            if ($newStatus == 'false') {
-                $note->setText(gettext('User Deactivated'));
-            } else {
-                $note->setText(gettext('User Activated'));
-            }
-            $note->setType('edit');
-            $note->setEntered(SessionUser::getUser()->getPersonId());
-            $note->save();
-
-            return $response->withJson(['success' => true]);
-          }
-        }
-        
-        return $response->withJson(['success' => false]);
-    });
-    
-
-    $this->post('/{userId:[0-9]+}/login/reset', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        $user = UserQuery::create()->findPk($args['userId']);
-        if (!is_null($user)) {
-            $user->setFailedLogins(0);
-            $user->save();
-            $user->createTimeLineNote("login-reset");
-            $email = new UnlockedEmail($user);
-            if (!$email->send()) {
-                $this->Logger->warn($email->getError());
-            }
-            return $response->withStatus(200)->withJson(['status' => "success"]);
-        } else {
-            return $response->withStatus(404);
-        }
-    });
-
-    $this->delete('/{userId:[0-9]+}', function ($request, $response, $args) {
-        if (!SessionUser::getUser()->isAdmin()) {
-            return $response->withStatus(401);
-        }
-        $user = UserQuery::create()->findPk($args['userId']);
-        if (!is_null($user)) {
-            $userConfig =  UserConfigQuery::create()->findPk($user->getId());
-            if (!is_null($userConfig)) {
-                $userConfig->delete();
-            }
-            $email = new AccountDeletedEmail($user);
-            $user->delete();
-            if (!$email->send()) {
-                $this->Logger->warn($email->getError());
-            }
-            return $response->withStatus(200)->withJson(['status' => "success"]);
-        } else {
-            return $response->withStatus(404);
-        }
-    });
+    $this->post('/{userId:[0-9]+}/password/reset', 'passwordReset');
+    $this->post('/applyrole' , 'applyRole');
+    $this->post('/webdavKey' , 'webDavKey');
+    $this->post('/lockunlock', 'lockUnlock');
+    $this->post('/{userId:[0-9]+}/login/reset', 'loginReset');
+    $this->delete('/{userId:[0-9]+}', 'deleteUser');
 });
+
+function passwordReset (Request $request, Response $response, array $args ) {
+  if (!SessionUser::getUser()->isAdmin()) {
+      return $response->withStatus(401);
+  }
+  $user = UserQuery::create()->findPk($args['userId']);
+  if (!is_null($user)) {
+      $password = $user->resetPasswordToRandom();
+      $user->save();
+      $user->createTimeLineNote("password-reset");
+      $email = new ResetPasswordEmail($user, $password);
+      if ($email->send()) {
+          return $response->withStatus(200)->withJson(['status' => "success"]);
+      } else {
+          $this->Logger->error($email->getError());
+          throw new \Exception($email->getError());
+      }
+  } else {
+      return $response->withStatus(404);
+  }
+}
+
+
+function applyRole ($request, $response, $args) {
+    if (!SessionUser::getUser()->isAdmin()) {
+        return $response->withStatus(401);
+    }
+    
+    $params = (object)$request->getParsedBody();
+      
+    if (isset ($params->userID) && isset ($params->roleID)) {
+      $user = UserQuery::create()->findPk($params->userID);
+       
+      if (!is_null($user)) {
+         $user->ApplyRole($params->roleID);
+
+         return $response->withJson(['success' => true,'userID' => $params->userID]);
+      }
+    }
+        
+    return $response->withJson(['success' => false]);
+}
+
+function webDavKey ($request, $response, $args) {
+    if (!SessionUser::getUser()->isAdmin()) {
+        return $response->withStatus(401);
+    }
+    
+    $params = (object)$request->getParsedBody();
+      
+    if (isset ($params->userID)) {
+    
+      $user = UserQuery::create()->findPk($params->userID);
+      if (!is_null($user)) {
+        return $response->withJson(['status' => "success", "token" => $user->getWebdavkey()]);
+      }
+    }
+    
+    return $response->withJson(['status' => "failed"]);
+}
+
+function lockUnlock ($request, $response, $args) {
+    if (!SessionUser::getUser()->isAdmin()) {
+        return $response->withStatus(401);
+    }
+    
+    $params = (object)$request->getParsedBody();
+      
+    if (isset ($params->userID)) {
+    
+      $user = UserQuery::create()->findPk($params->userID);
+      
+      if (!is_null($user) && $user->getPersonId() != 1) {            
+        $newStatus = (empty($user->getIsDeactivated()) ? true : false);
+
+        //update only if the value is different
+        if ($newStatus) {
+            $user->setIsDeactivated(true);
+        } else {
+            $user->setIsDeactivated(false);
+        }
+        
+        $user->save();
+        
+        // a mail is notified
+        $email = new UpdateAccountEmail($user, ($newStatus)?gettext("Deactivate account"):gettext("The same as before"));
+        $email->send();
+
+
+        //Create a note to record the status change
+        $note = new Note();
+        $note->setPerId($user->getPersonId());
+        if ($newStatus == 'false') {
+            $note->setText(gettext('User Deactivated'));
+        } else {
+            $note->setText(gettext('User Activated'));
+        }
+        $note->setType('edit');
+        $note->setEntered(SessionUser::getUser()->getPersonId());
+        $note->save();
+
+        return $response->withJson(['success' => true]);
+      }
+    }
+    
+    return $response->withJson(['success' => false]);
+}
+
+function loginReset ($request, $response, $args) {
+    if (!SessionUser::getUser()->isAdmin()) {
+        return $response->withStatus(401);
+    }
+    $user = UserQuery::create()->findPk($args['userId']);
+    if (!is_null($user)) {
+        $user->setFailedLogins(0);
+        $user->save();
+        $user->createTimeLineNote("login-reset");
+        $email = new UnlockedEmail($user);
+        if (!$email->send()) {
+            $this->Logger->warn($email->getError());
+        }
+        return $response->withStatus(200)->withJson(['status' => "success"]);
+    } else {
+        return $response->withStatus(404);
+    }
+}
+
+function deleteUser ($request, $response, $args) {
+    if (!SessionUser::getUser()->isAdmin()) {
+        return $response->withStatus(401);
+    }
+    $user = UserQuery::create()->findPk($args['userId']);
+    if (!is_null($user)) {
+        $userConfig =  UserConfigQuery::create()->findPk($user->getId());
+        if (!is_null($userConfig)) {
+            $userConfig->delete();
+        }
+        $email = new AccountDeletedEmail($user);
+        $user->delete();
+        if (!$email->send()) {
+            $this->Logger->warn($email->getError());
+        }
+        return $response->withStatus(200)->withJson(['status' => "success"]);
+    } else {
+        return $response->withStatus(404);
+    }
+}
