10ab864a1db94a741efa4810a9c4f9e313c7b33a
ecclesia <philippelogel@free.fr>
2019-01-31 21:10:47 +0100

    Update propel.php

--- propel/propel.php
+++ propel/propel.php
@@ -5,7 +5,7 @@ return [
             'connections' => [
                 'default' => [
                     'adapter'  => 'mysql',
-                    'dsn'      => 'mysql:host=localhost;port=3306;dbname=churchcrm',
+                    'dsn'      => 'mysql:host=localhost;port=3306;dbname=ecclesiacrm',
                     'user'     => 'churchcrm',
                     'password' => 'churchcrm',
                     'settings' => [
