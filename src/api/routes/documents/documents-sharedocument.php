91a30e3d59ecf4b7b662f38b5cabce76c2269794
Philippe Logel <philippelogel@free.fr>
2019-02-02 15:02:48 +0100

    The next step

--- src/api/routes/documents/documents-sharedocument.php
+++ src/api/routes/documents/documents-sharedocument.php
@@ -8,6 +8,9 @@
 *                 groups, families, etc...
 *
 ******************************************************************************/
+use Slim\Http\Request;
+use Slim\Http\Response;
+
 use EcclesiaCRM\DepositQuery;
 use EcclesiaCRM\dto\SystemURLs;
 use EcclesiaCRM\dto\SystemConfig;
@@ -25,168 +28,174 @@ use EcclesiaCRM\UserQuery;
 
 
 // Routes sharedocument
-
 $app->group('/sharedocument', function () {
   
-  $this->post('/getallperson',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    $result = [];
-    
-    if (isset ($params->noteId)) {
-          $personShareQuery = NoteShareQuery::create()
-            ->joinWithNote()
-            ->findByNoteId($params->noteId);
-            
-          foreach ($personShareQuery as $noteShare) {
-            $id = $noteShare->getSharePerId();
-            $name = PersonQuery::Create()->findOneById ($id)->getFullName();
-            
-            $person = ['id' => $id,
-                       'name' => (($noteShare->getRights() == 1)?gettext("[👀  ]"):gettext("[👀 ✐]"))."   ".$name];
-                    
-            array_push($result, $person);        
-          }
-    }
-          
-    return $response->withJson($result);                  
-  });
-  
-  $this->post('/addperson',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->personID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
-        $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
+  $this->post('/getallperson','getAllShareForPerson' );
+  $this->post('/addperson','addPersonToShare');
+  $this->post('/addfamily','addFamilyToShare' );
+  $this->post('/addgroup', 'addGroupToShare' );
+  $this->post('/deleteperson', 'deletePersonFromShare');  
+  $this->post('/setrights','setRightsForPerson');  
+  $this->post('/cleardocument','clearDocument' );
+
+});
+
+
+function getAllShareForPerson(Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
         
-        if ( empty($noteShare) && $params->currentPersonID != $params->personID) {
-          $noteShare = new NoteShare();
+  $result = [];
+  
+  if (isset ($params->noteId)) {
+        $personShareQuery = NoteShareQuery::create()
+          ->joinWithNote()
+          ->findByNoteId($params->noteId);
           
-          $noteShare->setSharePerId($params->personID);
-          $noteShare->setNoteId($params->noteId);
+        foreach ($personShareQuery as $noteShare) {
+          $id = $noteShare->getSharePerId();
+          $name = PersonQuery::Create()->findOneById ($id)->getFullName();
           
-          $noteShare->save();
+          $person = ['id' => $id,
+                     'name' => (($noteShare->getRights() == 1)?gettext("[👀  ]"):gettext("[👀 ✐]"))."   ".$name];
+                  
+          array_push($result, $person);        
+        }
+  }
+        
+  return $response->withJson($result);                  
+}
+
+function addPersonToShare (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->personID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
+      $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
+      
+      if ( empty($noteShare) && $params->currentPersonID != $params->personID) {
+        $noteShare = new NoteShare();
+        
+        $noteShare->setSharePerId($params->personID);
+        $noteShare->setNoteId($params->noteId);
+        
+        $noteShare->save();
+        
+        if (isset ($params->notification)  && $params->notification) {
+          $user = UserQuery::Create()->findOneByPersonId($params->personID);
+        
+          if ( !empty($user) ){
+            $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
+            $email->send();
+          }
+        }
+      }
+  }
+        
+  return $response->withJson(['status' => "success"]);                  
+}
+
+function addFamilyToShare (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->familyID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
+     $members = FamilyQuery::Create()->findOneById($params->familyID)->getActivatedPeople();
+     
+     foreach ($members as $member) {   
+        if ($member->getId() > 0) {   
+          $noteShare = NoteShareQuery::Create()->filterBySharePerId($member->getId())->findOneByNoteId($params->noteId);
           
-          if (isset ($params->notification)  && $params->notification) {
-            $user = UserQuery::Create()->findOneByPersonId($params->personID);
-          
-            if ( !empty($user) ){
-              $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
-              $email->send();
+          if ( empty($noteShare) && $params->currentPersonID != $member->getId() ) {             
+            $noteShare = new NoteShare();
+        
+            $noteShare->setSharePerId($member->getId());
+            $noteShare->setNoteId($params->noteId);
+        
+            $noteShare->save();
+            
+            if (isset ($params->notification)  && $params->notification) {
+              $user = UserQuery::Create()->findOneByPersonId($member->getId());
+        
+              if ( !empty($user) ){
+                $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
+                $email->send();
+              }
             }
           }
         }
-    }
+     }
+  }
+        
+  return $response->withJson(['status' => "success"]);                  
+}
+
+function addGroupToShare (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->groupID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
+     $members = GroupQuery::Create()->findOneById($params->groupID)->getPerson2group2roleP2g2rs();
+     
+     foreach ($members as $member) {   
+        if ($member->getPersonId() > 0) {   
+          $noteShare = NoteShareQuery::Create()->filterBySharePerId($member->getPersonId())->findOneByNoteId($params->noteId);
           
-    return $response->withJson(['status' => "success"]);                  
-  });
-  
-  $this->post('/addfamily',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->familyID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
-       $members = FamilyQuery::Create()->findOneById($params->familyID)->getActivatedPeople();
-       
-       foreach ($members as $member) {   
-          if ($member->getId() > 0) {   
-            $noteShare = NoteShareQuery::Create()->filterBySharePerId($member->getId())->findOneByNoteId($params->noteId);
+          if ( empty($noteShare) && $params->currentPersonID != $member->getPersonId() ) {             
+            $noteShare = new NoteShare();
+        
+            $noteShare->setSharePerId($member->getPersonId());
+            $noteShare->setNoteId($params->noteId);
+        
+            $noteShare->save();
             
-            if ( empty($noteShare) && $params->currentPersonID != $member->getId() ) {             
-              $noteShare = new NoteShare();
-          
-              $noteShare->setSharePerId($member->getId());
-              $noteShare->setNoteId($params->noteId);
-          
-              $noteShare->save();
-              
-              if (isset ($params->notification)  && $params->notification) {
-                $user = UserQuery::Create()->findOneByPersonId($member->getId());
-          
-                if ( !empty($user) ){
-                  $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
-                  $email->send();
-                }
+            if (isset ($params->notification) && $params->notification) {
+              $user = UserQuery::Create()->findOneByPersonId($member->getPersonId());
+        
+              if ( !empty($user) ){
+                $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
+                $email->send();
               }
             }
           }
-       }
-    }
-          
-    return $response->withJson(['status' => "success"]);                  
-  });
+        }
+     }
+  }
+        
+  return $response->withJson(['status' => $params->groupID]);                  
+}
 
-  $this->post('/addgroup',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->groupID) && isset ($params->noteId) && isset ($params->currentPersonID) && isset ($params->notification) ) {
-       $members = GroupQuery::Create()->findOneById($params->groupID)->getPerson2group2roleP2g2rs();
-       
-       foreach ($members as $member) {   
-          if ($member->getPersonId() > 0) {   
-            $noteShare = NoteShareQuery::Create()->filterBySharePerId($member->getPersonId())->findOneByNoteId($params->noteId);
+function deletePersonFromShare (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->personID) && isset ($params->noteId)) {
+        $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
+        
+        $noteShare->delete();
+  }
+  
+  $noteShare = NoteShareQuery::Create()->findByNoteId($params->noteId);
+        
+  return $response->withJson(['status' => "success",'count' => $noteShare->count()]);                  
+}
+
+function setRightsForPerson (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->personID) && isset ($params->noteId) && isset ($params->rightAccess) && $params->rightAccess > 0) {
+        $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
+        
+        $noteShare->setRights($params->rightAccess);
+        $noteShare->save();
+  }
             
-            if ( empty($noteShare) && $params->currentPersonID != $member->getPersonId() ) {             
-              $noteShare = new NoteShare();
-          
-              $noteShare->setSharePerId($member->getPersonId());
-              $noteShare->setNoteId($params->noteId);
-          
-              $noteShare->save();
-              
-              if (isset ($params->notification) && $params->notification) {
-                $user = UserQuery::Create()->findOneByPersonId($member->getPersonId());
-          
-                if ( !empty($user) ){
-                  $email = new DocumentEmail($user, gettext("You can visualize it in your account, in the time Line or the notes tab."));
-                  $email->send();
-                }
-              }
-            }
-          }
-       }
-    }
-          
-    return $response->withJson(['status' => $params->groupID]);                  
-  });
+  return $response->withJson(['status' => "success"]);                  
+}
 
-  $this->post('/deleteperson',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->personID) && isset ($params->noteId)) {
-          $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
-          
-          $noteShare->delete();
-    }
-    
-    $noteShare = NoteShareQuery::Create()->findByNoteId($params->noteId);
-          
-    return $response->withJson(['status' => "success",'count' => $noteShare->count()]);                  
-  });  
-  
-  $this->post('/setrights',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->personID) && isset ($params->noteId) && isset ($params->rightAccess) && $params->rightAccess > 0) {
-          $noteShare = NoteShareQuery::Create()->filterBySharePerId($params->personID)->findOneByNoteId($params->noteId);
-          
-          $noteShare->setRights($params->rightAccess);
-          $noteShare->save();
-    }
-              
-    return $response->withJson(['status' => "success"]);                  
-  });  
-
-  
-  $this->post('/cleardocument',function($request,$response,$args) {
-    $params = (object)$request->getParsedBody();
-          
-    if (isset ($params->noteId)) {
-          $noteShare = NoteShareQuery::Create()->findByNoteId($params->noteId);
-          
-          $noteShare->delete();
-    }
-          
-    return $response->withJson(['status' => "success"]);                  
-  });
-  
-
-});
+function clearDocument (Request $request, Response $response, array $args) {
+  $params = (object)$request->getParsedBody();
+        
+  if (isset ($params->noteId)) {
+        $noteShare = NoteShareQuery::Create()->findByNoteId($params->noteId);
+        
+        $noteShare->delete();
+  }
+        
+  return $response->withJson(['status' => "success"]);                  
+}
