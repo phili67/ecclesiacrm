07c31387768c500aca8f53e7da49412a4b35ee9c
ecclesia <philippelogel@free.fr>
2019-02-03 20:05:18 +0100

    Update CentOS7.sh

--- install/CentOS7.sh
+++ install/CentOS7.sh
@@ -19,6 +19,6 @@ service mysqld start
 
 cd /var/www/
 rm -rf html
-wget https://github.com/ChurchCRM/CRM/releases/download/2.4.4/ChurchCRM-2.4.4.zip
-unzip ChurchCRM-2.4.4.zip
-mv churchcrm/ html
+wget https://github.com/phili67/ecclesiacrm/releases/download/5.3.1/EcclesiaCRM-5.3.1.zip
+unzip EcclesiaCRM-5.3.1.zip
+mv ecclesiacrm/ html
