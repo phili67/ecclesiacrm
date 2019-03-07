1753b6eca7f62b2666ff72f8e3afaedb1cf5448f
Philippe Logel <philippelogel@free.fr>
2019-03-07 22:27:19 +0100

    Mailchimp upload bug resolution

--- src/skin/js/email/MailChimp/ManageList.js
+++ src/skin/js/email/MailChimp/ManageList.js
@@ -93,10 +93,10 @@ $(document).ready(function () {
           language : window.CRM.lang,
           width : '100%',
           extraPlugins : 'uploadfile,uploadimage,filebrowser',
-          uploadUrl: window.CRM.root+'/uploader/upload.php?type=privateDocuments',
-          imageUploadUrl: window.CRM.root+'/uploader/upload.php?type=privateImages',
-          filebrowserUploadUrl: window.CRM.root+'/uploader/upload.php?type=privateDocuments',
-          filebrowserBrowseUrl: window.CRM.root+'/browser/browse.php?type=privateDocuments'
+          uploadUrl: window.CRM.root+'/uploader/upload.php?type=publicDocuments',
+          imageUploadUrl: window.CRM.root+'/uploader/upload.php?type=publicImages',
+          filebrowserUploadUrl: window.CRM.root+'/uploader/upload.php?type=publicDocuments',
+          filebrowserBrowseUrl: window.CRM.root+'/browser/browse.php?type=publicDocuments'
          });
    
          add_ckeditor_buttons(editor);
