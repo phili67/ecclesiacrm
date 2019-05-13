70e985dc2cc1565e69f8e8d6b09ca6532e028ddc
phili67 <philippelogel@free.fr>
2019-05-13 10:17:50 +0200

    5.7.1 upgrade constraint DB

--- src/mysql/upgrade/5.7.1-upgrade.sql
+++ src/mysql/upgrade/5.7.1-upgrade.sql
@@ -1,1 +1,5 @@
 ALTER TABLE `person_per` ADD `per_SendNewsLetter` enum('FALSE','TRUE') NOT NULL default 'FALSE';
+
+ALTER TABLE `user_usr`
+ADD CONSTRAINT fk_usr_per_ID
+  FOREIGN KEY (usr_per_ID) REFERENCES person_per(per_ID);
