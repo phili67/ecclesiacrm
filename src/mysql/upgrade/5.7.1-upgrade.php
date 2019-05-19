7450a17e57f81df664b8015ad91cae7a868384eb
phili67 <philippelogel@free.fr>
2019-05-19 08:59:22 +0200

    5.7.1 upgrade scripts

---
+++ src/mysql/upgrade/5.7.1-upgrade.php
@@ -0,0 +1,20 @@
+<?php 
+// pour le debug on se met au bon endroit : http://192.168.151.205/mysql/upgrade/5.7.1-upgrade.php
+// et il faut décommenter
+/*define("webdav", "1");
+require '../../Include/Config.php';*/
+
+  use Propel\Runtime\Propel;
+  use EcclesiaCRM\Utils\LoggerUtils;
+  use EcclesiaCRM\dto\SystemURLs;
+
+  $logger = LoggerUtils::getAppLogger();
+  
+  $logger->info("Start to delete : all unusefull files");
+
+  unlink(SystemURLs::getDocumentRoot()."/Include/CanvassUtilities.php");
+  unlink(SystemURLs::getDocumentRoot()."/Include/ReportFunctions.php");
+  unlink(SystemURLs::getDocumentRoot()."/Include/EnvelopeFunctions.php");
+  
+  $logger->info("End of delete :  all unusefull files");
+?>
