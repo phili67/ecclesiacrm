b6c1f990449d5291eeb30223c37824371450124e
phili67 <philippelogel@free.fr>
2019-06-23 22:35:20 +0200

    GroupList v2

---
+++ src/v2/routes/group/groups.php
@@ -0,0 +1,39 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+use EcclesiaCRM\ListOptionQuery;
+use EcclesiaCRM\dto\SystemURLs;
+
+use Slim\Views\PhpRenderer;
+
+$app->group('/group', function () {
+    $this->get('/list', 'groupList');
+});
+
+
+function groupList (Request $request, Response $response, array $args) {
+    $renderer = new PhpRenderer('templates/group/');
+
+    return $renderer->render($response, 'grouplist.php', renderMapArray());
+}
+
+function renderGroupListArray ()
+{
+    $rsGroupTypes = ListOptionQuery::create()->filterById('3')->find();
+
+    //Set the page title
+    $sPageTitle = _("Group Listing");
+
+    $sRootDocument  = SystemURLs::getDocumentRoot();
+    $CSPNonce = SystemURLs::getCSPNonce();
+
+    $paramsArguments = ['sRootPath'    => SystemURLs::getRootPath(),
+        'sRootDocument' => $sRootDocument,
+        'sPageTitle'    => $sPageTitle,
+        'CSPNonce'      => $CSPNonce,
+        'rsGroupTypes'  => $rsGroupTypes];
+
+    return $paramsArguments;
+}
---
+++ src/v2/templates/group/grouplist.php
@@ -0,0 +1,85 @@
+<?php
+
+/*******************************************************************************
+ *
+ *  filename    : grouplist.php
+ *  last change : 2019-06-23
+ *  description : manage the group list
+ *
+ *  http://www.ecclesiacrm.com/
+ *
+ *  This code is under copyright not under MIT Licence
+ *  copyright   : 2019 Philippe Logel all right reserved not MIT licence
+ *                This code can't be incoprorated in another software without authorizaion
+ *
+ ******************************************************************************/
+
+use EcclesiaCRM\SessionUser;
+
+require $sRootDocument . '/Include/Header.php';
+?>
+
+<div class="row">
+    <div class="col-lg-3">
+        <label>
+            <?= _("Show type of group:") ?>
+        </label>
+    </div>
+    <div class="col-lg-3">
+        <select id="table-filter" class="form-control input-sm">
+            <option value=""><?= _("All") ?></option>
+            <option><?= _("Unassigned") ?></option>
+            <?php
+            foreach ($rsGroupTypes as $groupType) {
+                ?>
+                <option><?= $groupType->getOptionName() ?></option>
+                <?php
+            }
+            ?>
+        </select>
+    </div>
+    <div class="col-lg-5" align="right">
+        <label>
+            <?= _("Groups count:") ?>
+        </label>
+        <span id="numberOfGroups"></span>
+    </div>
+</div>
+
+<br>
+
+<div class="box box-body">
+    <table class="table table-striped table-bordered data-table" id="groupsTable" style="width:100%">
+    </table>
+    <?php
+    if (SessionUser::getUser()->isManageGroupsEnabled()) {
+        ?>
+        <br>
+        <form action="#" method="get" class="form">
+            <label for="addNewGroup"><?= _('Add New Group') ?> :</label>
+            <input class="form-control newGroup" name="groupName" id="groupName" style="width:100%">
+            <br>
+            <button type="button" class="btn btn-primary" id="addNewGroup"><?= _('Add New Group') ?></button>
+        </form>
+        <?php
+    }
+    ?>
+
+</div>
+
+<script src="<?= $sRootPath ?>/skin/js/group/GroupList.js"></script>
+<script nonce="<?= $CSPNonce ?>">
+    $( document).ready(function() {
+        var gS = localStorage.getItem("groupSelect");
+        if (gS != null)
+        {
+            tf = document.getElementById("table-filter");
+            tf.selectedIndex = gS;
+
+            window.groupSelect = tf.value;
+        }
+    });
+
+</script>
+
+<?php require $sRootDocument . '/Include/Footer.php'; ?>
