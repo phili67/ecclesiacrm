b02e3da591b384b9c578f64a75037d15e6f42038
phili67 <philippelogel@free.fr>
2019-07-09 08:44:49 +0200

    5.6.1 upgrade script

--- src/mysql/upgrade/5.6.1-upgrade.php
+++ src/mysql/upgrade/5.6.1-upgrade.php
@@ -1,5 +1,5 @@
 <?php 
-// pour le debug on se met au bon endroit : http://192.168.151.205/mysql/upgrade/5.6.1-upgrade.php
+// pour le debug on se met au bon endroit : http://192.168.151.207/mysql/upgrade/5.6.1-upgrade.php
 // et il faut décommenter
 /*define("webdav", "1");
 require '../../Include/Config.php';*/
@@ -7,14 +7,10 @@ require '../../Include/Config.php';*/
   use Propel\Runtime\Propel;
   use EcclesiaCRM\Utils\LoggerUtils;
   use EcclesiaCRM\dto\SystemURLs;
-  use EcclesiaCRM\GroupQuery;
-  use EcclesiaCRM\Person2group2roleP2g2rQuery;
   
   use Sabre\CardDAV;
   use Sabre\DAV;
   use EcclesiaCRM\MyPDO\CardDavPDO;
-
-
 
   $logger = LoggerUtils::getAppLogger();
   
@@ -26,11 +22,16 @@ require '../../Include/Config.php';*/
 
   $logger->info("Start to delete : start group add to addressbook");
   
-  $groups = GroupQuery::create()->find();
+  $sSQL = "SELECT `grp_ID`, `grp_Name` FROM `group_grp` WHERE 1";
+  
+  $statement = $pdo->prepare($sSQL);
+  $statement->execute();
+
+  $groups = $statement->fetchAll(PDO::FETCH_ASSOC);// permet de récupérer le tableau associatif
   
   foreach ($groups as $group) {
      // first we add the adress book
-      $addressbook = $carddavBackend->getAddressBookForGroup ($group->getId());
+      $addressbook = $carddavBackend->getAddressBookForGroup ($group['grp_ID']);
       
       if ($addressbook['id'] != 0) continue;
       
@@ -40,52 +41,61 @@ require '../../Include/Config.php';*/
             'principals/admin',
             \Sabre\DAV\UUIDUtil::getUUID(),
             [
-                '{DAV:}displayname'                                       => $group->getName(),
+                '{DAV:}displayname'                                       => $group['grp_Name'],
                 '{urn:ietf:params:xml:ns:carddav}addressbook-description' => 'AddressBook description',
             ],
-            $group->getId()
+            $group['grp_ID']
       );
       
-      $members = EcclesiaCRM\Person2group2roleP2g2rQuery::create()
-            ->joinWithPerson()
-            ->usePersonQuery()
-              ->filterByDateDeactivated(null)// RGPD, when a person is completely deactivated
-            ->endUse()
-            ->findByGroupId($group->getId());
+      $sSQL = "SELECT per.per_FirstName as perFirstName, per.per_MiddleName as perMiddleName, per.per_LastName as perLastName, 
+      per.per_Address1  as perAddress1, per.per_City  as perCity, per.per_State  as perState, 
+      per.per_Zip as perZip, per.per_Country as perCountry, per.per_HomePhone as perHomePhone, 
+      per.per_WorkPhone as perWorkPhone, per.per_CellPhone  as perCellPhone, 
+      per.per_Email as perEmail, per.per_WorkEmail  as perWorkEmail, 
+      per.per_fam_ID as perFamID, fam.fam_Address1 as famAddress1, 
+      fam.fam_City as famCity, fam.fam_Zip as famZip 
+      FROM person2group2role_p2g2r as p2g2r
+      INNER JOIN person_per as per ON (p2g2r.p2g2r_per_ID=per.per_ID)
+      LEFT JOIN family_fam as fam ON (fam.fam_ID=per.per_fam_ID) 
+      WHERE per.per_DateDeactivated IS NULL 
+      AND p2g2r.p2g2r_grp_ID=".$group['grp_ID'];
+      
+      $statement = $pdo->prepare($sSQL);
+      $statement->execute();
+      
+      $members = $statement->fetchAll(PDO::FETCH_ASSOC);// permet de récupérer le tableau associatif
             
-      foreach ($members as $member) {
-        $person = $member->getPerson();
-        
+      foreach ($members as $person) {        
         // now we'll create all the cards
         $card = 'BEGIN:VCARD
 VERSION:3.0
 PRODID:-//Apple Inc.//Mac OS X 10.12.6//EN
-N:'.$person->getLastName().';'.$person->getFirstName().';'.$person->getMiddleName().';;
-FN:'.$person->getFirstName().' '.$person->getLastName();
+N:'.$person['perLastName'].';'.$person['perFirstName'].';'.$person['perMiddleName'].';;
+FN:'.$person['perFirstName'].' '.$person['perLastName'];
 
-        if ( !empty($person->getWorkEmail()) ) {
-          $card .="\nEMAIL;type=INTERNET;type=WORK;type=pref:".$person->getWorkEmail();
+        if ( !empty($person['perWorkEmail']) ) {
+          $card .="\nEMAIL;type=INTERNET;type=WORK;type=pref:".$person['perWorkEmail'];
         }
-        if ( !empty($person->getEmail()) ) {
-          $card .="\nEMAIL;type=INTERNET;type=HOME;type=pref:".$person->getEmail();
-        }
-
-        if ( !empty($person->getHomePhone()) ) {
-          $card .="\nTEL;type=HOME;type=VOICE;type=pref:".$person->getHomePhone();
+        if ( !empty($person['perEmail']) ) {
+          $card .="\nEMAIL;type=INTERNET;type=HOME;type=pref:".$person['perEmail'];
         }
 
-        if ( !empty($person->getCellPhone()) ) {
-          $card .="\nTEL;type=CELL;type=VOICE:".$person->getCellPhone();
+        if ( !empty($person['perHomePhone']) ) {
+          $card .="\nTEL;type=HOME;type=VOICE;type=pref:".$person['perHomePhone'];
         }
 
-        if ( !empty($person->getWorkPhone()) ) {
-          $card .="\nTEL;type=WORK;type=VOICE:".$person->getWorkPhone();
+        if ( !empty($person['perCellPhone']) ) {
+          $card .="\nTEL;type=CELL;type=VOICE:".$person['perCellPhone'];
         }
 
-        if ( !empty($person->getAddress1()) || !empty($person->getCity()) || !empty($person->getZip()) ) {
-          $card .="\nitem1.ADR;type=HOME;type=pref:;;".$person->getAddress1().';'.$person->getCity().';;'.$person->getZip();
-        } else if (!is_null ($person->getFamily())) {
-          $card .="\nitem1.ADR;type=HOME;type=pref:;;".$person->getFamily()->getAddress1().';'.$person->getFamily()->getCity().';;'.$person->getFamily()->getZip();        
+        if ( !empty($person['perWorkPhone']) ) {
+          $card .="\nTEL;type=WORK;type=VOICE:".$person['perWorkPhone'];
+        }
+
+        if ( !empty($person['perAddress1']) || !empty($person['perCity']) || !empty($person['perZip']) ) {
+          $card .="\nitem1.ADR;type=HOME;type=pref:;;".$person['perAddress1'].';'.$person['perCity'].';;'.$person['perZip'];
+        } else if (!is_null ($person['perFamID'])) {
+          $card .="\nitem1.ADR;type=HOME;type=pref:;;".$person['famAddress1'].';'.$person['famCity'].';;'.$person['famZip'];
         }
 
         $card .= "\nitem1.X-ABADR:fr
