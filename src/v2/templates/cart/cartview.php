1cea9a6ecbfab7aafdd79a371b65045c3a871bc1
phili67 <philippelogel@free.fr>
2019-12-25 23:21:26 +0100

    v2 code

--- src/v2/templates/cart/cartview.php
+++ src/v2/templates/cart/cartview.php
@@ -1,110 +1,28 @@
 <?php
+
 /*******************************************************************************
  *
- *  filename    : CartView.php
- *  website     : http://www.ecclesiacrm.com
+ *  filename    : templates/backup.php
+ *  last change : 2019-11-21
+ *  description : manage the backup
  *
- *  Copyright 2001-2003 Phillip Hullquist, Deane Barker, Chris Gebhardt
- *  Copyright 2019 Philippe Logel
+ *  http://www.ecclesiacrm.com/
+ *
+ *  This code is under copyright not under MIT Licence
+ *  copyright   : 2018 Philippe Logel all right reserved not MIT licence
+ *                This code can't be incorporated in another software authorization
  *
  ******************************************************************************/
 
-require 'Include/Config.php';
-require 'Include/Functions.php';
-
-use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\dto\Cart;
-use EcclesiaCRM\dto\SystemConfig;
-use EcclesiaCRM\Utils\MiscUtils;
 use EcclesiaCRM\Utils\LabelUtils;
-use EcclesiaCRM\Utils\RedirectUtils;
 use EcclesiaCRM\SessionUser;
 
-use Propel\Runtime\Propel;
-
-$connection = Propel::getConnection();
-
-// Set the page title and include HTML header
-$sPageTitle = _('View Your Cart');
-require 'Include/Header.php';
-
-
-if (!SessionUser::getUser()->isShowCartEnabled() || !Cart::HasPeople()) {
-    RedirectUtils::Redirect('Menu.php');
-    exit;
-}
-
-// Confirmation message that people where added to Event from Cart
-$iNumPersons = Cart::CountPeople();
-$iNumFamilies = Cart::CountFamilies();
-
-// Email Cart links
-// Note: This will email entire group, even if a specific role is currently selected.
-$sSQL = "SELECT per_Email, fam_Email
-                        FROM person_per
-                        LEFT JOIN person2group2role_p2g2r ON per_ID = p2g2r_per_ID
-                        LEFT JOIN group_grp ON grp_ID = p2g2r_grp_ID
-                        LEFT JOIN family_fam ON per_fam_ID = family_fam.fam_ID
-                    WHERE per_ID NOT IN (SELECT per_ID FROM person_per INNER JOIN record2property_r2p ON r2p_record_ID = per_ID INNER JOIN property_pro ON r2p_pro_ID = pro_ID AND pro_Name = 'Do Not Email') AND per_ID IN (" . Cart::ConvertCartToString($_SESSION['aPeopleCart']) . ')';
-
-$statementEmails = $connection->prepare($sSQL);
-$statementEmails->execute();
-
-$sEmailLink = '';
-while ($row = $statementEmails->fetch(\PDO::FETCH_BOTH)) {
-    $sEmail = MiscUtils::SelectWhichInfo($row['per_Email'], $row['fam_Email'], false);
-    if ($sEmail) {
-        /* if ($sEmailLink) // Don't put delimiter before first email
-            $sEmailLink .= SessionUser::getUser()->MailtoDelimiter(); */
-        // Add email only if email address is not already in string
-        if (!stristr($sEmailLink, $sEmail)) {
-            $sEmailLink .= $sEmail .= SessionUser::getUser()->MailtoDelimiter();
-        }
-    }
-}
-
-$sEmailLink = mb_substr($sEmailLink, 0, -1);
-
-if ($sEmailLink) {
-    // Add default email if default email has been set and is not already in string
-    if (SystemConfig::getValue('sToEmailAddress') != '' && !stristr($sEmailLink, SystemConfig::getValue('sToEmailAddress'))) {
-        $sEmailLink .= SessionUser::getUser()->MailtoDelimiter() . SystemConfig::getValue('sToEmailAddress');
-    }
-
-    $sEmailLink = urlencode($sEmailLink);  // Mailto should comply with RFC 2368
-}
-
-//Text Cart Link
-$sSQL = "SELECT per_CellPhone, fam_CellPhone
-                            FROM person_per LEFT
-                            JOIN family_fam ON person_per.per_fam_ID = family_fam.fam_ID
-                        WHERE per_ID NOT IN (SELECT per_ID FROM person_per INNER JOIN record2property_r2p ON r2p_record_ID = per_ID INNER JOIN property_pro ON r2p_pro_ID = pro_ID AND pro_Name = 'Do Not SMS') AND per_ID IN (" . Cart::ConvertCartToString($_SESSION['aPeopleCart']) . ')';
-
-$statement = $connection->prepare($sSQL);
-$statement->execute();
-
-$sPhoneLink = '';
-$sPhoneLinkSMS = '';
-$sCommaDelimiter = ', ';
-
-while ($row = $statement->fetch(\PDO::FETCH_BOTH)) {
-    $sPhone = MiscUtils::SelectWhichInfo($row['per_CellPhone'], $row['fam_CellPhone'], false);
-    if ($sPhone) {
-        /* if ($sPhoneLink) // Don't put delimiter before first phone
-            $sPhoneLink .= $sCommaDelimiter;  */
-        // Add phone only if phone is not already in string
-        if (!stristr($sPhoneLink, $sPhone)) {
-            $sPhoneLink .= $sPhone . $sCommaDelimiter;
-            $sPhoneLinkSMS .= $sPhone . $sCommaDelimiter;
-        }
-    }
-}
-
+require $sRootDocument . '/Include/Header.php';
 ?>
 
 <!-- BEGIN CART FUNCTIONS -->
 <?php
-if (Cart::CountPeople() > 0) {
+if ($iNumPersons > 0) {
     ?>
     <div class="box">
         <div class="box-header with-border">
@@ -121,7 +39,7 @@ if (Cart::CountPeople() > 0) {
             }
             if (SessionUser::getUser()->isAddRecordsEnabled()) {
                 ?>
-                <a href="<?= SystemURLs::getRootPath() ?>/CartToFamily.php" class="btn btn-app"><i
+                <a href="<?= $sRootPath ?>/CartToFamily.php" class="btn btn-app"><i
                         class="fa fa-users"></i><?= _('Empty Cart to Family') ?></a>
                 <?php
             } ?>
@@ -131,21 +49,21 @@ if (Cart::CountPeople() > 0) {
             <?php
             if (SessionUser::getUser()->isShowMapEnabled()) {
                 ?>
-                <a href="<?= SystemURLs::getRootPath() ?>/v2/map/0" class="btn btn-app"><i
+                <a href="<?= $sRootPath ?>/v2/map/0" class="btn btn-app"><i
                         class="fa fa-map-marker"></i><?= _('Map Cart') ?></a>
                 <?php
             }
             ?>
             <?php if (SessionUser::getUser()->isCSVExportEnabled()) {
                 ?>
-                <a href="<?= SystemURLs::getRootPath() ?>/CSVExport.php?Source=cart" class="btn btn-app bg-green"><i
+                <a href="<?= $sRootPath ?>/CSVExport.php?Source=cart" class="btn btn-app bg-green"><i
                         class="fa fa-file-excel-o"></i><?= _('CSV Export') ?></a>
                 <?php
             } ?>
-            <a href="<?= SystemURLs::getRootPath() ?>/Reports/NameTags.php?labeltype=74536&labelfont=times&labelfontsize=36"
+            <a href="<?= $sRootPath ?>/Reports/NameTags.php?labeltype=74536&labelfont=times&labelfontsize=36"
                class="btn btn-app bg-aqua"><i
                     class="fa fa-file-pdf-o"></i><?= _('Name Tags') ?></a>
-            <a class="btn btn-app bg-purple" href="<?= SystemURLs::getRootPath() ?>/CartToBadge.php"> <i
+            <a class="btn btn-app bg-purple" href="<?= $sRootPath ?>/CartToBadge.php"> <i
                     class="fa fa-file-picture-o"></i> <span class="cartActionDescription"><?= _("Badges") ?></span></a>
             <?php
 
@@ -181,7 +99,7 @@ if (Cart::CountPeople() > 0) {
                     <?php
                 }
             } ?>
-            <a href="<?= SystemURLs::getRootPath() ?>/DirectoryReports.php?cartdir=Cart+Directory"
+            <a href="<?= $sRootPath ?>/DirectoryReports.php?cartdir=Cart+Directory"
                class="btn btn-app"><i
                     class="fa fa-book"></i><?= _('Create Directory From Cart') ?></a>
 
@@ -204,7 +122,7 @@ if (Cart::CountPeople() > 0) {
         <div class="box-header with-border">
             <h3 class="box-title"><?= _('Generate Labels') ?></h3>
         </div>
-        <form method="get" action="<?= SystemURLs::getRootPath() ?>/Reports/PDFLabel.php" name="labelform">
+        <form method="get" action="<?= $sRootPath ?>/Reports/PDFLabel.php" name="labelform">
             <div class="box-body">
                 <?php
                 LabelUtils::LabelGroupSelect('groupbymode');
@@ -277,13 +195,11 @@ if (Cart::CountPeople() > 0) {
 <?php endif; ?>
 <!-- END CART LISTING -->
 
-<script src="<?= SystemURLs::getRootPath() ?>/skin/js/CartView.js"></script>
+<script src="<?= $sRootPath ?>/skin/js/CartView.js"></script>
 
-<script nonce="<?= SystemURLs::getCSPNonce() ?>">
+<script nonce="<?= $CSPNonce ?>">
     window.CRM.sEmailLink = "<?= mb_substr($sEmailLink, 0, -2) ?>";
     window.CRM.sPhoneLink = "<?= mb_substr($sPhoneLink, 0, -2) ?>";
 </script>
 
-<?php
-require 'Include/Footer.php';
-?>
+<?php require $sRootDocument . '/Include/Footer.php'; ?>
