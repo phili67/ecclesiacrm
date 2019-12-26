60076fb62417be2db6164aea8e299ee9eb4cafbd
phili67 <philippelogel@free.fr>
2019-12-26 09:34:41 +0100

    mise à jour

--- src/v2/routes/cart/cart.php
+++ src/v2/routes/cart/cart.php
@@ -2,14 +2,14 @@
 
 /*******************************************************************************
  *
- *  filename    : route/backup.php
- *  last change : 2019-11-21
- *  description : manage the backup
+ *  filename    : route/cartview.php
+ *  last change : 2019-12-26
+ *  description : manage the cartview
  *
  *  http://www.ecclesiacrm.com/
  *
  *  This code is under copyright not under MIT Licence
- *  copyright   : 2018 Philippe Logel all right reserved not MIT licence
+ *  copyright   : 2019 Philippe Logel all right reserved not MIT licence
  *                This code can't be incorporated in another software authorization
  *
  ******************************************************************************/
