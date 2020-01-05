753ca3851cadace30d3de058a4dcc7589e24536e
phili67 <philippelogel@free.fr>
2020-01-05 21:03:31 +0100

    pastoralcarefamily update

--- src/v2/templates/people/pastoralcarefamily.php
+++ src/v2/templates/people/pastoralcarefamily.php
@@ -149,6 +149,7 @@ require $sRootDocument . '/Include/Header.php';
 <script nonce="<?= $sCSPNonce ?>">
   var currentFamilyID = <?= $currentFamilyID ?>;
   var currentPastorId = <?= $currentPastorId ?>;
+  var sPageTitle      = '<?= $sPageTitle ?>';
 </script>
 
 <script src="<?= $sRootPath ?>/skin/js/people/PastoralCareFamily.js"></script>
