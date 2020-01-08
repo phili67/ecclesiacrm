992df46639db38446f7d1d89163f26f586104175
phili67 <philippelogel@free.fr>
2020-01-08 08:11:32 +0100

    New group search way

---
+++ src/EcclesiaCRM/Search/AddressSearchRes.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace EcclesiaCRM\Search;
+
+use EcclesiaCRM\Search\BaseSearchRes;
+use EcclesiaCRM\FamilyQuery;
+use EcclesiaCRM\dto\SystemConfig;
+use EcclesiaCRM\Utils\LoggerUtils;
+use Propel\Runtime\ActiveQuery\Criteria;
+
+
+class AddressSearchRes extends BaseSearchRes
+{
+    public function __construct()
+    {
+        $this->name = _('Address');
+        parent::__construct();
+    }
+
+    public function buildSearch(string $qry)
+    {
+        if (SystemConfig::getBooleanValue("bSearchIncludeAddresses")) {
+            try {
+                $searchLikeString = '%'.$qry.'%';
+                $addresses = FamilyQuery::create();
+
+                if (SystemConfig::getBooleanValue('bGDPR')) {
+                    $addresses->filterByDateDeactivated(null);// GDPR, when a family is completely deactivated
+                }
+
+
+                $addresses->filterByCity($searchLikeString, Criteria::LIKE)->
+                _or()->filterByAddress1($searchLikeString, Criteria::LIKE)->
+                _or()->filterByAddress2($searchLikeString, Criteria::LIKE)->
+                _or()->filterByZip($searchLikeString, Criteria::LIKE)->
+                _or()->filterByState($searchLikeString, Criteria::LIKE)->
+                limit(SystemConfig::getValue("iSearchIncludeAddressesMax"))->find();
+
+                if (!is_null($addresses))
+                {
+                    $id=1;
+
+                    foreach ($addresses as $address) {
+                        $elt = ['id' => 'address-id-'.$id++,
+                            'text'=>$address->getFamilyString(SystemConfig::getBooleanValue("bSearchIncludeFamilyHOH")),
+                            'uri'=>$address->getViewURI()
+                        ];
+
+                        array_push($this->results, $elt);
+                    }
+                }
+            } catch (Exception $e) {
+                LoggerUtils::getAppLogger()->warn($e->getMessage());
+            }
+        }
+    }
+}
