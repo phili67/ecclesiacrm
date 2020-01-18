7e6619d532773f8b268738a97b81cd8ce9838e6f
phili67 <philippelogel@free.fr>
2020-01-18 18:24:40 +0100

    mklocalisation.sh upgrade

--- mklocalisation.sh
+++ mklocalisation.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-./locale/update-locale.sh 
+./locale/update-locale.sh
 
 grunt genLocaleJSFiles
+
+# now we have to change all the defineLocale to updateLocale
+sed -i 's/defineLocale/updateLocale/g' src/locale/js/*.js
