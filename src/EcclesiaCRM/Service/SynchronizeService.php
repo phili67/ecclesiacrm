4fd9f593fa0279ef25364bf1148f906f09c55270
phili67 <philippelogel@free.fr>
2020-01-24 21:52:03 +0100

    SynchronizeService.php introduction

---
+++ src/EcclesiaCRM/Service/SynchronizeService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace EcclesiaCRM\Service;
+
+class SynchronizeService
+{
+
+  public static function getDashboardItems($PageName) {
+     $DashboardItems = array (
+       "EcclesiaCRM\Synchronize\EventsDashboardItem",
+       "EcclesiaCRM\Synchronize\ClassificationDashboardItem",
+       "EcclesiaCRM\Synchronize\FamilyDashboardItem",
+       "EcclesiaCRM\Synchronize\GroupsDashboardItem",
+       "EcclesiaCRM\Synchronize\PersonDashboardItem",
+       "EcclesiaCRM\Synchronize\PersonDemographicDashboardItem",
+       "EcclesiaCRM\Synchronize\MailchimpDashboardItem",
+       "EcclesiaCRM\Synchronize\SundaySchoolDashboardItem",
+       "EcclesiaCRM\Synchronize\CalendarPageItem",
+       "EcclesiaCRM\Synchronize\EDrivePageItem",
+       "EcclesiaCRM\Synchronize\AttendeesPageItem"
+    );
+    $ReturnValues = array ();
+    Foreach ($DashboardItems as $DashboardItem) {
+      if ($DashboardItem::shouldInclude($PageName)){
+        array_push($ReturnValues, $DashboardItem);
+      }
+    }
+    return $ReturnValues;
+
+  }
+  public static function getValues($PageName) {
+    $ReturnValues = array ();
+    Foreach (self::getDashboardItems($PageName) as $DashboardItem) {
+        $ReturnValues[$DashboardItem::getDashboardItemName()] = $DashboardItem::getDashboardItemValue();
+    }
+    return $ReturnValues;
+  }
+
+}
