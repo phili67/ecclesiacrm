4de3877ad188525b7bf8590d14b3476a351bdf21
phili67 <philippelogel@free.fr>
2020-03-06 19:09:47 +0100

    EcclesiaCRM/Search/PersonCustomSearchRes.php upgrade

--- src/EcclesiaCRM/Search/PersonCustomSearchRes.php
+++ src/EcclesiaCRM/Search/PersonCustomSearchRes.php
@@ -2,20 +2,23 @@
 
 namespace EcclesiaCRM\Search;
 
+use EcclesiaCRM\dto\Cart;
+use EcclesiaCRM\dto\SystemURLs;
 use EcclesiaCRM\Search\BaseSearchRes;
 use EcclesiaCRM\PersonCustomQuery;
 use EcclesiaCRM\PersonCustomMasterQuery;
 
 use EcclesiaCRM\dto\SystemConfig;
 use EcclesiaCRM\Utils\LoggerUtils;
+use EcclesiaCRM\Utils\MiscUtils;
 
 
 class PersonCustomSearchRes extends BaseSearchRes
 {
-    public function __construct()
+    public function __construct($global = false)
     {
         $this->name = _('Person Custom Search');
-        parent::__construct();
+        parent::__construct($global, "Person Custom Search");
     }
 
     public function buildSearch(string $qry)
@@ -43,8 +46,10 @@ class PersonCustomSearchRes extends BaseSearchRes
                     $personsCustom->_or();
                 }
 
-                $personsCustom->limit(SystemConfig::getValue("iSearchIncludePersonsMax"))
-                    ->find();
+                if (!$this->global_search) {
+                    $personsCustom->limit(SystemConfig::getValue("iSearchIncludePersonsMax"))
+                        ->find();
+                }
 
                 if (!is_null($personsCustom))
                 {
@@ -55,6 +60,29 @@ class PersonCustomSearchRes extends BaseSearchRes
                             'text' => $per->getPerson()->getFullName(),
                             'uri' => $per->getPerson()->getViewURI()
                         ];
+
+
+
+                        if ($this->global_search) {
+                            $fam = $per->getPerson()->getFamily();
+
+                            $address = "";
+                            if (!is_null($fam)) {
+                                $address = '<a href="'.SystemURLs::getRootPath().'/FamilyView.php?FamilyID='.$fam->getID().'">'.
+                                    $fam->getName().MiscUtils::FormatAddressLine($per->getPerson()->getFamily()->getAddress1(), $per->getPerson()->getFamily()->getCity(), $per->getPerson()->getFamily()->getState()).
+                                    "</a>";
+                            }
+
+                            $elt["id"] = $per->getPerson()->getId();
+                            $elt["address"] = $address;
+                            $elt["type"] = _($this->getGlobalSearchType());
+                            $elt["realType"] = $this->getGlobalSearchType();
+                            $elt["Gender"] = "";
+                            $elt["Classification"] = "";
+                            $elt["ProNames"] = "";
+                            $elt["FamilyRole"] = "";
+                            $elt["inCart"] = Cart::PersonInCart($per->getPerson()->getId());
+                        }
 
                         array_push($this->results, $elt);
                     }
