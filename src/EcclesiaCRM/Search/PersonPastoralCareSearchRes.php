b35f0476b57eed7e0f2355d91f2445b5c64cded8
phili67 <philippelogel@free.fr>
2020-03-06 19:10:15 +0100

    EcclesiaCRM/Search/PersonPastoralCareSearchRes.php upgrade

--- src/EcclesiaCRM/Search/PersonPastoralCareSearchRes.php
+++ src/EcclesiaCRM/Search/PersonPastoralCareSearchRes.php
@@ -2,10 +2,12 @@
 
 namespace EcclesiaCRM\Search;
 
+use EcclesiaCRM\dto\Cart;
 use EcclesiaCRM\Search\BaseSearchRes;
 use EcclesiaCRM\dto\SystemConfig;
 use EcclesiaCRM\Utils\LoggerUtils;
 use EcclesiaCRM\PastoralCareQuery;
+use EcclesiaCRM\Utils\MiscUtils;
 use Propel\Runtime\ActiveQuery\Criteria;
 use EcclesiaCRM\SessionUser;
 use EcclesiaCRM\dto\SystemURLs;
@@ -13,10 +15,10 @@ use EcclesiaCRM\dto\SystemURLs;
 
 class PersonPastoralCareSearchRes extends BaseSearchRes
 {
-    public function __construct()
+    public function __construct($global = false)
     {
         $this->name = _('Individual Pastoral Care');
-        parent::__construct();
+        parent::__construct($global,' Individual Pastoral Care');
     }
 
     public function buildSearch(string $qry)
@@ -48,8 +50,11 @@ class PersonPastoralCareSearchRes extends BaseSearchRes
                     ->_or()
                     ->filterByFirstName($searchLikeString, Criteria::LIKE)
                     ->endUse()
-                    ->orderByDate(Criteria::DESC)
-                    ->limit(SystemConfig::getValue("iSearchIncludePastoralCareMax"));
+                    ->orderByDate(Criteria::DESC);
+
+                if (!$this->global_search) {
+                    $cares->limit(SystemConfig::getValue("iSearchIncludePastoralCareMax"));
+                }
 
 
                 if (SessionUser::getUser()->isAdmin()) {
@@ -65,6 +70,28 @@ class PersonPastoralCareSearchRes extends BaseSearchRes
                         $elt = ['id' => "person-pastoralcare-id-".$id++,
                             'text' => $care->getPastoralCareType()->getTitle() . " : " . $care->getPersonRelatedByPersonId()->getFullName(),
                             'uri' => SystemURLs::getRootPath() . "/v2/pastoralcare/person/" . $care->getPersonId()];
+
+                        if ($this->global_search) {
+                            $per = $care->getPersonRelatedByPersonId();
+                            $fam = $per->getFamily();
+
+                            $address = "";
+                            if (!is_null($fam)) {
+                                $address = '<a href="'.SystemURLs::getRootPath().'/FamilyView.php?FamilyID='.$fam->getID().'">'.
+                                    $fam->getName().MiscUtils::FormatAddressLine($per->getFamily()->getAddress1(), $per->getFamily()->getCity(), $per->getFamily()->getState()).
+                                    "</a>";
+                            }
+
+                            $elt["id"] = $per->getId();
+                            $elt["address"] = $address;
+                            $elt["type"] = _($this->getGlobalSearchType());
+                            $elt["realType"] = $this->getGlobalSearchType();
+                            $elt["Gender"] = "";
+                            $elt["Classification"] = "";
+                            $elt["ProNames"] = "";
+                            $elt["FamilyRole"] = "";
+                            $elt["inCart"] = Cart::PersonInCart($per->getId());
+                        }
 
                         array_push($this->results, $elt);
                     }
