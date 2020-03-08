42700ba1e2723c575c148be8a30fa1a966b6391d
phili67 <philippelogel@free.fr>
2020-03-08 17:20:31 +0100

    Now we can search in global for custom family fields

--- src/EcclesiaCRM/Search/FamilyCustomSearchRes.php
+++ src/EcclesiaCRM/Search/FamilyCustomSearchRes.php
@@ -2,6 +2,9 @@
 
 namespace EcclesiaCRM\Search;
 
+use EcclesiaCRM\dto\Cart;
+use EcclesiaCRM\dto\SystemURLs;
+use EcclesiaCRM\Person2group2roleP2g2rQuery;
 use EcclesiaCRM\Search\BaseSearchRes;
 use EcclesiaCRM\Base\FamilyCustomMasterQuery;
 use EcclesiaCRM\Base\FamilyCustomQuery;
@@ -12,10 +15,10 @@ use EcclesiaCRM\Utils\LoggerUtils;
 
 class FamilyCustomSearchRes extends BaseSearchRes
 {
-    public function __construct()
+    public function __construct($global = false)
     {
         $this->name = _('Family Custom Search');
-        parent::__construct();
+        parent::__construct($global, "Family Custom Search");
     }
 
     public function buildSearch(string $qry)
@@ -43,9 +46,11 @@ class FamilyCustomSearchRes extends BaseSearchRes
                     $familiesCustom->_or();
                 }
 
-                $familiesCustom
-                    ->limit(SystemConfig::getValue("iSearchIncludeFamiliesMax"))
-                    ->find();
+                if ($this->global_search) {
+                    $familiesCustom->limit(SystemConfig::getValue("iSearchIncludeFamiliesMax"));
+                }
+
+                $familiesCustom->find();
 
 
                 if (!is_null($familiesCustom))
@@ -57,6 +62,29 @@ class FamilyCustomSearchRes extends BaseSearchRes
                             "text" => $fam->getFamily()->getFamilyString(SystemConfig::getBooleanValue("bSearchIncludeFamilyHOH")),
                             "uri" => $fam->getFamily()->getViewURI()
                         ];
+
+                        if ($this->global_search) {
+                            $members = $fam->getFamily()->getPeopleSorted();
+
+                            $res_members = [];
+                            $globalMembers = "";
+
+                            foreach ($members as $member) {
+                                $res_members[] = $member->getId();
+                                $globalMembers .= '• <a href="'.SystemURLs::getRootPath().'/PersonView.php?PersonID='.$member->getId().'">'.$member->getFirstName()." ".$member->getLastName()."</a><br>";
+                            }
+                            $elt["text"] = _("Family").' : <a href="'.SystemURLs::getRootPath().'/FamilyView.php?FamilyID='.$fam->getFamily()->getId().'" data-toggle="tooltip" data-placement="top" data-original-title="'._('Edit').'">'.$fam->getFamily()->getName().'</a>'." "._("Members")." : <br>".$globalMembers;
+                            $elt["id"] = $fam->getFamily()->getId();
+                            $elt["address"] = $fam->getFamily()->getFamilyString(SystemConfig::getBooleanValue("bSearchIncludeFamilyHOH"));
+                            $elt["type"] = _($this->getGlobalSearchType());
+                            $elt["realType"] = $this->getGlobalSearchType();
+                            $elt["Gender"] = "";
+                            $elt["Classification"] = "";
+                            $elt["ProNames"] = "";
+                            $elt["FamilyRole"] = "";
+                            $elt["inCart"] = Cart::FamilyInCart($fam->getFamily()->getId());
+                            $elt["members"] = $res_members;
+                        }
 
                         array_push($this->results, $elt);
                     }
