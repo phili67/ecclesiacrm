cf68f679d307be03c8570b4a98b0ab2c061628e9
phili67 <philippelogel@free.fr>
2020-03-12 21:41:45 +0100

    Last sql upgrade

--- src/mysql/upgrade/6.0.0-upgrade.sql
+++ src/mysql/upgrade/6.0.0-upgrade.sql
@@ -9,21 +9,3 @@ ADD CONSTRAINT fk_pst_cr_family_id
 
 -- change the the type of the pst_cr_person_id
 ALTER TABLE `pastoral_care` MODIFY `pst_cr_person_id`  mediumint(9) unsigned NULL default NULL;
-
-
-
--- we have to delete the lost datas, sometimes when a group is deleted the member are not deleted too
-DELETE FROM person2group2role_p2g2r WHERE p2g2r_grp_ID IN (
-SELECT *
-FROM
-(
-SELECT DISTINCT t1. p2g2r_grp_ID
-FROM `person2group2role_p2g2r` t1
-WHERE t1.p2g2r_grp_ID NOT IN (
-	SELECT  grp_ID
-	FROM `group_grp`
-	WHERE 1
-)
-)
-AS tmp
-)
