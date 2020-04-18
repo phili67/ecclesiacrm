b145a9b9acb78bcad1db0b204bcf952c57f65505
phili67 <philippelogel@free.fr>
2020-04-18 13:14:54 +0200

    gdpr dashboard upgrade

--- src/v2/templates/gdpr/dashboard.php
+++ src/v2/templates/gdpr/dashboard.php
@@ -17,32 +17,40 @@
 require $sRootDocument . '/Include/Header.php';
 ?>
 
-<div class="box box-primary box-body">
-  <div class="row ">
-      <div class="col-sm-2" style="vertical-align: middle;">
-         <a class="btn btn-app" href="<?= $sRootPath ?>/Reports/GDPR/GDPRListExport.php"><i class="fa fa-print"></i> <?= _("Printable Page") ?></a>
-      </div>
-      <div class="col-sm-10" style="vertical-align: middle;">
-        <table>
-          <tr>
-            <td><label><?= _("GDPR DPO Signer") ?></label></td><td>&nbsp;:&nbsp;</td><td><?= $gdprSigner ?></td>
-          </tr>
-          <tr>
-            <td><label><?= _("GDPR DPO Signer Email") ?></label></td><td>&nbsp;:&nbsp;</td><td><?= $gdprSignerEmail ?></td>
-          </tr>
-        </table>
-      </div>
+<div class="card card-primary card-body">
+    <div class="row ">
+        <div class="col-sm-2" style="vertical-align: middle;">
+            <a class="btn btn-app" href="<?= $sRootPath ?>/Reports/GDPR/GDPRListExport.php"><i
+                    class="fa fa-print"></i> <?= _("Printable Page") ?></a>
+        </div>
+        <div class="col-sm-10" style="vertical-align: middle;">
+            <table>
+                <tr>
+                    <td><label><?= _("GDPR DPO Signer") ?></label></td>
+                    <td>&nbsp;:&nbsp;</td>
+                    <td><?= $gdprSigner ?></td>
+                </tr>
+                <tr>
+                    <td><label><?= _("GDPR DPO Signer Email") ?></label></td>
+                    <td>&nbsp;:&nbsp;</td>
+                    <td><?= $gdprSignerEmail ?></td>
+                </tr>
+            </table>
+        </div>
     </div>
 </div>
 
-<div class="box box-body">
-<div class="box-header with-border">
-  <i class="fa fa-user"></i>
-  <h3 class="box-title"><?= _("GDPR Person status") ?></h3>
-</div>
-  <table class="table table-striped table-bordered" id="GDRP-Table" cellpadding="5" cellspacing="0"  width="100%"></table>
+<div class="card">
+    <div class="card-header with-border">
+        <i class="fa fa-user"></i>
+        <h3 class="card-title"><?= _("GDPR Person status") ?></h3>
+    </div>
+    <div class="card-body">
+        <table class="table table-striped table-bordered" id="GDRP-Table" cellpadding="5" cellspacing="0"
+               width="100%"></table>
+    </div>
 </div>
 
-<script src="<?= $sRootPath ?>/skin/js/gdpr/GDRPDashboard.js" ></script>
+<script src="<?= $sRootPath ?>/skin/js/gdpr/GDRPDashboard.js"></script>
 
 <?php require $sRootDocument . '/Include/Footer.php'; ?>
