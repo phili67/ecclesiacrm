b537093f8beca71c0319abfb6a87faf3c3eba0dc
phili67 <philippelogel@free.fr>
2020-04-18 14:34:52 +0200

    GeoPage.php upgrade

--- src/GeoPage.php
+++ src/GeoPage.php
@@ -206,23 +206,32 @@ $families = FamilyQuery::create()
     ->orderByName()
     ->find(); ?>
 <form class="form-horizontal" method="POST" action="GeoPage.php" name="GeoPage">
-    <div class="box container">
-        <div class="box-body">
+    <div class="card card-primary">
+        <div class="card-header">
+            <div class="card-title">
+                <h4><?= _("Set your elements") ?></h4>
+            </div>
+        </div>
+        <div class="card-body">
             <div class="form-group">
                 <label for="Family"
                        class="control-label col-xs-12 col-sm-3 col-md-3 col-lg-3"><?= _('Select Family:') ?></label>
                 <div class="col-xs-12 col-sm-9">
-                    <select name='Family' data-placeholder="<?= _('Select a family') ?>" class="form-control choiceSelectBox"
+                    <select name='Family' data-placeholder="<?= _('Select a family') ?>"
+                            class="form-control choiceSelectBox"
                             style="width: 100%">
                         <option></option>
                         <?php
-                        foreach ($families as $family) {
-                    ?>
-                        <option value="<?= $family->getId() ?>" <?= ($iFamily == $family->getId())?' selected':'' ?>>
-                          <?= $family->getName() ?> - <?=  $family->getAddress() ?>
-                      <?php
-                        }
+                        foreach ($families
+
+                        as $family) {
                         ?>
+                        <option
+                            value="<?= $family->getId() ?>" <?= ($iFamily == $family->getId()) ? ' selected' : '' ?>>
+                            <?= $family->getName() ?> - <?= $family->getAddress() ?>
+                            <?php
+                            }
+                            ?>
                     </select>
                 </div>
             </div>
@@ -279,14 +288,14 @@ $families = FamilyQuery::create()
         ?>
 
         <!--Datafile section -->
-        <div class="box box-default collapsed-box">
-            <div class="box-header with-border">
-                <h3 class="box-title"><?= _('Make Data File') ?></h3>
-                <div class="box-tools pull-right">
-                    <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
+        <div class="card card-secondary collapsed-card">
+            <div class="card-header with-border">
+                <h3 class="card-title"><?= _('Make Data File') ?></h3>
+                <div class="card-tools pull-right">
+                    <button class="btn btn-tool" data-card-widget="collapse"><i class="fa fa-plus"></i></button>
                 </div><!-- /.box-tools -->
             </div><!-- /.box-header -->
-            <div class="box-body">
+            <div class="card-body">
                 <div class="form-group">
                     <label for="CoordFileFormat"
                            class="control-label col-xs-12 col-sm-3 col-md-3 col-lg-3"><?= _('Data file format:') ?></label>
@@ -334,100 +343,113 @@ $families = FamilyQuery::create()
                 </div>
             </div><!-- /.box-body -->
         </div><!-- /.box -->
-    </div><!-- /.box container -->
 
-    <div class="box box-pane no-border">
+
         <?php
         $aPersonIDs = [];
 
         if ($iFamily != 0 &&
-        (isset($_POST['FindNeighbors']) ||
-            isset($_POST['PersonIDList']))
+            (isset($_POST['FindNeighbors']) ||
+                isset($_POST['PersonIDList']))
         ) {
             $resultsByDistance = FamilyInfoByDistance($iFamily);
 
             $counter = 0; ?>
-        <!-- Column Headings -->
-        <table id="neighbours" class="table table-striped table-bordered data-table dataTable no-footer"
-               cellspacing="0" role="grid">
-            <!--table class="table table-striped"-->
-            <thead>
-            <tr class="success">
-                <td><strong><?= _('Distance') ?> </strong></td>
-                <td><strong><?= _('Direction') ?></strong></td>
-                <td><strong><?= _('Name') ?>     </strong></td>
-                <td><strong><?= _('Address') ?>  </strong></td>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            foreach ($resultsByDistance as $oneResult) {
-                if ($counter >= $iNumNeighbors || $oneResult['Distance'] > $nMaxDistance) {
-                    break;
-                } // Determine how many people in this family will be listed
-                $persons = PersonQuery::Create()->filterByFamId($oneResult['fam_ID']);
-                if ($bClassificationPost) {
-                   $persons->_and()->filterByClsId($sClassificationList, Criteria::IN);
-                }
-                $persons->find();
+            <!-- Column Headings -->
+            <div class="car card-success">
+                <div class="card-header">
+                    <div class="card-title">
+                        <h4><?= _("Results") ?></h4>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table id="neighbours" class="table table-striped table-bordered data-table dataTable no-footer"
+                           cellspacing="0" role="grid">
+                        <!--table class="table table-striped"-->
+                        <thead>
+                        <tr class="success">
+                            <td><strong><?= _('Distance') ?> </strong></td>
+                            <td><strong><?= _('Direction') ?></strong></td>
+                            <td><strong><?= _('Name') ?>     </strong></td>
+                            <td><strong><?= _('Address') ?>  </strong></td>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($resultsByDistance as $oneResult) {
+                            if ($counter >= $iNumNeighbors || $oneResult['Distance'] > $nMaxDistance) {
+                                break;
+                            } // Determine how many people in this family will be listed
+                            $persons = PersonQuery::Create()->filterByFamId($oneResult['fam_ID']);
+                            if ($bClassificationPost) {
+                                $persons->_and()->filterByClsId($sClassificationList, Criteria::IN);
+                            }
+                            $persons->find();
 
-                $numListed = $persons->count();
-                if (!$numListed) { // skip familes with zero members
-                    continue;
-                }
-                $counter++; 
-            ?>
-                <tr class="info">
-                    <td><?= OutputUtils::number_localized($oneResult['Distance']) ?> </td>
-                    <td><?= $oneResult['Bearing'] ?> <?= OutputUtils::GetRouteFromCoordinates($oneResult['fam_Latitude'], $oneResult['fam_Longitude']) ?>
-                    </td>
-                    <td><b><?= $oneResult['fam_Name'] ?> </b></td>
-                    <td>
-                       <?= OutputUtils::GetLinkMapFromCoordinates ($oneResult['fam_Latitude'], $oneResult['fam_Longitude'], $oneResult['fam_Address']) ?>
-                    </td>
-                </tr>
+                            $numListed = $persons->count();
+                            if (!$numListed) { // skip familes with zero members
+                                continue;
+                            }
+                            $counter++;
+                            ?>
+                            <tr class="info" style="background-color: rgba(18, 145, 190, 0.1) !important;">
+                                <td><?= OutputUtils::number_localized($oneResult['Distance']) ?> </td>
+                                <td><?= $oneResult['Bearing'] ?> <?= OutputUtils::GetRouteFromCoordinates($oneResult['fam_Latitude'], $oneResult['fam_Longitude']) ?>
+                                </td>
+                                <td><b><?= $oneResult['fam_Name'] ?> </b></td>
+                                <td>
+                                    <?= OutputUtils::GetLinkMapFromCoordinates($oneResult['fam_Latitude'], $oneResult['fam_Longitude'], $oneResult['fam_Address']) ?>
+                                </td>
+                            </tr>
+                            <?php
+                            foreach ($persons as $person) {
+                                if (!in_array($person->getId(), $aPersonIDs)) {
+                                    $aPersonIDs[] = $person->getId();
+                                } ?>
+                                <tr>
+                                    <td><BR></td>
+                                    <td><BR></td>
+                                    <td align="right"><?= $person->getFirstName() . ' ' . $person->getLastName() ?> </td>
+                                    <td align="left"><?= $aClassificationName[$person->getClsId()] ?></td>
+                                </tr>
+                                <?php
+                            }
+                        } ?>
+                        </tbody>
+                    </table>
+                </div>
+
+
                 <?php
-                foreach ($persons as $person) {
-                    if (!in_array($person->getId(), $aPersonIDs)) {
-                        $aPersonIDs[] = $person->getId();
-                    } ?>
-                    <tr>
-                        <td><BR></td>
-                        <td><BR></td>
-                        <td align="right"><?= $person->getFirstName() . ' ' . $person->getLastName() ?> </td>
-                        <td align="left"><?= $aClassificationName[$person->getClsId()] ?></td>
-                    </tr>
-                    <?php
-                }
-            } ?>
-            </tbody>
-        </table>
-    </div><!-- /.box -->
+                $sPersonIDList = implode(',', $aPersonIDs); ?>
 
-    <?php
-    $sPersonIDList = implode(',', $aPersonIDs); ?>
+                <div class="card-footer">
+                    <input type="hidden" name="PersonIDList" value="<?= $sPersonIDList ?>">
 
-    <input type="hidden" name="PersonIDList" value="<?= $sPersonIDList ?>">
+                    <div class="row">
+                        <div class="col-4">
+                            <a id="AddAllToCart" class="btn btn-primary"><?= _('Add All to Cart') ?></a>
+                        </div>
+                        <div class="col-4">
+                            <input name="IntersectCart" type="submit" class="btn btn-primary"
+                                   value="<?= _('Intersect with Cart') ?>">
+                        </div>
+                        <div class="col-4">
+                            <a id="RemoveAllFromCart" class="btn btn-danger"><?= _('Remove All from Cart') ?></a>
+                        </div>
 
-    <div class="row">
-        <div class="col-xs-7 col-md-4">
-            <a id="AddAllToCart" class="btn btn-primary" ><?= _('Add All to Cart') ?></a>
-        </div>
-        <div class="col-xs-7 col-md-4">
-            <input name="IntersectCart" type="submit" class="btn btn-primary"
-                   value="<?= _('Intersect with Cart') ?>">
-        </div>
-        <div class="col-xs-7 col-md-4">
-           <a id="RemoveAllFromCart" class="btn btn-danger" ><?= _('Remove All from Cart') ?></a>
-        </div>
-    </div>
-    <?php
+                    </div>
+                </div>
+            </div>
+            <?php
         }
-    ?>
+        ?>
+
+    </div><!-- /.card -->
 </form>
 
 <script nonce="<?= SystemURLs::getCSPNonce() ?>">
-    var listPeople=<?= json_encode($aPersonIDs)?>;
+    var listPeople =<?= json_encode($aPersonIDs)?>;
 </script>
 <script src="<?= SystemURLs::getRootPath() ?>/skin/js/GeoPage.js"></script>
 <?php
