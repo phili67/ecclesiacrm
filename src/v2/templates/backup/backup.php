e9015b19fae45273b0f7e27c6a8b15908bda577e
phili67 <philippelogel@free.fr>
2020-04-20 21:29:33 +0200

    backup upgrade

--- src/v2/templates/backup/backup.php
+++ src/v2/templates/backup/backup.php
@@ -19,7 +19,7 @@ require $sRootDocument . '/Include/Header.php';
 
 <div class="card">
     <div class="card-header with-border">
-        <h3 class="box-title"><?= _('This tool will assist you in manually backing up the EcclesiaCRM database.') ?></h3>
+        <h3 class="card-title"><?= _('This tool will assist you in manually backing up the EcclesiaCRM database.') ?></h3>
     </div>
     <div class="card-body">
         <ul>
@@ -100,7 +100,7 @@ require $sRootDocument . '/Include/Header.php';
 </div>
 <div class="card">
     <div class="card-header">
-        <h1 class="card-title"><?= _('Backup Status:') ?> </h1>&nbsp;<h1 class="box-title" id="backupstatus"
+        <h1 class="card-title"><?= _('Backup Status:') ?> </h1>&nbsp;<h1 class="card-title" id="backupstatus"
                                                                         style="color:red"> <?= _('No Backup Running') ?></h1>
     </div>
     <div class="card-body" id="resultFiles">
