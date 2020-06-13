4f8193276684692f42045aefa19ad68e2410e205
phili67 <philippelogel@free.fr>
2020-06-13 10:59:33 +0200

    arch v2 kioskmanager upgrade

---
+++ src/v2/routes/sidebar/kioskmanager.php
@@ -0,0 +1,41 @@
+<?php
+
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+use EcclesiaCRM\Utils\InputUtils;
+use EcclesiaCRM\dto\SystemURLs;
+use EcclesiaCRM\utils\RedirectUtils;
+use EcclesiaCRM\SessionUser;
+
+use Slim\Views\PhpRenderer;
+
+$app->group('/kioskmanager', function () {
+    $this->get('', 'renderKioskManager');
+    $this->get('/', 'renderKioskManager');
+});
+
+
+function renderKioskManager (Request $request, Response $response, array $args) {
+    $renderer = new PhpRenderer('templates/sidebar/');
+
+    if ( !( SessionUser::getUser()->isAdmin() ) ) {
+      return $response->withStatus(302)->withHeader('Location', SystemURLs::getRootPath() . '/Menu.php');
+    }
+
+    return $renderer->render($response, 'kioskmanager.php', argumentsKioskManagerArray());
+}
+
+function argumentsKioskManagerArray ()
+{
+    //Set the page title
+    $sPageTitle = _("Kiosk Manager");
+
+    $sRootDocument  = SystemURLs::getDocumentRoot();
+
+    $paramsArguments = ['sRootPath'    => SystemURLs::getRootPath(),
+                       'sRootDocument' => $sRootDocument,
+                       'sPageTitle'    => $sPageTitle
+                        ];
+   return $paramsArguments;
+}
