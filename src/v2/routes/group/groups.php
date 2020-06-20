0b57f93390c9727d12958db4b6124a370782fd0f
phili67 <philippelogel@free.fr>
2020-06-20 20:59:17 +0200

    new group route for badges (group + sunday school)

--- src/v2/routes/group/groups.php
+++ src/v2/routes/group/groups.php
@@ -11,12 +11,15 @@ use EcclesiaCRM\GroupPropMasterQuery;
 use EcclesiaCRM\GroupQuery;
 use EcclesiaCRM\PropertyQuery;
 use EcclesiaCRM\SessionUser;
+use EcclesiaCRM\Utils\MiscUtils;
+use Propel\Runtime\ActiveQuery\Criteria;
 
 use Slim\Views\PhpRenderer;
 
 $app->group('/group', function () {
     $this->get('/list', 'groupList' );
     $this->get('/{groupID:[0-9]+}/view', 'groupView' );
+    $this->get('/{groupId:[0-9]+}/badge/{useCart:[0-9]+}/{type}', 'groupBadge' );
 });
 
 
@@ -108,6 +111,57 @@ function renderGroupViewArray ($iGroupID)
         'is_group_manager' => $is_group_manager,
         'ormProperties'    => $ormProperties,
         'ormPropList'      => $ormPropList];
+
+    return $paramsArguments;
+}
+
+function groupBadge (Request $request, Response $response, array $args) {
+    $renderer = new PhpRenderer('templates/group/');
+
+    $groupId = $args['groupId'];
+    $useCart = $args['useCart'];
+
+    if ( !( SessionUser::getUser()->isSundayShoolTeacherForGroup($groupId) || SessionUser::getUser()->isExportSundaySchoolPDFEnabled() ) ) {
+        return $response->withStatus(302)->withHeader('Location', SystemURLs::getRootPath() . '/Menu.php');
+    }
+
+
+    return $renderer->render($response, 'groupbadge.php', argumentsGroupBadgeArray($groupId,$useCart));
+}
+
+function argumentsGroupBadgeArray ($iGroupID,$useCart)
+{
+    $imgs = MiscUtils::getImagesInPath ('../Images/background');
+
+    $group = GroupQuery::Create()->findOneById ($iGroupID);
+
+    // Get all the sunday school classes
+    $groups = GroupQuery::create()
+        ->orderByName(Criteria::ASC)
+        ->filterByType(4)
+        ->find();
+
+    // Set the page title and include HTML header
+    if ( $group->isSundaySchool() ) {
+        $sPageTitle = _('Sunday School Badges for') . ' : ' . $group->getName();
+    } else {
+        $sPageTitle = _('Badges for Group') . ' : ' . $group->getName();
+    }
+
+    $sRootDocument = SystemURLs::getDocumentRoot();
+    $CSPNonce = SystemURLs::getCSPNonce();
+
+    $paramsArguments = ['sRootPath' => SystemURLs::getRootPath(),
+        'sRootDocument'             => $sRootDocument,
+        'CSPNonce'                  => $CSPNonce,
+        'sPageTitle'                => $sPageTitle,
+        'iGroupID'                  => $iGroupID,
+        'useCart'                   => $useCart,
+        'imgs'                      => $imgs,
+        'group'                     => $group,
+        'groups'                    => $groups,
+        'isSundaySchool'            => $group->isSundaySchool()
+    ];
 
     return $paramsArguments;
 }
