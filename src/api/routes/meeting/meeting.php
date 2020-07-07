e687d9d3e88a1399c42d5ceea208dcc56b788c4f
phili67 <philippelogel@free.fr>
2020-07-07 22:46:35 +0200

    Last meeting improvements

--- src/api/routes/meeting/meeting.php
+++ src/api/routes/meeting/meeting.php
@@ -30,7 +30,7 @@ function deleteAllMeetingRooms(Request $request, Response $response, array $args
         $all_pms->delete();
     }
 
-    echo true;
+    return $response->withJson(['status' => "success"]);
 }
 
 function selectMeetingRoom(Request $request, Response $response, array $args)
@@ -50,7 +50,7 @@ function selectMeetingRoom(Request $request, Response $response, array $args)
         $lpm->setPersonId($personId);
         $lpm->save();
 
-        echo $lpm->toJSON();
+        return $response->withJson($lpm->toArray());
     }
 
     echo null;
@@ -62,6 +62,7 @@ function createMeetingRoom(Request $request, Response $response, array $args)
 
     if ( isset($input->roomName) ) {
         $personId = SessionUser::getUser()->getPersonId();
+
 
         $pm = new PersonMeeting();
         $pm->setCode(basename($input->roomName));
@@ -81,7 +82,7 @@ function createMeetingRoom(Request $request, Response $response, array $args)
         $lpm->setPersonId($personId);
         $lpm->save();
 
-        return $pm->toJSON();
+        return $response->withJson($pm->toArray());
     }
 
     return null;
@@ -95,7 +96,7 @@ function getLastMeeting(Request $request, Response $response, array $args)
 
     if (!is_null($lpm)) {
         $pm = PersonMeetingQuery::create()->findOneById($lpm->getPersonMeetingId());
-        return $pm->toJSON();
+        return $response->withJson($pm->toArray());
     } else {
         return null;
     }
@@ -108,6 +109,5 @@ function getAllMettings(Request $request, Response $response, array $args)
     $meetings = PersonMeetingQuery::create()
         ->findByPersonId($personId);
 
-    return $meetings->toJSON();
+    return $response->withJson(['PersonMeetings' => $meetings->toArray()]);
 }
-
