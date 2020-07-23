fa368ccb135afa8b3f1c73d7a76b2e66148c4d51
phili67 <philippelogel@free.fr>
2020-07-23 10:28:54 +0200

    Install script upgrade

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# Dans le cas de php 7.2
+cp -rf propel/vendor/* src/vendor/propel
+
+
 npm install --unsafe-perm
 
 npm run install
@@ -7,5 +11,7 @@ npm run postinstall
 npm run orm-gen
 
 #npm run composer-update
+
+chown -R www-data:www-data src
 
 
