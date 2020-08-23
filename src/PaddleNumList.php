d64cbd156cff9c14119148c214dbd4f5c2f73334
phili67 <philippelogel@free.fr>
2020-08-23 17:12:23 +0200

    PaddleNumList.php card upgrade

--- src/PaddleNumList.php
+++ src/PaddleNumList.php
@@ -48,68 +48,73 @@ require 'Include/Header.php';
 
     <div class="card card-body">
         <div class="row">
-        <?php
-        if ($iFundRaiserID > 0) {
-            ?>
-            <input type=button class="btn btn-default btn-sm" value="<?= _('Select all') ?>" name=SelectAll
-                   onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&SelectAll=1&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
             <?php
-        }
-        ?>
-        <input type=button class="btn btn-default btn-sm" value="<?= _('Select none') ?>" name=SelectNone
-               onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
-        <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Buyer') ?> " name=AddBuyer
-               onclick="javascript:document.location='PaddleNumEditor.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
-        <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Donors to Buyer List') ?> " name=AddBuyer
-               onclick="javascript:document.location='AddDonors.php?FundRaiserID=<?= $iFundRaiserID ?>'">
+            if ($iFundRaiserID > 0) {
+                ?>
+                <input type=button class="btn btn-default btn-sm" value="<?= _('Select all') ?>" name=SelectAll
+                       onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&SelectAll=1&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
+                <?php
+            }
+            ?>
+            <input type=button class="btn btn-default btn-sm" value="<?= _('Select none') ?>" name=SelectNone
+                   onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
+            <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Buyer') ?> " name=AddBuyer
+                   onclick="javascript:document.location='PaddleNumEditor.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
+            <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Donors to Buyer List') ?> "
+                   name=AddBuyer
+                   onclick="javascript:document.location='AddDonors.php?FundRaiserID=<?= $iFundRaiserID ?>'">
 
-        <input type=submit class="btn btn-info btn-sm" value="<?= _('Generate Statements for Selected') ?>"
-               name=GenerateStatements>
+            <input type=submit class="btn btn-info btn-sm" value="<?= _('Generate Statements for Selected') ?>"
+                   name=GenerateStatements>
         </div>
     </div>
-    <div class="card card-body">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title"><?= _("Buyers") ?></h3>
+        </div>
+        <div class="card-body">
+            <table cellpadding="5" cellspacing="5"
+                   class="table table-striped table-bordered dataTable no-footer dtr-inline  paddleNumeList-table"
+                   width="100%">
 
-        <table cellpadding="5" cellspacing="5"
-               class="table table-striped table-bordered dataTable no-footer dtr-inline  paddleNumeList-table"
-               width="100%">
+                <thead>
+                <th><?= _('Select') ?></th>
+                <th><?= _('Number') ?></th>
+                <th><?= _('Buyer') ?></th>
+                <th><?= _('Delete') ?></th>
+                </thead>
 
-            <thead>
-            <th><?= _('Select') ?></th>
-            <th><?= _('Number') ?></th>
-            <th><?= _('Buyer') ?></th>
-            <th><?= _('Delete') ?></th>
-            </thead>
+                <?php
+                $tog = 0;
 
-            <?php
-            $tog = 0;
+                //Loop through all buyers
+                if (!is_null($ormPaddleNumes)) {
+                    foreach ($ormPaddleNumes as $num) {
+                        $sRowClass = 'RowColorA'; ?>
+                        <tr>
+                            <td>
+                                <input type="checkbox"
+                                       name="Chk<?= $num->getId() ?>" <?= (isset($_GET['SelectAll'])) ? ' checked="yes"' : '' ?>></input>
+                            </td>
+                            <td>
+                                <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumEditor.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php"> <?= $num->getNum() ?></a>
+                            </td>
 
-            //Loop through all buyers
-            if (!is_null($ormPaddleNumes)) {
-                foreach ($ormPaddleNumes as $num) {
-                    $sRowClass = 'RowColorA'; ?>
-                    <tr>
-                        <td>
-                            <input type="checkbox"
-                                   name="Chk<?= $num->getId() ?>" <?= (isset($_GET['SelectAll'])) ? ' checked="yes"' : '' ?>></input>
-                        </td>
-                        <td>
-                            <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumEditor.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php"> <?= $num->getNum() ?></a>
-                        </td>
+                            <td>
+                                <?= $num->getBuyerFirstName() . ' ' . $num->getBuyerLastName() ?>&nbsp;
+                            </td>
+                            <td>
+                                <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumDelete.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>">
+                                    <i class="fa fa-trash-o" aria-hidden="true" style="color:red"></i></a>
+                            </td>
+                        </tr>
+                        <?php
+                    } // while
+                } // if
+                ?>
 
-                        <td>
-                            <?= $num->getBuyerFirstName() . ' ' . $num->getBuyerLastName() ?>&nbsp;
-                        </td>
-                        <td>
-                            <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumDelete.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>">
-                                <i class="fa fa-trash-o" aria-hidden="true" style="color:red"></i></a>
-                        </td>
-                    </tr>
-                    <?php
-                } // while
-            } // if
-            ?>
-
-        </table>
+            </table>
+        </div>
     </div>
 </form>
 
