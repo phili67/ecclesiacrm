9e474443ed0d4e61a712784a02487a1ac8feb1a3
phili67 <philippelogel@free.fr>
2020-09-18 22:12:26 +0200

    PaddleNumList.php js upgrade

--- src/PaddleNumList.php
+++ src/PaddleNumList.php
@@ -14,9 +14,6 @@ require 'Include/Functions.php';
 
 use EcclesiaCRM\Utils\InputUtils;
 use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\PaddleNumQuery;
-
-use EcclesiaCRM\Map\PersonTableMap;
 
 
 $linkBack = InputUtils::LegacyFilterInputArr($_GET, 'linkBack');
@@ -25,19 +22,6 @@ if (isset ($_GET['FundRaiserID'])) {
     $iFundRaiserID = InputUtils::LegacyFilterInputArr($_GET, 'FundRaiserID');
 } else {
     $iFundRaiserID = $_SESSION['iCurrentFundraiser'];
-}
-
-if ($iFundRaiserID > 0) {
-    //Get the paddlenum records for this fundraiser
-    $ormPaddleNumes = PaddleNumQuery::create()
-        ->usePersonQuery()
-        ->addAsColumn('BuyerFirstName', PersonTableMap::COL_PER_FIRSTNAME)
-        ->addAsColumn('BuyerLastName', PersonTableMap::COL_PER_LASTNAME)
-        ->endUse()
-        ->orderByNum()
-        ->findByFrId($iFundRaiserID);
-} else {
-    $ormPaddleNumes = null;
 }
 
 $sPageTitle = _('Buyers for this fundraiser:') . $iFundRaiserID;
@@ -51,15 +35,12 @@ require 'Include/Header.php';
             <?php
             if ($iFundRaiserID > 0) {
                 ?>
-                <input type=button class="btn btn-default btn-sm" value="<?= _('Select all') ?>" name=SelectAll
-                       onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&SelectAll=1&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
+                <input type=button class="btn btn-default btn-sm" value="<?= _('Select all') ?>" name=SelectAll id="SelectAll">
                 <?php
             }
             ?>
-            <input type=button class="btn btn-default btn-sm" value="<?= _('Select none') ?>" name=SelectNone
-                   onclick="javascript:document.location='PaddleNumList.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
-            <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Buyer') ?> " name=AddBuyer
-                   onclick="javascript:document.location='PaddleNumEditor.php?CurrentFundraiser=<?= $iFundRaiserID ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>&CurrentFundraiser=<?= $iFundRaiserID ?>'">
+            <input type=button class="btn btn-default btn-sm" value="<?= _('Select none') ?>" name=SelectNone id="SelectNone">
+            <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Buyer') ?> " name=AddBuyer id="AddBuyer">
             <input type=button class="btn btn-primary btn-sm" value="<?= _('Add Donors to Buyer List') ?> "
                    name=AddBuyer
                    onclick="javascript:document.location='AddDonors.php?FundRaiserID=<?= $iFundRaiserID ?>'">
@@ -74,61 +55,18 @@ require 'Include/Header.php';
         </div>
         <div class="card-body">
             <table cellpadding="5" cellspacing="5"
-                   class="table table-striped table-bordered dataTable no-footer dtr-inline  paddleNumeList-table"
-                   width="100%">
-
-                <thead>
-                <th><?= _('Select') ?></th>
-                <th><?= _('Number') ?></th>
-                <th><?= _('Buyer') ?></th>
-                <th><?= _('Delete') ?></th>
-                </thead>
-
-                <?php
-                $tog = 0;
-
-                //Loop through all buyers
-                if (!is_null($ormPaddleNumes)) {
-                    foreach ($ormPaddleNumes as $num) {
-                        $sRowClass = 'RowColorA'; ?>
-                        <tr>
-                            <td>
-                                <input type="checkbox"
-                                       name="Chk<?= $num->getId() ?>" <?= (isset($_GET['SelectAll'])) ? ' checked="yes"' : '' ?>></input>
-                            </td>
-                            <td>
-                                <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumEditor.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php"> <?= $num->getNum() ?></a>
-                            </td>
-
-                            <td>
-                                <?= $num->getBuyerFirstName() . ' ' . $num->getBuyerLastName() ?>&nbsp;
-                            </td>
-                            <td>
-                                <a href="<?= SystemURLs::getRootPath() ?>/PaddleNumDelete.php?PaddleNumID=<?= $num->getId() ?>&linkBack=PaddleNumList.php?FundRaiserID=<?= $iFundRaiserID ?>">
-                                    <i class="fa fa-trash-o" aria-hidden="true" style="color:red"></i></a>
-                            </td>
-                        </tr>
-                        <?php
-                    } // while
-                } // if
-                ?>
-
-            </table>
+                   class="table table-striped table-bordered dataTable no-footer dtr-inline"
+                   id="buyer-listing-table"
+                   width="100%"></table>
         </div>
     </div>
 </form>
 
 <script nonce="<?= SystemURLs::getCSPNonce() ?>">
-    $(document).ready(function () {
-        $("#Buyers").select2();
-
-        $('.paddleNumeList-table').DataTable({
-            responsive: true,
-            "language": {
-                "url": window.CRM.plugin.dataTable.language.url
-            },
-        });
-    });
+    window.CRM.fundraiserID = <?= $iFundRaiserID ?>;
+    window.CRM.checkAll = false;
 </script>
+
+<script src="<?= SystemURLs::getRootPath() ?>/skin/js/fundraiser/paddleNumList.js">
 
 <?php require 'Include/Footer.php' ?>
