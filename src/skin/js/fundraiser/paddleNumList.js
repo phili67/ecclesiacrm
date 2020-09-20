e5836237d9016cce26855db82b0d587e2f0dd6a7
phili67 <philippelogel@free.fr>
2020-09-20 21:42:27 +0200

    PaddleNumEditor in js code

--- src/skin/js/fundraiser/paddleNumList.js
+++ src/skin/js/fundraiser/paddleNumList.js
@@ -14,6 +14,102 @@ $(document).ready(function () {
         });
     });
 
+    function addPersonsToSelectList(iPerID) {
+
+        if (iPerID === undefined) {
+            iPerID = false;
+        }
+
+        window.CRM.APIRequest({
+            method: 'GET',
+            path: 'fundraiser/paddlenum/persons/all/' + window.CRM.fundraiserID,
+        }).done(function (data) {
+            var elt = document.getElementById("Buyers");
+            var persons = data.persons;
+            var len = persons.length;
+
+            $("#Number").val(data.Number);
+
+            var option = document.createElement("option");
+            option.text = i18next.t ('Unassigned');
+            option.value = 0;
+            elt.appendChild(option);
+
+            for (i = 0; i < len; ++i) {
+                var option = document.createElement("option");
+                option.text = persons[i].LastName + ", " + persons[i].FirstName + " - " + persons[i].Address1;
+                option.value = persons[i].Id;
+
+                /*if (iPerID && iPerID === persons[i].Id) {
+                    option.setAttribute('selected', 'selected');
+                }*/
+
+                elt.appendChild(option);
+            }
+            $("#Buyers").select2();
+
+            window.CRM.addbuyerModal.modal("show");
+        });
+    }
+
+    function BootboxContent( windowtitle, number ) {
+        var frm_str = '<h3 style="margin-top:-5px">' + i18next.t ('Buyer Number Editor') + '</h3><div id="some-form">'
+            + '<div>'
+            + '  <div class="row  div-title BuyerTitle">'
+            + '      <div class="col-md-3"><label>' + i18next.t('Add buyer') + "</label></div>"
+            + '  </div>'
+            + '  <div class="row NumberTitle">'
+            + '      <div class="col-md-3">' + i18next.t('Number') + ":</div>"
+            + '      <div class="col-md-9">'
+            + "         <input type='text' id='Number' placeholder='" + i18next.t("Buyer Number") + "' size='30' maxlength='100' class='form-control input-sm'  width='100%' style='width: 100%' required " + ((windowtitle != undefined) ? ("value='" + number + "'") : "") + ">"
+            + '      </div>'
+            + '  </div><br>'
+            + '  <div class="row  buyer-list-title">'
+            + '      <div class="col-md-3">' + i18next.t('Buyer') + ":</div>"
+            + '      <div class="col-md-9">'
+            + '          <select name="PerID" class="form-control select2  input-sm" id="Buyers" style="width: 100%">></select>'
+            + '      </div>'
+            + '  </div>'
+            + '</div>';
+
+        var object = $('<div/>').html(frm_str).contents();
+
+        return object
+    }
+
+
+    function createBuyerEditorWindow(number, windowtitle, iPerdId) // dialogType : createEvent or modifyEvent, eventID is when you modify and event
+    {
+        windowtitle = i18next.t ('Buyer Number Editor');
+
+        window.CRM.addbuyerModal = bootbox.dialog({
+            message: BootboxContent(windowtitle, number),
+            size: 'large',
+            buttons: [
+                {
+                    label: '<i class="fa fa-times"></i> ' + i18next.t("Close"),
+                    className: "btn btn-default",
+                    callback: function () {
+                        console.log("just do something on close");
+                    }
+                },
+                {
+                    label: '<i class="fa fa-check"></i> ' + i18next.t("Ok"),
+                    className: "btn btn-primary",
+                    callback: function () {
+
+                    }
+                }
+            ],
+            show: false/*,
+         onEscape: function() {
+            modal.modal("hide");
+         }*/
+        });
+
+        addPersonsToSelectList(iPerdId);
+    }
+
     $("#SelectAll").click(function () {
         window.CRM.checkAll = true;
 
@@ -27,13 +123,15 @@ $(document).ready(function () {
     });
 
     $("#AddBuyer").click(function () {
-        location.href = window.CRM.root + '/PaddleNumEditor.php?CurrentFundraiser=' + window.CRM.fundraiserID + '&linkBack=PaddleNumList.php?FundRaiserID=' + window.CRM.fundraiserID + '&CurrentFundraiser='+window.CRM.fundraiserID;
+        createBuyerEditorWindow('1', i18next.t('Buyer Number Editor'));
+
+        //location.href = window.CRM.root + '/PaddleNumEditor.php?CurrentFundraiser=' + window.CRM.fundraiserID + '&linkBack=PaddleNumList.php?FundRaiserID=' + window.CRM.fundraiserID + '&CurrentFundraiser='+window.CRM.fundraiserID;
     });
 
     $("#AddDonnor").click(function () {
         window.CRM.APIRequest({
             method: 'POST',
-            path: 'fundraiser/add/donnors',
+            path: 'fundraiser/paddlenum/add/donnors',
             data: JSON.stringify({"fundraiserID": window.CRM.fundraiserID})
         }).done(function(data) {
             if (data.status == "success") {
