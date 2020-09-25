87b5f4a93d2c81e71d8f3f8ddec5b62c26ab3fdd
phili67 <philippelogel@free.fr>
2020-09-25 14:42:50 +0200

    FRBidSheets.php propeled

--- src/Reports/FRBidSheets.php
+++ src/Reports/FRBidSheets.php
@@ -13,14 +13,27 @@ require '../Include/Functions.php';
 use EcclesiaCRM\dto\SystemConfig;
 use EcclesiaCRM\Reports\ChurchInfoReport;
 
+use EcclesiaCRM\Utils\OutputUtils;
+
+use EcclesiaCRM\FundRaiserQuery;
+use EcclesiaCRM\DonatedItemQuery;
+
+use EcclesiaCRM\Map\PersonTableMap;
+use EcclesiaCRM\Map\DonatedItemTableMap;
+
+use Propel\Runtime\ActiveQuery\Criteria;
+
 $iCurrentFundraiser = $_GET['CurrentFundraiser'];
 
 class PDF_FRBidSheetsReport extends ChurchInfoReport
 {
+    private $fundraiser = null;
+
     // Constructor
-    public function __construct()
+    public function __construct($fundraiser)
     {
         parent::__construct('P', 'mm', $this->paperFormat);
+        $this->fundraiser = $fundraiser;
         $this->leftX = 10;
         $this->SetFont('Times', '', 10);
         $this->SetMargins(15, 25);
@@ -30,51 +43,53 @@ class PDF_FRBidSheetsReport extends ChurchInfoReport
 
     public function AddPage($orientation = '', $format = '')
     {
-        global $fr_title, $fr_description;
-
         parent::AddPage($orientation, $format);
 
-        //    	$this->SetFont("Times",'B',16);
-//    	$this->Write (8, $fr_title."\n");
-//		$curY += 8;
-//		$this->Write (8, $fr_description."\n\n");
-//		$curY += 8;
-//   	$this->SetFont("Times",'',10);
+        //$this->SetFont("Times",'B',16);
+    	//$this->Write (8, $this->fundraiser->getTitle()."\n");
+		//$curY += 8;
+		//$this->Write (8, $this->fundraiser->getDescription()."\n\n");
+		//$curY += 8;
+   	    //$this->SetFont("Times",'',10);
     }
 }
 
+$currency = OutputUtils::translate_currency_fpdf(SystemConfig::getValue("sCurrency"));
+
 // Get the information about this fundraiser
-$sSQL = 'SELECT * FROM fundraiser_fr WHERE fr_ID='.$iCurrentFundraiser;
-$rsFR = RunQuery($sSQL);
-$thisFR = mysqli_fetch_array($rsFR);
-extract($thisFR);
+$thisFRORM = FundRaiserQuery::create()->findOneById($iCurrentFundraiser);
 
 // Get all the donated items
-$sSQL = 'SELECT * FROM donateditem_di LEFT JOIN person_per on per_ID=di_donor_ID '.
-        ' WHERE di_FR_ID='.$iCurrentFundraiser.
-        ' ORDER BY SUBSTR(di_item,1,1),cast(SUBSTR(di_item,2) as unsigned integer),SUBSTR(di_item,4)';
+$ormItems = DonatedItemQuery::create()
+    ->addJoin(DonatedItemTableMap::COL_DI_DONOR_ID, PersonTableMap::COL_PER_ID, Criteria::LEFT_JOIN)
+    ->addAsColumn('FirstName', PersonTableMap::COL_PER_FIRSTNAME)
+    ->addAsColumn('LastName', PersonTableMap::COL_PER_LASTNAME)
+    ->addAsColumn('cri1', 'SUBSTR('. DonatedItemTableMap::COL_DI_ITEM.',1,1)')
+    ->addAsColumn('cri2', 'cast(SUBSTR('. DonatedItemTableMap::COL_DI_ITEM.',2) as unsigned integer)')
+    ->addAsColumn('cri3', 'SUBSTR('. DonatedItemTableMap::COL_DI_ITEM.',4)')
+    ->orderBy('cri1')
+    ->orderBy('cri2')
+    ->orderBy('cri3')
+    ->findByFrId($iCurrentFundraiser);
 
-$rsItems = RunQuery($sSQL);
-
-$pdf = new PDF_FRBidSheetsReport();
-$pdf->SetTitle($fr_title);
+$pdf = new PDF_FRBidSheetsReport($thisFRORM);
+$pdf->SetTitle(OutputUtils::translate_text_fpdf($thisFRORM->getTitle()));
 
 // Loop through items
-while ($oneItem = mysqli_fetch_array($rsItems)) {
-    extract($oneItem);
 
+foreach ($ormItems as $item) {
     $pdf->AddPage();
 
     $pdf->SetFont('Times', 'B', 24);
-    $pdf->Write(5, $di_item.":\t");
-    $pdf->Write(5, stripslashes($di_title)."\n\n");
+    $pdf->Write(5, OutputUtils::translate_text_fpdf($item->getItem()).":\t");
+    $pdf->Write(5, OutputUtils::translate_text_fpdf(stripslashes($item->getTitle()))."\n\n");
     $pdf->SetFont('Times', '', 16);
-    $pdf->Write(8, stripslashes($di_description)."\n");
-    if ($di_estprice > 0) {
-        $pdf->Write(8, _('Estimated value ').'$'.$di_estprice.'.  ');
+    $pdf->Write(8, OutputUtils::translate_text_fpdf(stripslashes($item->getDescription()))."\n");
+    if ($item->getEstprice() > 0) {
+        $pdf->Write(8, OutputUtils::translate_text_fpdf(_('Estimated value ')).$currency.OutputUtils::money_localized($item->getEstprice()).'.  ');
     }
-    if ($per_LastName != '') {
-        $pdf->Write(8, _('Donated by ').$per_FirstName.' '.$per_LastName.".\n");
+    if ($item->getLastName() != '') {
+        $pdf->Write(8, OutputUtils::translate_text_fpdf(_('Donated by ').$item->getFirstName().' '.$item->getLastName()).".\n");
     }
     $pdf->Write(8, "\n");
 
@@ -88,10 +103,10 @@ while ($oneItem = mysqli_fetch_array($rsItems)) {
     $pdf->Cell($widPaddle, $lineHeight, _('Paddle'), 1, 0);
     $pdf->Cell($widBid, $lineHeight, _('Bid'), 1, 1);
 
-    if ($di_minimum > 0) {
+    if ($item->getMinimum() > 0) {
         $pdf->Cell($widName, $lineHeight, '', 1, 0);
         $pdf->Cell($widPaddle, $lineHeight, '', 1, 0);
-        $pdf->Cell($widBid, $lineHeight, '$'.$di_minimum, 1, 1);
+        $pdf->Cell($widBid, $lineHeight, $currency.OutputUtils::money_localized($item->getMinimum()), 1, 1);
     }
     for ($i = 0; $i < 20; $i += 1) {
         $pdf->Cell($widName, $lineHeight, '', 1, 0);
