85d26c24794b7de39b9d061d9e16df4c7343d34b
phili67 <philippelogel@free.fr>
2020-10-05 22:17:04 +0200

    Comments

--- src/FamilyVerify.php
+++ src/FamilyVerify.php
@@ -1,7 +1,15 @@
 <?php
+/***********************************************************************
+ *
+ * This source code can only be called through :
+ * http://www.mydomain.com/ecclesiacrm/FamilyVerify.php?FamilyID=274
+ *
+ ***********************************************************************/
 
 require 'Include/Config.php';
 require 'Include/Functions.php';
+
+
 
 use EcclesiaCRM\FamilyQuery;
 use EcclesiaCRM\Utils\InputUtils;
