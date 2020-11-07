dffc339a65089e8ba43b06085af4165f0bd7bb9e
phili67 <philippelogel@free.fr>
2020-11-07 17:55:47 +0100

    final version

--- src/Include/Header.php
+++ src/Include/Header.php
@@ -201,7 +201,7 @@ Header_system_notifications();
             <img src="<?= SystemURLs::getRootPath() ?>/icon-small.png" alt="EcclesiaCRM Logo"
                  class="brand-image img-circle elevation-3" style="opacity: .8">
             <span
-                class="brand-text font-weight-light">Ecclesia<b>CRM</b> <?= SystemService::getDBMainVersion() ?></span> GM3
+                class="brand-text font-weight-light">Ecclesia<b>CRM</b> <?= SystemService::getDBMainVersion() ?></span>
         </a>
 
         <section class="sidebar">
