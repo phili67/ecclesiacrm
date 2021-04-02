bf16952da06c6d6ea404035d7a6e8a61ea745fe2
phili67 <philippelogel@free.fr>
2021-04-02 07:34:23 +0200

    v2/calendar.php s4

--- src/v2/routes/calendar/calendar.php
+++ src/v2/routes/calendar/calendar.php
@@ -1,7 +1,9 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
+
 
 use EcclesiaCRM\EventTypesQuery;
 use EcclesiaCRM\dto\ChurchMetaData;
@@ -12,14 +14,14 @@ use EcclesiaCRM\SessionUser;
 
 use Slim\Views\PhpRenderer;
 
-$app->group('/calendar', function () {
-    $this->get('', 'renderCalendar');
-    $this->get('/', 'renderCalendar');
+$app->group('/calendar', function (RouteCollectorProxy $group) {
+    $group->get('', 'renderCalendar');
+    $group->get('/', 'renderCalendar');
 });
 
 function renderCalendar (Request $request, Response $response, array $args) {
     $renderer = new PhpRenderer('templates/calendar/');
-    
+
     return $renderer->render($response, 'calendar.php', argumentsCalendarArray());
 }
 
@@ -28,20 +30,20 @@ function argumentsCalendarArray ()
    $eventTypes = EventTypesQuery::Create()
       ->orderByName()
       ->find();
-      
+
    $lat = OutputUtils::number_dot(ChurchMetaData::getChurchLatitude());
    $lng = OutputUtils::number_dot(ChurchMetaData::getChurchLongitude());
-   
+
    $iLittleMapZoom = SystemConfig::getValue("iLittleMapZoom");
    $sMapProvider   = SystemConfig::getValue('sMapProvider');
    $sGoogleMapKey  = SystemConfig::getValue('sGoogleMapKey');
-      
+
    $paramsArguments = ['sRootPath'   => SystemURLs::getRootPath(),
                        'sRootDocument' => SystemURLs::getDocumentRoot(),
-                       'sPageTitle'  => _('Church Calendar'), 
-                       'eventTypes'  => $eventTypes, 
+                       'sPageTitle'  => _('Church Calendar'),
+                       'eventTypes'  => $eventTypes,
                        'coordinates' => [
-                           'lat' => $lat, 
+                           'lat' => $lat,
                            'lng' => $lng
                         ],
                        'iLittleMapZoom' => $iLittleMapZoom,
@@ -49,6 +51,6 @@ function argumentsCalendarArray ()
                        'sMapProvider'   => $sMapProvider,
                        'sessionUsr'     => SessionUser::getUser()
                       ];
-   
+
    return $paramsArguments;
 }
