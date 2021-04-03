160a82dc1d3afa637b420f2d06ad1b521c35560c
phili67 <philippelogel@free.fr>
2021-04-03 15:55:58 +0200

    api/systemupgrade upgrade and to test

--- src/api/routes/system/system-system-upgrade.php
+++ src/api/routes/system/system-system-upgrade.php
@@ -1,30 +1,34 @@
 <?php
 
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
+
 // Routes
 
 use EcclesiaCRM\SessionUser;
 
-$app->group('/systemupgrade', function () {
-    $this->get('/downloadlatestrelease', function () {
+$app->group('/systemupgrade', function (RouteCollectorProxy $group) {
+    $group->get('/downloadlatestrelease', function (Request $request, Response $response, array $args) {
         $upgradeFile = $this->SystemService->downloadLatestRelease();
-        echo json_encode($upgradeFile);
+        return $response->write(json_encode($upgradeFile));
     });
 
-    $this->post('/doupgrade', function ($request, $response, $args) {
+    $group->post('/doupgrade', function (Request $request, Response $response, $args) {
         $input = (object) $request->getParsedBody();
         $upgradeResult = $this->SystemService->doUpgrade($input->fullPath, $input->sha1);
-        echo json_encode($upgradeResult);
+        return $response->write(json_encode($upgradeResult));
     });
-    
-    $this->post('/isUpdateRequired', function ($request, $response, $args) {
+
+    $group->post('/isUpdateRequired', function (Request $request, Response $response, array $args) {
         if (SessionUser::getUser()->isAdmin() && $_SESSION['isSoftwareUpdateTestPassed'] == false) {
           $isUpdateRequired = $_SESSION['latestVersion'] != null && $_SESSION['latestVersion']['name'] != $_SESSION['sSoftwareInstalledVersion'];
-          $_SESSION['isSoftwareUpdateTestPassed'] = true;        
+          $_SESSION['isSoftwareUpdateTestPassed'] = true;
         } else {
           $isUpdateRequired = 0;
         }
-        
-        echo json_encode(["Upgrade" => $isUpdateRequired,"latestVersion" => $_SESSION['latestVersion'], "installedVersion" => $_SESSION['sSoftwareInstalledVersion']]);
+
+        return $response->withJson(["Upgrade" => $isUpdateRequired,"latestVersion" => $_SESSION['latestVersion'], "installedVersion" => $_SESSION['sSoftwareInstalledVersion']]);
     });
-    
+
 });
