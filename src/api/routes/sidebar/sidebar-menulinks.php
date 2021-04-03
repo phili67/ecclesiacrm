ddf1217afe072dbeabe4c7ce89d879e8402d31e5
phili67 <philippelogel@free.fr>
2021-04-03 16:01:52 +0200

    api/sidebar-menulinks upgrade

--- src/api/routes/sidebar/sidebar-menulinks.php
+++ src/api/routes/sidebar/sidebar-menulinks.php
@@ -14,26 +14,25 @@
  *
  ******************************************************************************/
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\dto\SystemConfig;
 
 use EcclesiaCRM\MenuLink;
 use EcclesiaCRM\MenuLinkQuery;
-use EcclesiaCRM\PersonQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
 use EcclesiaCRM\SessionUser;
 
-$app->group('/menulinks', function () {
+$app->group('/menulinks', function (RouteCollectorProxy $group) {
 
-  $this->post('/{userId:[0-9]+}', 'getMenuLinksForUser' );
-  $this->post('/delete', 'deleteMenuLink' );
-  $this->post('/upaction', 'upMenuLink' );
-  $this->post('/downaction', 'downMenuLink' );  
-  $this->post('/create', 'createMenuLink' );
-  $this->post('/set', 'setMenuLink' );  
-  $this->post('/edit', 'editMenuLink' );  
+  $group->post('/{userId:[0-9]+}', 'getMenuLinksForUser' );
+  $group->post('/delete', 'deleteMenuLink' );
+  $group->post('/upaction', 'upMenuLink' );
+  $group->post('/downaction', 'downMenuLink' );
+  $group->post('/create', 'createMenuLink' );
+  $group->post('/set', 'setMenuLink' );
+  $group->post('/edit', 'editMenuLink' );
 
 });
 
@@ -41,28 +40,28 @@ function getMenuLinksForUser (Request $request, Response $response, array $args)
     if ($args['userId'] == 0 && !SessionUser::getUser()->isMenuOptionsEnabled()) {
             return $response->withStatus(401);
     }
-    
+
     if ($args['userId'] == 0) {
       $menuLinks = MenuLinkQuery::Create()->orderByOrder(Criteria::ASC)->findByPersonId(null);
     } else {
       $menuLinks = MenuLinkQuery::Create()->orderByOrder(Criteria::ASC)->findByPersonId($args['userId']);
     }
-    
+
     $arr = $menuLinks->toArray();
-    
+
     $res = "";
     $place = 0;
-    
+
     $count = count($arr);
-    
+
     foreach ($arr as $elt) {
       $new_elt = "{";
       foreach ($elt as $key => $value) {
         $new_elt .= "\"".$key."\":".json_encode($value).",";
       }
-      
+
       $place++;
-      
+
       if ($place == 1 && $count != 1) {
         $position = "first";
       } else if ($place == $count && $count != 1) {
@@ -72,20 +71,20 @@ function getMenuLinksForUser (Request $request, Response $response, array $args)
       } else {
         $position = "none";
       }
-      
+
       $res .= $new_elt."\"place\":\"".$position."\",\"realplace\":\"".$place."\"},";
     }
-    
-    echo "{\"MenuLinks\":[".substr($res, 0, -1)."]}"; 
+
+    return $response->write("{\"MenuLinks\":[".substr($res, 0, -1)."]}");
   }
-  
-  function deleteMenuLink (Request $request, Response $response, array $args) {    
+
+  function deleteMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if ( isset ($input->MenuLinkId) && SessionUser::getUser()->isMenuOptionsEnabled() ){
       $menuLink = MenuLinkQuery::Create()->findOneById($input->MenuLinkId);
       $place = $menuLink->getOrder();
-      
+
       // we search all the links
       $menuLinks = MenuLinkQuery::Create()->findByPersonId($menuLink->getPersonId());
       $count = $menuLinks->count();
@@ -93,7 +92,7 @@ function getMenuLinksForUser (Request $request, Response $response, array $args)
       if ($menuLink != null) {
         $menuLink->delete();
       }
-      
+
       for ($i = $place+1;$i <= $count-1;$i++) {
         $menuLink = MenuLinkQuery::Create()->findOneByOrder($i);
         if (!is_null($menuLink)) {
@@ -101,79 +100,79 @@ function getMenuLinksForUser (Request $request, Response $response, array $args)
           $menuLink->save();
         }
       }
-      
-      return $response->withJson(['success' => true]); 
-      
-    }   
-    
+
+      return $response->withJson(['success' => true]);
+
+    }
+
     return $response->withJson(['success' => false]);
   }
-  
-  function upMenuLink (Request $request, Response $response, array $args) {    
+
+  function upMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if ( isset($input->PersonID) && isset ($input->MenuLinkId) && isset ($input->MenuPlace) && SessionUser::getUser()->isMenuOptionsEnabled() ){
       if ($input->PersonID == 0) {
         $personID = null;
       } else {
         $personID = $input->PersonID;
       }
-      
+
       // Check if this field is a custom list type.  If so, the list needs to be deleted from list_lst.
       $firstMenu = MenuLinkQuery::Create()->filterByPersonId($personID)->findOneByOrder($input->MenuPlace - 1);
       $firstMenu->setOrder($input->MenuPlace)->save();
-        
+
       $secondFamCus = MenuLinkQuery::Create()->filterByPersonId($personID)->findOneById($input->MenuLinkId);
       $secondFamCus->setOrder($input->MenuPlace - 1)->save();
 
       return $response->withJson(['success' => true]);
     }
-    
+
     return $response->withJson(['success' => false]);
   }
-  
-  function downMenuLink (Request $request, Response $response, array $args) {    
+
+  function downMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if ( isset($input->PersonID) && isset ($input->MenuLinkId) && isset ($input->MenuPlace) && SessionUser::getUser()->isMenuOptionsEnabled() ){
             if ($input->PersonID == 0) {
         $personID = null;
       } else {
         $personID = $input->PersonID;
       }
-      
+
       // Check if this field is a custom list type.  If so, the list needs to be deleted from list_lst.
       $firstMenu = MenuLinkQuery::Create()->filterByPersonId($personID)->findOneByOrder($input->MenuPlace + 1);
       $firstMenu->setOrder($input->MenuPlace)->save();
-        
+
       $secondFamCus = MenuLinkQuery::Create()->filterByPersonId($personID)->findOneById($input->MenuLinkId);
       $secondFamCus->setOrder($input->MenuPlace + 1)->save();
-         
+
       return $response->withJson(['success' => true]);
     }
-    
+
     return $response->withJson(['success' => false]);
   }
-  
-  function createMenuLink (Request $request, Response $response, array $args) {    
+
+  function createMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if (isset ($input->PersonID) && isset ($input->Name) && isset ($input->URI) && SessionUser::getUser()->isMenuOptionsEnabled() ){
       if ($input->PersonID == 0) {
         $menuLinks = MenuLinkQuery::Create()->orderByOrder(Criteria::DESC)->findByPersonId(null);
       } else {
         $menuLinks = MenuLinkQuery::Create()->orderByOrder(Criteria::DESC)->findByPersonId($input->PersonID);
       }
-    
+
       $place = 1;
-    
+
       foreach ($menuLinks as $menuLink) {// get the last Order !!!
          $place = $menuLink->getOrder()+1;
          break;
       }
-    
+
       $menuLink = new MenuLink();
-      
+
       if ($input->PersonID == 0) {
         $menuLink->setPersonId(null);
       } else {
@@ -182,39 +181,39 @@ function getMenuLinksForUser (Request $request, Response $response, array $args)
       $menuLink->setName($input->Name);
       $menuLink->setUri($input->URI);
       $menuLink->setOrder($place);
-      
+
       $menuLink->save();
-      
+
       return $response->withJson(['success' => true]);
     }
-    
+
     return $response->withJson(['success' => false]);
   }
-  
-  function setMenuLink (Request $request, Response $response, array $args) {    
+
+  function setMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if (isset ($input->URI) && isset ($input->MenuLinkId) && isset ($input->Name) && SessionUser::getUser()->isMenuOptionsEnabled() ){
-      
+
       $menuLink = MenuLinkQuery::Create()->findOneById($input->MenuLinkId);
-      
+
       $menuLink->setName($input->Name);
       $menuLink->setUri($input->URI);
-      
+
       $menuLink->save();
-      
+
       return $response->withJson(['success' => true]);
-    }   
-    
+    }
+
     return $response->withJson(['success' => false]);
   }
-  
-  function editMenuLink (Request $request, Response $response, array $args) {    
+
+  function editMenuLink (Request $request, Response $response, array $args) {
     $input = (object)$request->getParsedBody();
-    
+
     if (isset ($input->MenuLinkId) && SessionUser::getUser()->isMenuOptionsEnabled() ){
       return MenuLinkQuery::Create()->findOneById($input->MenuLinkId)->toJSON();
-    }   
-    
+    }
+
     return $response->withJson(['success' => false]);
   }
