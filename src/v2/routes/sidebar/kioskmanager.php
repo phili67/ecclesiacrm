763914193d9a76dcecb10c44c7376a94c29f84be
phili67 <philippelogel@free.fr>
2021-04-03 14:40:51 +0200

    v2/kioskmanager upgrade

--- src/v2/routes/sidebar/kioskmanager.php
+++ src/v2/routes/sidebar/kioskmanager.php
@@ -1,18 +1,17 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\Utils\InputUtils;
 use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\utils\RedirectUtils;
 use EcclesiaCRM\SessionUser;
 
 use Slim\Views\PhpRenderer;
 
-$app->group('/kioskmanager', function () {
-    $this->get('', 'renderKioskManager');
-    $this->get('/', 'renderKioskManager');
+$app->group('/kioskmanager', function (RouteCollectorProxy $group) {
+    $group->get('', 'renderKioskManager');
+    $group->get('/', 'renderKioskManager');
 });
 
 
