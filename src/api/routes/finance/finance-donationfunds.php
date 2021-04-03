9daeb45eff70aab4f67dd33094707dc0f1673c87
phili67 <philippelogel@free.fr>
2021-04-03 16:08:15 +0200

    api/finance-donationfunds upgrade

--- src/api/routes/finance/finance-donationfunds.php
+++ src/api/routes/finance/finance-donationfunds.php
@@ -1,67 +1,66 @@
 <?php
 // Copyright 2018 Philippe Logel all right reserved
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 
 
-use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\dto\SystemConfig;
 use EcclesiaCRM\DonationFundQuery;
 use EcclesiaCRM\DonationFund;
 
-$app->group('/donationfunds', function () {
+$app->group('/donationfunds', function (RouteCollectorProxy $group) {
 
-  $this->post('/', 'getAllDonationFunds' );
-  $this->post('/edit', 'editDonationFund' );
-  $this->post('/set', 'setDonationFund' );
-  $this->post('/delete', 'deleteDonationFund' );
-  $this->post('/create', 'createDonationFund' );
+    $group->post('/', 'getAllDonationFunds' );
+    $group->post('/edit', 'editDonationFund' );
+    $group->post('/set', 'setDonationFund' );
+    $group->post('/delete', 'deleteDonationFund' );
+    $group->post('/create', 'createDonationFund' );
 
 });
 
-function getAllDonationFunds (Request $request, Response $response, array $args) {    
+function getAllDonationFunds (Request $request, Response $response, array $args) {
   return DonationFundQuery::Create()->find()->toJSON();
 }
 
-function editDonationFund (Request $request, Response $response, array $args) {    
+function editDonationFund (Request $request, Response $response, array $args) {
   $donation = (object)$request->getParsedBody();
-  
+
   return DonationFundQuery::Create()->findOneById($donation->fundId)->toJSON();
 }
 
-function setDonationFund (Request $request, Response $response, array $args) {    
+function setDonationFund (Request $request, Response $response, array $args) {
   $fund = (object)$request->getParsedBody();
-  
+
   $donation = DonationFundQuery::Create()->findOneById($fund->fundId);
-  
+
   $donation->setName($fund->Name);
   $donation->setDescription($fund->Description);
   $donation->setActive($fund->Activ);
-  
+
   $donation->save();
-  
-  return json_encode(['status' => "OK"]); 
+
+  return json_encode(['status' => "OK"]);
 }
 
-function deleteDonationFund (Request $request, Response $response, array $args) {    
+function deleteDonationFund (Request $request, Response $response, array $args) {
   $fund = (object)$request->getParsedBody();
-  
+
   $donation = DonationFundQuery::Create()->findOneById($fund->fundId);
   $donation->delete();
-  
-  return json_encode(['status' => "OK"]); 
+
+  return json_encode(['status' => "OK"]);
 }
 
-function createDonationFund (Request $request, Response $response, array $args) {    
+function createDonationFund (Request $request, Response $response, array $args) {
   $fund = (object)$request->getParsedBody();
-  
+
   $donation = new DonationFund();
-  
+
   $donation->setName($fund->Name);
   $donation->setDescription($fund->Description);
   $donation->setActive($fund->Activ);
-  
+
   $donation->save();
-  
-  return json_encode(['status' => "OK"]); 
+
+  return json_encode(['status' => "OK"]);
 }
