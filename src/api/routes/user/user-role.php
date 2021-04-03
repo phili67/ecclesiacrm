4a6a4d3a2684bcbab1a64797a8e5f76bb8d958df
phili67 <philippelogel@free.fr>
2021-04-03 15:21:20 +0200

    user-role upgrade

--- src/api/routes/user/user-role.php
+++ src/api/routes/user/user-role.php
@@ -1,22 +1,22 @@
 <?php
 
 /* copyright 2018 Logel Philippe All right reserved */
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\Person2group2roleP2g2r;
 use EcclesiaCRM\UserRoleQuery;
 use EcclesiaCRM\UserRole;
 use EcclesiaCRM\SessionUser;
 
-$app->group('/userrole', function () {
-  
-    $this->post('/add', 'addUserRole' );
-    $this->post('/get', 'getUserRole' );
-    $this->post('/rename', 'renameUserRole' );
-    $this->post('/getall', 'getAllUserRoles' );
-    $this->post('/delete', 'deleteUserRole' );
-    
+$app->group('/userrole', function (RouteCollectorProxy $group) {
+
+    $group->post('/add', 'addUserRole' );
+    $group->post('/get', 'getUserRole' );
+    $group->post('/rename', 'renameUserRole' );
+    $group->post('/getall', 'getAllUserRoles' );
+    $group->post('/delete', 'deleteUserRole' );
+
 });
 
 
@@ -26,7 +26,7 @@ function addUserRole (Request $request, Response $response, array $args) {
     }
 
     $params = (object)$request->getParsedBody();
-      
+
     if (isset ($params->name) && isset ($params->global) && isset ($params->userPerms) && isset ($params->userValues)) {
       $userCFGs = UserRoleQuery::Create()
         ->filterByName($params->name)
@@ -34,19 +34,19 @@ function addUserRole (Request $request, Response $response, array $args) {
         ->_and()->filterByPermissions($params->userPerms)
         ->_and()->filterByValue($params->userValues)
         ->find();
-        
+
       if ($userCFGs->count()) {
         return $response->withJson(['status' => "error"]);
       }
 
       $userCFG = new UserRole();
-      
+
       $userCFG->setName($params->name);
       $userCFG->setGlobal($params->global);
       $userCFG->setPermissions($params->userPerms);
       $userCFG->setValue($params->userValues);
-      
-      $userCFG->save();          
+
+      $userCFG->save();
     } else {
         throw new \Exception(gettext("POST to UserRole name, global variable, userPerms and userValues"),500);
     }
@@ -59,17 +59,17 @@ function getUserRole (Request $request, Response $response, array $args) {
     }
 
     $params = (object)$request->getParsedBody();
-      
+
     if (isset ($params->roleID)) {
       $userCFG = UserRoleQuery::Create()->findOneById($params->roleID);
-      
+
       return $response->withJson([
         'roleID' => $userCFG->getId(),
         'name' => $userCFG->getName(),
         'global' =>  $userCFG->getGlobal(),
         'usrPerms' =>  $userCFG->getPermissions(),
         'userValues' =>  $userCFG->getValue()
-      ]);                  
+      ]);
     } else {
         throw new \Exception(gettext("POST to UserRole name, global variable, userPerms and userValues"),500);
     }
@@ -82,14 +82,14 @@ function renameUserRole (Request $request, Response $response, array $args) {
     }
 
     $params = (object)$request->getParsedBody();
-      
+
     if (isset ($params->roleID) && isset ($params->name)) {
       $userCFG = UserRoleQuery::Create()->findOneById($params->roleID);
-      
+
       $userCFG->setName($params->name);
-      
+
       $userCFG->save();
-      
+
     } else {
         throw new \Exception(gettext("POST to UserRole name, global variable, userPerms and userValues"),500);
     }
@@ -102,8 +102,8 @@ function getAllUserRoles (Request $request, Response $response, array $args) {
     }
 
     $userCFG = UserRoleQuery::Create()->find();
-      
-    return $response->withJson($userCFG->toArray());                  
+
+    return $response->withJson($userCFG->toArray());
 }
 
 function deleteUserRole (Request $request, Response $response, array $args) {
@@ -112,10 +112,10 @@ function deleteUserRole (Request $request, Response $response, array $args) {
     }
 
     $params = (object)$request->getParsedBody();
-      
+
     if (isset ($params->roleID)) {
       $userCFG = UserRoleQuery::Create()->findOneById($params->roleID);
-      
+
       if (!empty($userCFG)) {
          $userCFG->delete();
       }
