bef283dace945cd499749c69f13d31c15dde5195
phili67 <philippelogel@free.fr>
2021-04-03 15:57:05 +0200

    api/system-issues upgrade

--- src/api/routes/system/system-issues.php
+++ src/api/routes/system/system-issues.php
@@ -1,9 +1,14 @@
 <?php
 
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
 // Routes
 
-$app->post('/issues', function ($request, $response, $args) {
+$app->post('/issues', function (Request $request, Response $response, $args) {
     $input = json_decode($request->getBody());
-    
-    return $this->SystemService->reportIssue($input);
+
+    $SystemService = $this->get('SystemService');
+
+    return $response->withJson($SystemService->reportIssue($input));
 });
