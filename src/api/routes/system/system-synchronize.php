d44dfacd11be19b545b41a93e32eedbdb94d4f61
phili67 <philippelogel@free.fr>
2021-04-03 15:56:35 +0200

    api/system-synchronize upgrade

--- src/api/routes/system/system-synchronize.php
+++ src/api/routes/system/system-synchronize.php
@@ -1,29 +1,33 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
+use Slim\Http\Response as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Routing\RouteCollectorProxy;
+
 use EcclesiaCRM\Service\SynchronizeService;
 
-$app->group('/synchronize', function () {
+$app->group('/synchronize', function (RouteCollectorProxy $group) {
 
 /*
  * @! Returns the dashboard items in function of the current page name : for CRMJsom.js
  * #! param: page->string :: current page name
  */
-    $this->get('/page', function ($request,$response,$args) {
-      $this->cache->withExpires($response, 0);
+    $group->get('/page', function (Request $request, Response $response, array $args) {
+      //$cacheProvider->withExpires($response, 0);
 
       $dataFull = [];
 
-      if ($this->SystemService->getSessionTimeout() < 10) {
-        $dataTimeout = ['timeOut' => 1,'availableTime' =>  $this->SystemService->getSessionTimeout()];
+      $SystemService = $this->get('SystemService');
+
+      if ($SystemService->getSessionTimeout() < 10) {
+        $dataTimeout = ['timeOut' => 1,'availableTime' =>  $SystemService->getSessionTimeout()];
       } else {
-        $dataTimeout = ['timeOut' => 0,'availableTime' =>  $this->SystemService->getSessionTimeout()];
+        $dataTimeout = ['timeOut' => 0,'availableTime' =>  $SystemService->getSessionTimeout()];
       }
 
       array_push ($dataFull,$dataTimeout);
 
-      if ($this->SystemService->getSessionTimeout() > 0) {
+      if ($SystemService->getSessionTimeout() > 0) {
           $pageName = $request->getQueryParam("currentpagename","");
           $DashboardValues = SynchronizeService::getValues($pageName);
           array_push ($dataFull,$DashboardValues);
