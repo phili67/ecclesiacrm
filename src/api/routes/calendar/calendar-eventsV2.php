e6dbcdcc23984b341e555f7ed374a1a024c41a6f
phili67 <philippelogel@free.fr>
2021-04-05 22:59:55 +0200

    src/api/routes/calendar/calendar-eventsV2.php : bug correction

--- src/api/routes/calendar/calendar-eventsV2.php
+++ src/api/routes/calendar/calendar-eventsV2.php
@@ -50,57 +50,57 @@ $app->group('/events', function (RouteCollectorProxy $group) {
     /*
       * @! Get all events for all calendars for a specified range
       */
-    $group->get('/', CalendarEventV2Controller::class . "getAllEvents" );
+    $group->get('/', CalendarEventV2Controller::class . ":getAllEvents" );
     /*
       * @! Get all events after now
       */
-    $group->get('/notDone', CalendarEventV2Controller::class . "getNotDoneEvents" );
+    $group->get('/notDone', CalendarEventV2Controller::class . ":getNotDoneEvents" );
     /*
      * @! Get all events from today
      */
-    $group->get('/numbers', CalendarEventV2Controller::class . "numbersOfEventOfToday") ;
+    $group->get('/numbers', CalendarEventV2Controller::class . ":numbersOfEventOfToday") ;
     /*
      * @! Get all event type
      */
-    $group->get('/types', CalendarEventV2Controller::class . "getEventTypes" );
+    $group->get('/types', CalendarEventV2Controller::class . ":getEventTypes" );
     /*
      * @! Get all event names
      */
-    $group->get('/names', CalendarEventV2Controller::class . "eventNames" );
+    $group->get('/names', CalendarEventV2Controller::class . ":eventNames" );
     /*
      * @! delete event type
      * #! param: id->int  :: type ID
      */
-    $group->post('/deleteeventtype', CalendarEventV2Controller::class . "deleteeventtype" );
+    $group->post('/deleteeventtype', CalendarEventV2Controller::class . ":deleteeventtype" );
     /*
      * @! get event info
      * #! param: id->int  :: event ID
      */
-    $group->post('/info', CalendarEventV2Controller::class . "eventInfo" );
+    $group->post('/info', CalendarEventV2Controller::class . ":eventInfo" );
     /*
     * @! Set a person for the event + check
     * #! param: id->int  :: event ID
     * #! param: id->int  :: person ID
     */
-    $group->post('/person', CalendarEventV2Controller::class . "personCheckIn" );
+    $group->post('/person', CalendarEventV2Controller::class . ":personCheckIn" );
     /*
     * @! Set the group persons for the event + check
     * #! param: id->int  :: event ID
     * #! param: id->int  :: group ID
     */
-    $group->post('/group', CalendarEventV2Controller::class . "groupCheckIn" );
+    $group->post('/group', CalendarEventV2Controller::class . ":groupCheckIn" );
     /*
     * @! Set the family persons for the event + check
     * #! param: id->int  :: event ID
     * #! param: id->int  :: family ID
     */
-    $group->post('/family', CalendarEventV2Controller::class . "familyCheckIn" );
+    $group->post('/family', CalendarEventV2Controller::class . ":familyCheckIn" );
     /*
     * @! get event count
     * #! param: id->int  :: event ID
     * #! param: id->int  :: type ID
     */
-    $group->post('/attendees', CalendarEventV2Controller::class . "eventCount" );
+    $group->post('/attendees', CalendarEventV2Controller::class . ":eventCount" );
     /*
     * @! manage an event eventAction, [createEvent,moveEvent,resizeEvent,attendeesCheckinEvent,suppress,modifyEvent]
     * #! param: id->int       :: eventID
@@ -111,7 +111,7 @@ $app->group('/events', function (RouteCollectorProxy $group) {
     * #! param: ref->start    :: the end date : YYYY-MM-DD
     * #! param: ref->location :: location
     */
-    $group->post('/', CalendarEventV2Controller::class . "manageEvent" );
+    $group->post('/', CalendarEventV2Controller::class . ":manageEvent" );
 
 });
 
