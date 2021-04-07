287ab14727ca5d5bf8eab37ac99a96deb6c21175
phili67 <philippelogel@free.fr>
2021-04-07 08:28:38 +0200

    src/api/routes/geocoder.php : GeocoderController

--- src/api/routes/geocoder.php
+++ src/api/routes/geocoder.php
@@ -1,29 +1,43 @@
 <?php
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 
 use EcclesiaCRM\Utils\GeoUtils;
 
 $app->group('/geocoder', function (RouteCollectorProxy $group) {
-    $group->post('/address', 'getGeoLocals' );
-    $group->post('/address/', 'getGeoLocals' );
+    $group->post('/address', GeocoderController::class . ':getGeoLocals' );
+    $group->post('/address/', GeocoderController::class . ':getGeoLocals' );
 });
 
+class GeocoderController
+{
+    private $container;
 
-/**
- * A method that return GeoLocation based on an address.
- *
- * @param \Slim\Http\Request $p_request   The request.
- * @param \Slim\Http\Response $p_response The response.
- * @param array $p_args Arguments
- * @return \Slim\Http\Response The augmented response.
- */
-function getGeoLocals(Request $request, Response $response, array $p_args ) {
-    $input = json_decode($request->getBody());
-    if (!empty($input)) {
-        return $response->withJson(GeoUtils::getLatLong($input->address));
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
     }
-    return $response->withStatus(404);
+
+    /**
+     * A method that return GeoLocation based on an address.
+     *
+     * @param \Slim\Http\Request $p_request   The request.
+     * @param \Slim\Http\Response $p_response The response.
+     * @param array $p_args Arguments
+     * @return \Slim\Http\Response The augmented response.
+     */
+    function getGeoLocals (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = json_decode($request->getBody());
+        if (!empty($input)) {
+            return $response->withJson(GeoUtils::getLatLong($input->address));
+        }
+        return $response->withStatus(404);
+    }
 }
+
+
