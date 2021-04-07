f19f173bb10fe926584b2dc1e58d39cf13b0c70f
phili67 <philippelogel@free.fr>
2021-04-07 09:09:55 +0200

    src/api/routes/documents/documents-ckeditor.php : DocumentCKEditorController

--- src/api/routes/documents/documents-ckeditor.php
+++ src/api/routes/documents/documents-ckeditor.php
@@ -8,9 +8,12 @@
 //
 
 // CKeditor APIs
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 
 use EcclesiaCRM\CKEditorTemplatesQuery;
 use EcclesiaCRM\CKEditorTemplates;
@@ -20,31 +23,40 @@ use EcclesiaCRM\SessionUser;
 
 $app->group('/ckeditor', function (RouteCollectorProxy $group) {
 
-    $group->get('/{personId:[0-9]+}/templates', 'templates' );
-    $group->post('/alltemplates', 'alltemplates' );
-    $group->post('/deletetemplate', 'deleteTemplate' );
-    $group->post('/renametemplate', 'renametemplate' );
-    $group->post('/savetemplate', 'saveTemplate' );
-    $group->post('/saveAsWordFile', 'saveAsWordFile' );
+    $group->get('/{personId:[0-9]+}/templates', DocumentCKEditorController::class . ':templates' );
+    $group->post('/alltemplates', DocumentCKEditorController::class . ':alltemplates' );
+    $group->post('/deletetemplate', DocumentCKEditorController::class . ':deleteTemplate' );
+    $group->post('/renametemplate', DocumentCKEditorController::class . ':renametemplate' );
+    $group->post('/savetemplate', DocumentCKEditorController::class . ':saveTemplate' );
+    $group->post('/saveAsWordFile', DocumentCKEditorController::class . ':saveAsWordFile' );
 
 });
 
-function templates (Request $request, Response $response, array $args) {
-  $templates = CKEditorTemplatesQuery::Create()->findByPersonID($args['personId']);
+class DocumentCKEditorController
+{
+    private $container;
 
-  $templatesArr = [];
-  foreach ($templates as $template) {
-    $elt = ['title' => $template->getTitle(),
-    'description' => $template->getDesc(),
-    'html' => $template->getText(),
-    'image' => $template->getImage(),
-    'id' => $template->getId()];
-    array_push($templatesArr, $elt);
-  }
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
-  $the_real_templates = json_encode($templatesArr);
+    public function templates (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $templates = CKEditorTemplatesQuery::Create()->findByPersonID($args['personId']);
 
-  return "// Register a template definition set named \"default\".
+        $templatesArr = [];
+        foreach ($templates as $template) {
+            $elt = ['title' => $template->getTitle(),
+                'description' => $template->getDesc(),
+                'html' => $template->getText(),
+                'image' => $template->getImage(),
+                'id' => $template->getId()];
+            array_push($templatesArr, $elt);
+        }
+
+        $the_real_templates = json_encode($templatesArr);
+
+        return "// Register a template definition set named \"default\".
 CKEDITOR.addTemplates( 'default',
 {
   // The name of the subfolder that contains the preview images of the templates.
@@ -53,127 +65,129 @@ CKEDITOR.addTemplates( 'default',
   // Template definitions.
   templates :".$the_real_templates."
 });";
-}
-
-
-function alltemplates (Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
-
-  if ( isset ($input->personID) ) {
-    $templates = CKEditorTemplatesQuery::Create()->findByPersonID($input->personID);
-
-    $templatesArr = [];
-    foreach ($templates as $template) {
-      $elt = ['title' => $template->getTitle(),
-      'description' => $template->getDesc(),
-      'html' => $template->getText(),
-      'image' => $template->getImage(),
-      'id' => $template->getId()];
-      array_push($templatesArr, $elt);
     }
 
-    return $response->withJson($templatesArr);
-  }
+    public function alltemplates (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
 
-  return $response->withJson(['status' => 'failed']);
-}
+        if ( isset ($input->personID) ) {
+            $templates = CKEditorTemplatesQuery::Create()->findByPersonID($input->personID);
 
-function deleteTemplate (Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
+            $templatesArr = [];
+            foreach ($templates as $template) {
+                $elt = ['title' => $template->getTitle(),
+                    'description' => $template->getDesc(),
+                    'html' => $template->getText(),
+                    'image' => $template->getImage(),
+                    'id' => $template->getId()];
+                array_push($templatesArr, $elt);
+            }
 
-  if ( isset ($input->templateID) ) {
-    $template = CKEditorTemplatesQuery::Create()->findOneByID($input->templateID);
+            return $response->withJson($templatesArr);
+        }
 
-    $template->delete();
-
-    return $response->withJson(['status' => 'success']);
-  }
-
-  return $response->withJson(['status' => 'failed']);
-}
-
-function renameTemplate (Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
-
-  if ( isset ($input->templateID) && isset ($input->title) && isset ($input->desc) ) {
-    $template = CKEditorTemplatesQuery::Create()->findOneByID($input->templateID);
-
-    $template->setTitle($input->title);
-    $template->setDesc($input->desc);
-    $template->setImage("template".rand(1, 3).".gif");
-
-    $template->save();
-
-    return $response->withJson(['status' => 'success']);
-  }
-
-  return $response->withJson(['status' => 'failed']);
-}
-
-function saveTemplate(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
-
-  if ( isset ($input->personID) && isset ($input->title) && isset ($input->desc) && isset ($input->text) ) {
-    $template = CKEditorTemplatesQuery::Create()->filterByTitle ($input->title)->findOneByDesc ($input->desc);
-
-    if (!is_null ($template)) {
-      $template->setText($input->text);
-      $template->save();
-    } else {
-      $template = new CKEditorTemplates();
-
-      $template->setPersonId($input->personID);
-      $template->setTitle($input->title);
-      $template->setDesc($input->desc);
-      $template->setText($input->text);
-      $template->setImage("template".rand(1, 3).".gif");
-
-      $template->save();
+        return $response->withJson(['status' => 'failed']);
     }
 
-    return $response->withJson(['status' => 'success']);
-  }
+    public function deleteTemplate (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
 
-  return $response->withJson(['status' => 'failed']);
-}
+        if ( isset ($input->templateID) ) {
+            $template = CKEditorTemplatesQuery::Create()->findOneByID($input->templateID);
 
-function saveAsWordFile ($request, $res, $args) {
-  $input = (object)$request->getParsedBody();
+            $template->delete();
 
-  if ( isset ($input->personID) && isset ($input->title) && isset ($input->text) ) {
-    $user = UserQuery::create()->findPk($input->personID);
+            return $response->withJson(['status' => 'success']);
+        }
 
-    if ( !is_null($user) ) {
-        $realNoteDir = $userDir = $user->getUserRootDir();
-        $userName    = $user->getUserName();
-        $currentpath = $user->getCurrentpath();
-
-        $pw = new \PhpOffice\PhpWord\PhpWord();
-
-        // [THE HTML]
-        $section = $pw->addSection();
-        \PhpOffice\PhpWord\Shared\Html::addHtml($section, $input->text, false, false);
-
-        // [SAVE FILE ON THE SERVER]
-        $tmpFile = dirname(__FILE__)."/../../../".$realNoteDir."/".$userName.$currentpath.$input->title.".docx";
-        $pw->save($tmpFile, "Word2007");
-
-        // now we create the note
-        $note = new Note();
-        $note->setPerId($input->personID);
-        $note->setFamId(0);
-        $note->setTitle($fileName);
-        $note->setPrivate(1);
-        $note->setText($userName . $currentpath . $input->title.".docx");
-        $note->setType('file');
-        $note->setEntered(SessionUser::getUser()->getPersonId());
-        $note->setInfo(gettext('Create file'));
-
-        $note->save();
-
-        return $res->withJson(['success' => $tmpFile ]);
+        return $response->withJson(['status' => 'failed']);
     }
-  }
 
-  return $res->withJson(['success' => false]);
+    public function renameTemplate (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
+
+        if ( isset ($input->templateID) && isset ($input->title) && isset ($input->desc) ) {
+            $template = CKEditorTemplatesQuery::Create()->findOneByID($input->templateID);
+
+            $template->setTitle($input->title);
+            $template->setDesc($input->desc);
+            $template->setImage("template".rand(1, 3).".gif");
+
+            $template->save();
+
+            return $response->withJson(['status' => 'success']);
+        }
+
+        return $response->withJson(['status' => 'failed']);
+    }
+
+    public function saveTemplate(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
+
+        if ( isset ($input->personID) && isset ($input->title) && isset ($input->desc) && isset ($input->text) ) {
+            $template = CKEditorTemplatesQuery::Create()->filterByTitle ($input->title)->findOneByDesc ($input->desc);
+
+            if (!is_null ($template)) {
+                $template->setText($input->text);
+                $template->save();
+            } else {
+                $template = new CKEditorTemplates();
+
+                $template->setPersonId($input->personID);
+                $template->setTitle($input->title);
+                $template->setDesc($input->desc);
+                $template->setText($input->text);
+                $template->setImage("template".rand(1, 3).".gif");
+
+                $template->save();
+            }
+
+            return $response->withJson(['status' => 'success']);
+        }
+
+        return $response->withJson(['status' => 'failed']);
+    }
+
+    public function saveAsWordFile (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
+
+        if ( isset ($input->personID) && isset ($input->title) && isset ($input->text) ) {
+            $user = UserQuery::create()->findPk($input->personID);
+
+            if ( !is_null($user) ) {
+                $realNoteDir = $userDir = $user->getUserRootDir();
+                $userName    = $user->getUserName();
+                $currentpath = $user->getCurrentpath();
+
+                $pw = new \PhpOffice\PhpWord\PhpWord();
+
+                // [THE HTML]
+                $section = $pw->addSection();
+                \PhpOffice\PhpWord\Shared\Html::addHtml($section, $input->text, false, false);
+
+                // [SAVE FILE ON THE SERVER]
+                $tmpFile = dirname(__FILE__)."/../../../".$realNoteDir."/".$userName.$currentpath.$input->title.".docx";
+                $pw->save($tmpFile, "Word2007");
+
+                // now we create the note
+                $note = new Note();
+                $note->setPerId($input->personID);
+                $note->setFamId(0);
+                $note->setTitle($tmpFile);
+                $note->setPrivate(1);
+                $note->setText($userName . $currentpath . $input->title.".docx");
+                $note->setType('file');
+                $note->setEntered(SessionUser::getUser()->getPersonId());
+                $note->setInfo(gettext('Create file'));
+
+                $note->save();
+
+                return $response->withJson(['success' => $tmpFile ]);
+            }
+        }
+
+        return $response->withJson(['success' => false]);
+    }
 }
+
+
