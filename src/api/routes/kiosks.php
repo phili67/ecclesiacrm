e2818e38d1417549507456a94f6eed919e5c53ff
phili67 <philippelogel@free.fr>
2021-04-07 08:29:04 +0200

    src/api/routes/kiosks.php

--- src/api/routes/kiosks.php
+++ src/api/routes/kiosks.php
@@ -4,117 +4,139 @@ use Slim\Http\Response as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+
 use EcclesiaCRM\dto\SystemConfig;
 use EcclesiaCRM\KioskDeviceQuery;
 use Propel\Runtime\ActiveQuery\Criteria;
 
 $app->group('/kiosks', function (RouteCollectorProxy $group) {
 
-    $group->get('/', 'getKioskDevices' );
-    $group->post('/allowRegistration', 'allowDeviceRegistration' );
-    $group->post('/{kioskId:[0-9]+}/reloadKiosk', 'reloadKiosk' );
-    $group->post('/{kioskId:[0-9]+}/identifyKiosk', 'identifyKiosk' );
-    $group->post('/{kioskId:[0-9]+}/acceptKiosk', 'acceptKiosk' );
-    $group->post('/{kioskId:[0-9]+}/setAssignment', 'setKioskAssignment' );
-    $group->delete('/{kioskId:[0-9]+}', 'deleteKiosk' );
+    $group->get('/', KiosksController::class . ':getKioskDevices' );
+    $group->post('/allowRegistration', KiosksController::class . ':allowDeviceRegistration' );
+    $group->post('/{kioskId:[0-9]+}/reloadKiosk', KiosksController::class . ':reloadKiosk' );
+    $group->post('/{kioskId:[0-9]+}/identifyKiosk', KiosksController::class . ':identifyKiosk' );
+    $group->post('/{kioskId:[0-9]+}/acceptKiosk', KiosksController::class . ':acceptKiosk' );
+    $group->post('/{kioskId:[0-9]+}/setAssignment', KiosksController::class . ':setKioskAssignment' );
+    $group->delete('/{kioskId:[0-9]+}', KiosksController::class . ':deleteKiosk' );
 
 });
 
-function deleteKiosk (Request $request, Response $response, array $args)
+class KiosksController
 {
-    $kioskId = $args['kioskId'];
+    private $container;
 
-    $kiosk = KioskDeviceQuery::create()
-        ->findOneById($kioskId);
-
-    if ( !is_null ($kiosk) ) {
-        foreach ($kiosk->getKioskAssignments() as $kioskAssignment) {
-            $kioskAssignment->delete();
-        }
-
-        $kiosk->delete();
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
     }
 
-    return $response->withJson(["status" => "success"]);
-}
+    public function deleteKiosk(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $kioskId = $args['kioskId'];
 
-function getKioskDevices (Request $request, Response $response, array $args) {
-    $Kiosks = KioskDeviceQuery::create()
+        $kiosk = KioskDeviceQuery::create()
+            ->findOneById($kioskId);
+
+        if (!is_null($kiosk)) {
+            foreach ($kiosk->getKioskAssignments() as $kioskAssignment) {
+                $kioskAssignment->delete();
+            }
+
+            $kiosk->delete();
+        }
+
+        return $response->withJson(["status" => "success"]);
+    }
+
+    public function getKioskDevices(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $Kiosks = KioskDeviceQuery::create()
             ->joinWithKioskAssignment(Criteria::LEFT_JOIN)
             ->useKioskAssignmentQuery()
-              ->joinWithEvent(Criteria::LEFT_JOIN)
+            ->joinWithEvent(Criteria::LEFT_JOIN)
             ->endUse()
             ->find();
 
-    $return = [];
+        $return = [];
 
-    foreach ($Kiosks as $kiosk){
-        $values['Id'] = $kiosk->getID();
-        $values['GUIDHash'] = $kiosk->getGUIDHash();
-        $values['Name'] = $kiosk->getName();
-        $values['DeviceType'] = $kiosk->getDeviceType();
-        $values['LastHeartbeat'] = $kiosk->getLastHeartbeat();
-        $values['Accepted'] = $kiosk->getAccepted();
-        $values['PendingCommands'] = $kiosk->getPendingCommands();
+        foreach ($Kiosks as $kiosk) {
+            $values['Id'] = $kiosk->getID();
+            $values['GUIDHash'] = $kiosk->getGUIDHash();
+            $values['Name'] = $kiosk->getName();
+            $values['DeviceType'] = $kiosk->getDeviceType();
+            $values['LastHeartbeat'] = $kiosk->getLastHeartbeat();
+            $values['Accepted'] = $kiosk->getAccepted();
+            $values['PendingCommands'] = $kiosk->getPendingCommands();
 
-        $KioskAssignments = [];
+            $KioskAssignments = [];
 
-        foreach ($kiosk->getKioskAssignments() as $kioskAssignment) {
-            $KioskAssignments_values['Id'] = $kioskAssignment->getId();
-            $KioskAssignments_values['KioskId'] = $kioskAssignment->getKioskId();
-            $KioskAssignments_values['AssignmentType'] = $kioskAssignment->getAssignmentType();
-            $KioskAssignments_values['EventId'] = $kioskAssignment->getEventId();
-            $KioskAssignments_values['KioskDevice'] = $kioskAssignment->getKioskDevice();
+            foreach ($kiosk->getKioskAssignments() as $kioskAssignment) {
+                $KioskAssignments_values['Id'] = $kioskAssignment->getId();
+                $KioskAssignments_values['KioskId'] = $kioskAssignment->getKioskId();
+                $KioskAssignments_values['AssignmentType'] = $kioskAssignment->getAssignmentType();
+                $KioskAssignments_values['EventId'] = $kioskAssignment->getEventId();
+                $KioskAssignments_values['KioskDevice'] = $kioskAssignment->getKioskDevice();
 
-            array_push($KioskAssignments, $KioskAssignments_values);
+                array_push($KioskAssignments, $KioskAssignments_values);
+            }
+
+            $values['KioskAssignments'] = $KioskAssignments;
+
+
+            array_push($return, $values);
         }
 
-        $values['KioskAssignments'] = $KioskAssignments;
-
-
-        array_push($return, $values);
+        return $response->withJson(["KioskDevices" => $return]);
     }
 
-    return $response->withJson(["KioskDevices" => $return]);
-}
+    public function allowDeviceRegistration(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $window = new DateTime();
+        $window->add(new DateInterval("PT05S"));
+        SystemConfig::setValue("sKioskVisibilityTimestamp", $window->format('Y-m-d H:i:s'));
+        return $response->write(json_encode(array("visibleUntil" => $window)));
+    }
 
-function allowDeviceRegistration (Request $request, Response $response, array $args) {
-    $window =new DateTime();
-    $window->add(new DateInterval("PT05S"));
-    SystemConfig::setValue("sKioskVisibilityTimestamp",$window->format('Y-m-d H:i:s'));
-    return $response->write(json_encode(array("visibleUntil"=>$window)));
-}
-
-function reloadKiosk (Request $request, Response $response, array $args) {
-    $kioskId = $args['kioskId'];
-    $reload = KioskDeviceQuery::create()
+    public function reloadKiosk(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $kioskId = $args['kioskId'];
+        $reload = KioskDeviceQuery::create()
             ->findOneById($kioskId)
             ->reloadKiosk();
-    return $response->write(json_encode($reload));
-}
+        return $response->write(json_encode($reload));
+    }
 
-function identifyKiosk (Request $request, Response $response, array $args) {
-    $kioskId = $args['kioskId'];
-    $identify = KioskDeviceQuery::create()
+    public function identifyKiosk(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $kioskId = $args['kioskId'];
+        $identify = KioskDeviceQuery::create()
             ->findOneById($kioskId)
             ->identifyKiosk();
-    return $response->write(json_encode($identify));
-}
+        return $response->write(json_encode($identify));
+    }
 
-function acceptKiosk (Request $request, Response $response, array $args) {
-    $kioskId = $args['kioskId'];
-    $accept = KioskDeviceQuery::create()
+    public function acceptKiosk(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $kioskId = $args['kioskId'];
+        $accept = KioskDeviceQuery::create()
             ->findOneById($kioskId)
             ->setAccepted(true)
             ->save();
-    return $response->write(json_encode($accept));
-}
+        return $response->write(json_encode($accept));
+    }
 
-function setKioskAssignment (Request $request, Response $response, array $args) {
-    $kioskId = $args['kioskId'];
-    $input = (object) $request->getParsedBody();
-    $accept = KioskDeviceQuery::create()
+    public function setKioskAssignment(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $kioskId = $args['kioskId'];
+        $input = (object)$request->getParsedBody();
+        $accept = KioskDeviceQuery::create()
             ->findOneById($kioskId)
             ->setAssignment($input->assignmentType, $input->eventId);
-    return $response->write(json_encode($accept));
+        return $response->write(json_encode($accept));
+    }
+
 }
