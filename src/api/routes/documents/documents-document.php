fb9506368acdc93e7ddfb5908024ea3780642e90
phili67 <philippelogel@free.fr>
2021-04-07 13:20:24 +0200

    src/api/routes/documents/documents-document.php  : DocumentDocumentController

--- src/api/routes/documents/documents-document.php
+++ src/api/routes/documents/documents-document.php
@@ -1,9 +1,12 @@
 <?php
 
-// copyright 2019 Philippe Logel All rights reserved not MIT licence
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
+// copyright 2021 Philippe Logel All rights reserved not MIT licence
 use Slim\Routing\RouteCollectorProxy;
+
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 
 // Documents filemanager APIs
 use EcclesiaCRM\Note;
@@ -14,145 +17,156 @@ use EcclesiaCRM\PersonQuery;
 
 $app->group('/document', function (RouteCollectorProxy $group) {
 
-    $group->post('/create', 'createDocument' );
-    $group->post('/get', 'getDocument' );
-    $group->post('/update', 'updateDocument' );
-    $group->post('/delete', 'deleteDocument' );
-    $group->post('/leave', 'leaveDocument' );
+    $group->post('/create', DocumentDocumentController::class . ':createDocument' );
+    $group->post('/get', DocumentDocumentController::class . ':getDocument' );
+    $group->post('/update', DocumentDocumentController::class . ':updateDocument' );
+    $group->post('/delete', DocumentDocumentController::class . ':deleteDocument' );
+    $group->post('/leave', DocumentDocumentController::class . ':leaveDocument' );
 
 });
 
-function createDocument(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
+class DocumentDocumentController
+{
+    private $container;
 
-  if ( isset ($input->personID) && isset ($input->famID) && isset ($input->title) && isset ($input->type) && isset ($input->text) && isset ($input->bPrivate)){
-    $note = new Note();
-    $note->setPerId($input->personID);
-    $note->setFamId($input->famID);
-    $note->setPrivate($input->bPrivate);
-    $note->setTitle($input->title);
-    $note->setText($input->text);
-    $note->setType($input->type);
-    $note->setEntered(SessionUser::getUser()->getPersonId());
-    $note->setDateLastEdited(new DateTime());
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
 
-    $note->setCurrentEditedBy(0);
-    $note->setCurrentEditedDate(NULL);
+    public function createDocument(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
 
-    $note->save();
+        if ( isset ($input->personID) && isset ($input->famID) && isset ($input->title) && isset ($input->type) && isset ($input->text) && isset ($input->bPrivate)){
+            $note = new Note();
+            $note->setPerId($input->personID);
+            $note->setFamId($input->famID);
+            $note->setPrivate($input->bPrivate);
+            $note->setTitle($input->title);
+            $note->setText($input->text);
+            $note->setType($input->type);
+            $note->setEntered(SessionUser::getUser()->getPersonId());
+            $note->setDateLastEdited(new DateTime());
 
-    return $response->withJson(['success' => true]);
-  }
+            $note->setCurrentEditedBy(0);
+            $note->setCurrentEditedDate(NULL);
 
-  return $response->withJson(['success' => false]);
-}
+            $note->save();
 
-function getDocument(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
-
-  if ( isset ($input->docID) && isset ($input->personID) && isset ($input->famID) ){
-    $note = NoteQuery::Create()->findOneById ($input->docID);
-
-    if ( $note->getCurrentEditedBy() > 0 && !( SessionUser::getUser()->isAdmin() || $note->isVisualableBy ($input->personID) ) ) {
-      $currentDate = new DateTime();
-
-      $since_start = $currentDate->diff($note->getCurrentEditedDate());
-
-      $min = $since_start->days * 24 * 60;
-      $min += $since_start->h * 60;
-      $min += $since_start->i;
-
-      if ( $min < SystemConfig::getValue('iDocumentTimeLeft') ) {
-        $editor = PersonQuery::create()->findPk($note->getCurrentEditedBy());
-        if ($editor != null) {
-            $currentUserName = gettext("This document is opened by")." : ".$editor->getFullName()." (".(SystemConfig::getValue('iDocumentTimeLeft')-$min)." ".gettext("Minutes left").")";
+            return $response->withJson(['success' => true]);
         }
 
-        return $response->withJson(['success' => false ,'note' => $note->toArray(),'message' => $currentUserName]);
-
-      } else {// we reset the count
-         $note->setCurrentEditedDate(null);
-         $note->setCurrentEditedBy(0);
-         $note->save();
-      }
-    } else {
-
-      // now the document is used by someone
-      $note->setCurrentEditedBy(SessionUser::getUser()->getPersonId());
-      $note->setCurrentEditedDate(new DateTime());
-
-      $note->save();
-      // !now the document is used by someone
-
+        return $response->withJson(['success' => false]);
     }
 
-    return $response->withJson(['success' => true ,'note' => $note->toArray()]);
-  }
+    public function getDocument(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
 
-  return $response->withJson(['success' => false]);
-}
+        if ( isset ($input->docID) && isset ($input->personID) && isset ($input->famID) ){
+            $note = NoteQuery::Create()->findOneById ($input->docID);
 
-function leaveDocument(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
+            if ( $note->getCurrentEditedBy() > 0 && !( SessionUser::getUser()->isAdmin() || $note->isVisualableBy ($input->personID) ) ) {
+                $currentDate = new DateTime();
 
-  if ( isset ($input->docID) ){
-    $note = NoteQuery::Create()->findOneById ($input->docID);
+                $since_start = $currentDate->diff($note->getCurrentEditedDate());
 
-    if (!is_null($note)) {
+                $min = $since_start->days * 24 * 60;
+                $min += $since_start->h * 60;
+                $min += $since_start->i;
 
-      // now the document is used by someone
-      $note->setCurrentEditedBy(0);
-      $note->setCurrentEditedDate(NULL);
+                if ( $min < SystemConfig::getValue('iDocumentTimeLeft') ) {
+                    $editor = PersonQuery::create()->findPk($note->getCurrentEditedBy());
+                    if ($editor != null) {
+                        $currentUserName = gettext("This document is opened by")." : ".$editor->getFullName()." (".(SystemConfig::getValue('iDocumentTimeLeft')-$min)." ".gettext("Minutes left").")";
+                    }
 
-      $note->save();
-      // !now the document is used by someone
+                    return $response->withJson(['success' => false ,'note' => $note->toArray(),'message' => $currentUserName]);
 
-      return $response->withJson(['success' => true ,'note' => $note->toArray()]);
+                } else {// we reset the count
+                    $note->setCurrentEditedDate(null);
+                    $note->setCurrentEditedBy(0);
+                    $note->save();
+                }
+            } else {
+
+                // now the document is used by someone
+                $note->setCurrentEditedBy(SessionUser::getUser()->getPersonId());
+                $note->setCurrentEditedDate(new DateTime());
+
+                $note->save();
+                // !now the document is used by someone
+
+            }
+
+            return $response->withJson(['success' => true ,'note' => $note->toArray()]);
+        }
+
+        return $response->withJson(['success' => false]);
     }
 
-    return $response->withJson(['success' => true]);
-  }
+    public function leaveDocument(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
 
-  return $response->withJson(['success' => false]);
+        if ( isset ($input->docID) ){
+            $note = NoteQuery::Create()->findOneById ($input->docID);
+
+            if (!is_null($note)) {
+
+                // now the document is used by someone
+                $note->setCurrentEditedBy(0);
+                $note->setCurrentEditedDate(NULL);
+
+                $note->save();
+                // !now the document is used by someone
+
+                return $response->withJson(['success' => true ,'note' => $note->toArray()]);
+            }
+
+            return $response->withJson(['success' => true]);
+        }
+
+        return $response->withJson(['success' => false]);
+    }
+
+    public function updateDocument(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
+
+        if ( isset ($input->docID) && isset ($input->title) && isset ($input->type) && isset ($input->text) && isset ($input->bPrivate)){
+            $note = NoteQuery::Create()->findOneById ($input->docID);
+
+            $note->setPrivate($input->bPrivate);
+            $note->setTitle($input->title);
+            $note->setText($input->text);
+            $note->setType($input->type);
+            $note->setEntered(SessionUser::getUser()->getPersonId());
+            $note->setDateLastEdited(new DateTime());
+
+            // now the document is no more used
+            $note->setCurrentEditedBy(0);
+            $note->setCurrentEditedDate(NULL);
+            // ! now the document is no more used
+
+            $note->save();
+
+            return $response->withJson(['success' => true,'note' => $note->toArray()]);
+        }
+
+        return $response->withJson(['success' => false]);
+    }
+
+    public function deleteDocument(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $input = (object)$request->getParsedBody();
+
+        if ( isset ($input->docID) ){
+            $note = NoteQuery::Create()->findOneById ($input->docID);
+
+            $note->delete();
+
+            return $response->withJson(['success']);
+        }
+
+        return $response->withJson(['success' => false]);
+    }
 }
 
-function updateDocument(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
 
-  if ( isset ($input->docID) && isset ($input->title) && isset ($input->type) && isset ($input->text) && isset ($input->bPrivate)){
-    $note = NoteQuery::Create()->findOneById ($input->docID);
-
-    $note->setPrivate($input->bPrivate);
-    $note->setTitle($input->title);
-    $note->setText($input->text);
-    $note->setType($input->type);
-    $note->setEntered(SessionUser::getUser()->getPersonId());
-    $note->setDateLastEdited(new DateTime());
-
-    // now the document is no more used
-    $note->setCurrentEditedBy(0);
-    $note->setCurrentEditedDate(NULL);
-    // ! now the document is no more used
-
-    $note->save();
-
-    return $response->withJson(['success' => true,'note' => $note->toArray()]);
-  }
-
-  return $response->withJson(['success' => false]);
-}
-
-
-function deleteDocument(Request $request, Response $response, array $args) {
-  $input = (object)$request->getParsedBody();
-
-  if ( isset ($input->docID) ){
-    $note = NoteQuery::Create()->findOneById ($input->docID);
-
-    $note->delete();
-
-    return $response->withJson(['success']);
-  }
-
-  return $response->withJson(['success' => false]);
-}
