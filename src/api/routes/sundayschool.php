7f54ae5f1e7fac38d4a92158e70f12eec522ce18
phili67 <philippelogel@free.fr>
2021-04-07 08:29:51 +0200

    src/api/routes/sundayschool.php : SundaySchoolController

--- src/api/routes/sundayschool.php
+++ src/api/routes/sundayschool.php
@@ -9,62 +9,75 @@
 *
 ******************************************************************************/
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\Service\SundaySchoolService;
+use Psr\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
 use EcclesiaCRM\dto\Cart;
 
 // Routes sundayschool
 $app->group('/sundayschool', function (RouteCollectorProxy $group) {
 
-    $group->post('/getallstudents/{groupId:[0-9]+}', 'getallstudentsForGroup' );
-    $group->post('/getAllGendersForDonut/{groupId:[0-9]+}', 'getAllGendersForDonut' );
-    $group->post('/getAllStudentsForChart/{groupId:[0-9]+}', 'getAllStudentsForChart' );
+    $group->post('/getallstudents/{groupId:[0-9]+}', SundaySchoolController::class . ':getallstudentsForGroup' );
+    $group->post('/getAllGendersForDonut/{groupId:[0-9]+}', SundaySchoolController::class . ':getAllGendersForDonut' );
+    $group->post('/getAllStudentsForChart/{groupId:[0-9]+}', SundaySchoolController::class . ':getAllStudentsForChart' );
 
 });
 
+class SundaySchoolController
+{
+    private $container;
 
-function getallstudentsForGroup (Request $request, Response $response, array $args) {
-  $sundaySchoolService = new SundaySchoolService();
-
-  $thisClassChildren = $sundaySchoolService->getKidsFullDetails($args['groupId']);
-
-  $result = [];
-  foreach ($thisClassChildren as $children) {
-    if (Cart::PersonInCart($children['kidId'])) {
-      $children['inCart']=1;
-    } else {
-      $children['inCart']=0;
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
     }
 
-    $result[] = $children;
-  }
+    public function getallstudentsForGroup (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $sundaySchoolService = $this->container->get('SundaySchoolService');
 
-    return $response->write('{"ClassroomStudents":'.json_encode($result)."}");
+        $thisClassChildren = $sundaySchoolService->getKidsFullDetails($args['groupId']);
+
+        $result = [];
+        foreach ($thisClassChildren as $children) {
+            if (Cart::PersonInCart($children['kidId'])) {
+                $children['inCart']=1;
+            } else {
+                $children['inCart']=0;
+            }
+
+            $result[] = $children;
+        }
+
+        return $response->write('{"ClassroomStudents":'.json_encode($result)."}");
+    }
+
+    public function getAllGendersForDonut (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $sundaySchoolService = $this->container->get('SundaySchoolService');
+
+        $genderChartArray = [];
+        foreach ($sundaySchoolService->getKidsGender($args['groupId']) as $gender => $kidsCount) {
+            array_push($genderChartArray, ["label" => gettext($gender), "data" => $kidsCount]);
+        }
+        return $response->withJson($genderChartArray);
+    }
+
+    public function getAllStudentsForChart (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
+        $sundaySchoolService = $this->container->get('SundaySchoolService');
+
+        $birthDayMonthChartArray = [];
+        foreach ($sundaySchoolService->getKidsBirthdayMonth($args['groupId']) as $birthDayMonth => $kidsCount) {
+            $res[0] = gettext($birthDayMonth);
+            $res[1] = $kidsCount;
+
+            $birthDayMonthChartArray[] = $res;
+        }
+
+        return  $response->withJson($birthDayMonthChartArray);
+    }
 }
 
-function getAllGendersForDonut (Request $request, Response $response, array $args) {
-  $sundaySchoolService = new SundaySchoolService();
 
-  $genderChartArray = [];
-  foreach ($sundaySchoolService->getKidsGender($args['groupId']) as $gender => $kidsCount) {
-      array_push($genderChartArray, ["label" => gettext($gender), "data" => $kidsCount]);
-  }
-  return $response->withJson($genderChartArray);
-}
 
-function getAllStudentsForChart (Request $request, Response $response, array $args) {
-  $sundaySchoolService = new SundaySchoolService();
-
-  $birthDayMonthChartArray = [];
-  foreach ($sundaySchoolService->getKidsBirthdayMonth($args['groupId']) as $birthDayMonth => $kidsCount) {
-      $res[0] = gettext($birthDayMonth);
-      $res[1] = $kidsCount;
-
-      $birthDayMonthChartArray[] = $res;
-  }
-
-  return  $response->withJson($birthDayMonthChartArray);
-}
