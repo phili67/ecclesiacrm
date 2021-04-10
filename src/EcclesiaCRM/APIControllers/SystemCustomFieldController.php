d7f86fec95782ab60fd6f6e3a7640c22685f475b
phili67 <philippelogel@free.fr>
2021-04-10 10:08:11 +0200

    src/api/routes/system/system-database.php src/EcclesiaCRM/APIControllers/SystemCustomFieldController.php

---
+++ src/EcclesiaCRM/APIControllers/SystemCustomFieldController.php
@@ -0,0 +1,51 @@
+<?php
+
+//
+//  This code is under copyright not under MIT Licence
+//  copyright   : 2021 Philippe Logel all right reserved not MIT licence
+//                This code can't be included in another software
+//
+//  Updated : 2021/04/06
+//
+
+namespace EcclesiaCRM\APIControllers;
+
+use Interop\Container\ContainerInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+use EcclesiaCRM\PersonCustomMasterQuery;
+
+class SystemCustomFieldController
+{
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * A method that does the work to handle getting an existing person custom fields by type.
+     *
+     * @param \Slim\Http\Request $p_request The request.
+     * @param \Slim\Http\Response $p_response The response.
+     * @param array $p_args Arguments
+     * @return \Slim\Http\Response The augmented response.
+     */
+    public function getPersonFieldsByType(ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface
+    {
+        $params = $request->getQueryParams();
+        $typeId = $params['typeId'];
+
+        $fields = PersonCustomMasterQuery::create()->filterByTypeId($typeId)->find();
+
+        $keyValue = [];
+
+        foreach ($fields as $field) {
+            array_push($keyValue, ["id" => $field->getId(), "value" => $field->getCustomName()]);
+        }
+
+        return $response->withJson($keyValue);
+    }
+}
--- src/api/routes/system/system-database.php
+++ src/api/routes/system/system-database.php
@@ -1,115 +1,17 @@
 <?php
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\dto\Photo;
-use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\FamilyCustomQuery;
-use EcclesiaCRM\FamilyQuery;
-use EcclesiaCRM\FileSystemUtils;
-use EcclesiaCRM\NoteQuery;
-use EcclesiaCRM\Person2group2roleP2g2rQuery;
-use EcclesiaCRM\PersonCustomQuery;
-use EcclesiaCRM\PersonQuery;
-use EcclesiaCRM\PersonVolunteerOpportunityQuery;
-use EcclesiaCRM\Slim\Middleware\Request\Auth\AdminRoleAuthMiddleware;
-use EcclesiaCRM\UserQuery;
-use EcclesiaCRM\Utils\LoggerUtils;
-use Propel\Runtime\ActiveQuery\Criteria;
-use Propel\Runtime\Propel;
-
-
-use EcclesiaCRM\Backup\RestoreBackup;
-use EcclesiaCRM\Backup\CreateBackup;
-use EcclesiaCRM\Backup\DownloadManager;
+use EcclesiaCRM\APIControllers\SystemBackupRestoreController;
 
 // Routes
 
 $app->group('/database', function (RouteCollectorProxy $group) {
-    $group->post('/backup', function (Request $request, Response $response, array $args) {
-        $input = (object) $request->getParsedBody();
 
-        $createBackup = new CreateBackup($input);
-        $backup = $createBackup->run();
+    $group->post('/backup', SystemBackupRestoreController::class . ':backup');
+    $group->post('/backupRemote', SystemBackupRestoreController::class . ':backupRemote');
+    $group->post('/restore', SystemBackupRestoreController::class . ':restore' );
+    $group->get('/download/{filename}', SystemBackupRestoreController::class . ':download' );
+    $group->delete('/people/clear', SystemBackupRestoreController::class . ':clearPeopleTables' );
 
-        return $response->write(json_encode(get_object_vars($backup)));
-    });
-
-    $group->post('/backupRemote', function(Request $request, Response $response, array $args) {
-        // without parameters the backup is done on the remote server
-        $input = (object) $request->getParsedBody();
-
-        $createBackup = new CreateBackup($input);
-        $backup = $createBackup->run();
-
-        return $response->write(json_encode(get_object_vars($backup)));
-    });
-
-    $group->post('/restore', function (Request $request, Response $response, array $args) {
-        $fileName = $_FILES['restoreFile'];
-
-        $restoreJob = new RestoreBackup($fileName);
-        $restore = $restoreJob->run();
-
-        return $response->write(json_encode(get_object_vars($restore)));
-    });
-
-    $group->get('/download/{filename}', function (Request $request, Response $response, array $args) {
-        $filename = $args['filename'];
-        DownloadManager::run($filename);
-        exit;// bug resolution for safari
-    });
-
-    $group->delete('/people/clear', 'clearPeopleTables');
 });
-
-function clearPeopleTables(Request $request, Response $response, array $p_args)
-{
-    $connection = Propel::getConnection();
-    $logger = LoggerUtils::getAppLogger();
-
-    $curUserId = $_SESSION["user"]->getId();
-
-    $logger->info("People DB Clear started ");
-
-    FamilyCustomQuery::create()->deleteAll($connection);
-    $logger->info("Family custom deleted ");
-
-    FamilyQuery::create()->deleteAll($connection);
-    $logger->info("Families deleted");
-
-    // Delete Family Photos
-    FileSystemUtils::deleteFiles(SystemURLs::getImagesRoot() . "/Family/", Photo::getValidExtensions());
-    FileSystemUtils::deleteFiles(SystemURLs::getImagesRoot() . "/Family/thumbnails/", Photo::getValidExtensions());
-    $logger->info("family photos deleted");
-
-    Person2group2roleP2g2rQuery::create()->deleteAll($connection);
-    $logger->info("Person Group Roles deleted");
-
-    PersonCustomQuery::create()->deleteAll($connection);
-    $logger->info("Person Custom deleted");
-
-    PersonVolunteerOpportunityQuery::create()->deleteAll($connection);
-    $logger->info("Person Volunteer deleted");
-
-    UserQuery::create()->filterByPersonId($curUserId, Criteria::NOT_EQUAL)->delete($connection);
-    $logger->info("Users aide from person logged in deleted");
-
-    PersonQuery::create()->filterById($curUserId, Criteria::NOT_EQUAL)->delete($connection);
-    $logger->info("Persons aide from person logged in deleted");
-
-    // Delete Person Photos
-    FileSystemUtils::deleteFiles(SystemURLs::getImagesRoot() . "/Person/", Photo::getValidExtensions());
-    FileSystemUtils::deleteFiles(SystemURLs::getImagesRoot() . "/Person/thumbnails/", Photo::getValidExtensions());
-    $logger->info("people photos deleted");
-
-    NoteQuery::create()->filterByPerId($curUserId, Criteria::NOT_EQUAL)->delete($connection);
-    $logger->info("Notes deleted");
-
-    // we empty the cart, to reset all
-    $_SESSION['aPeopleCart'] = [];
-
-    return $response->withJson(['success' => true, 'msg' => gettext('The people and families has been cleared from the database.')]);
-}
