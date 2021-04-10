6107b822598d80a23f0e6e44aa9e22a8ace2bf07
phili67 <philippelogel@free.fr>
2021-04-10 10:06:35 +0200

    src/api/routes/system/system-synchronize.php : src/EcclesiaCRM/APIControllers/SystemSynchronizeController.php

--- src/api/routes/system/system-synchronize.php
+++ src/api/routes/system/system-synchronize.php
@@ -1,10 +1,8 @@
 <?php
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\Service\SynchronizeService;
+use EcclesiaCRM\APIControllers\SystemSynchronizeController;
 
 $app->group('/synchronize', function (RouteCollectorProxy $group) {
 
@@ -12,27 +10,8 @@ $app->group('/synchronize', function (RouteCollectorProxy $group) {
  * @! Returns the dashboard items in function of the current page name : for CRMJsom.js
  * #! param: page->string :: current page name
  */
-    $group->get('/page', function (Request $request, Response $response, array $args) {
-      //$cacheProvider->withExpires($response, 0);
+    $group->get('/page', SystemSynchronizeController::class . ':synchronize' );
 
-      $dataFull = [];
-
-      $SystemService = $this->get('SystemService');
-
-      if ($SystemService->getSessionTimeout() < 10) {
-        $dataTimeout = ['timeOut' => 1,'availableTime' =>  $SystemService->getSessionTimeout()];
-      } else {
-        $dataTimeout = ['timeOut' => 0,'availableTime' =>  $SystemService->getSessionTimeout()];
-      }
-
-      array_push ($dataFull,$dataTimeout);
-
-      if ($SystemService->getSessionTimeout() > 0) {
-          $pageName = $request->getQueryParam("currentpagename","");
-          $DashboardValues = SynchronizeService::getValues($pageName);
-          array_push ($dataFull,$DashboardValues);
-      }
-
-      return $response->withJson($dataFull);
-    });
 });
+
+
