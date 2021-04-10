4094602aeae88d20c77279415fd680fd6000821a
phili67 <philippelogel@free.fr>
2021-04-10 10:07:12 +0200

    src/api/routes/system/system-issues.php : src/EcclesiaCRM/APIControllers/SystemIssueController.php

--- src/api/routes/system/system-issues.php
+++ src/api/routes/system/system-issues.php
@@ -1,14 +1,6 @@
 <?php
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
 // Routes
+use EcclesiaCRM\APIControllers\SystemIssueController;
 
-$app->post('/issues', function (Request $request, Response $response, $args) {
-    $input = json_decode($request->getBody());
-
-    $SystemService = $this->get('SystemService');
-
-    return $response->write($SystemService->reportIssue($input));
-});
+$app->post('/issues', SystemIssueController::class . ':issues' );
