5da28d6a9cc6edd79ba1252cec229fff481a8e2a
phili67 <philippelogel@free.fr>
2021-04-10 10:06:07 +0200

    src/api/routes/system/system-system-upgrade.php : src/EcclesiaCRM/APIControllers/SystemUpgradeController.php

--- src/api/routes/system/system-system-upgrade.php
+++ src/api/routes/system/system-system-upgrade.php
@@ -1,37 +1,15 @@
 <?php
 
-use Slim\Http\Response as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-// Routes
-
-use EcclesiaCRM\SessionUser;
+use EcclesiaCRM\APIControllers\SystemUpgradeController;
 
 $app->group('/systemupgrade', function (RouteCollectorProxy $group) {
-    $group->get('/downloadlatestrelease', function (Request $request, Response $response, array $args) {
-        $SystemService = $this->get('SystemService');
-        $upgradeFile = $SystemService->downloadLatestRelease();
-        return $response->write(json_encode($upgradeFile));
-    });
 
-    $group->post('/doupgrade', function (Request $request, Response $response, $args) {
-        $input = (object) $request->getParsedBody();
-
-        $SystemService = $this->get('SystemService');
-        $upgradeResult = $SystemService->doUpgrade($input->fullPath, $input->sha1);
-        return $response->write(json_encode($upgradeResult));
-    });
-
-    $group->post('/isUpdateRequired', function (Request $request, Response $response, array $args) {
-        if (SessionUser::getUser()->isAdmin() && $_SESSION['isSoftwareUpdateTestPassed'] == false) {
-          $isUpdateRequired = $_SESSION['latestVersion'] != null && $_SESSION['latestVersion']['name'] != $_SESSION['sSoftwareInstalledVersion'];
-          $_SESSION['isSoftwareUpdateTestPassed'] = true;
-        } else {
-          $isUpdateRequired = 0;
-        }
-
-        return $response->withJson(["Upgrade" => $isUpdateRequired,"latestVersion" => $_SESSION['latestVersion'], "installedVersion" => $_SESSION['sSoftwareInstalledVersion']]);
-    });
+    $group->get('/downloadlatestrelease', SystemUpgradeController::class . ':downloadlatestrelease' );
+    $group->post('/doupgrade', SystemUpgradeController::class . ':doupgrade' );
+    $group->post('/isUpdateRequired', SystemUpgradeController::class . ':isUpdateRequired' );
 
 });
+
+
