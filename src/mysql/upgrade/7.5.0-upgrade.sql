9ff7b01fd9a81a25422d8c42d9d949f1d229c08e
phili67 <philippelogel@free.fr>
2021-04-19 07:36:17 +0200

    mysql : automatic

--- src/mysql/upgrade/7.5.0-upgrade.sql
+++ src/mysql/upgrade/7.5.0-upgrade.sql
@@ -1,3 +1,8 @@
+DELETE FROM userconfig_ucfg WHERE ucfg_id="20";
+
+INSERT INTO `userconfig_choices_ucfg_ch` (`ucfg_ch_id`,`ucfg_name`,`ucfg_choices`) VALUES
+(6,'DarkMode', 'light,dark,automatic' );
+
 INSERT INTO `userconfig_ucfg` (`ucfg_per_id`, `ucfg_id`, `ucfg_name`, `ucfg_value`, `ucfg_type`, `ucfg_choices_id`, `ucfg_tooltip`, `ucfg_permission`, `ucfg_cat`) VALUES
-(0, 20, 'sDarkMode', '0', 'boolean', NULL, 'AdminLTE 3.1 Dark Mode', 'TRUE', ''),
-(1, 20, 'sDarkMode', '0', 'boolean', NULL,'AdminLTE 3.1 Dark Mode color style', 'TRUE', '');
+(0, 20, 'sDarkMode', 'automatic', 'choice', '6', 'AdminLTE 3.1 Dark Mode', 'TRUE', ''),
+(1, 20, 'sDarkMode', 'automatic', 'choice', '6','AdminLTE 3.1 Dark Mode color style', 'TRUE', '');
