13ba1d3dccd3d53d7b3833ada8e0c318e74c3fb5
EcclesiaCRM <philippe.logel@gmail.com>
2021-05-01 06:41:09 +0200

    Revert "Gruntfile.js calendar update"

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function (grunt) {
             '!logs/*.log'
         ],
         clean: {
-            skin: ["src/skin/external"],
+            skin: ["src/skin/external", "src/locale/datatables"],
             release: ["target"]
         },
         copy: {
@@ -74,57 +74,15 @@ module.exports = function (grunt) {
                         expand: true,
                         filter: 'isFile',
                         flatten: true,
-                        src: ['node_modules/@fullcalendar/core/*'],
-                        dest: 'src/skin/external/fullcalendar/core/'
+                        src: ['node_modules/fullcalendar/*'],
+                        dest: 'src/skin/external/fullcalendar/'
                     },
                     {
                         expand: true,
                         filter: 'isFile',
                         flatten: true,
-                        src: ['node_modules/@fullcalendar/bootstrap/*'],
-                        dest: 'src/skin/external/fullcalendar/bootstrap/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/timegrid/*'],
-                        dest: 'src/skin/external/fullcalendar/timegrid/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/daygrid/*'],
-                        dest: 'src/skin/external/fullcalendar/daygrid/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/timegrid/*'],
-                        dest: 'src/skin/external/fullcalendar/timegrid/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/list/*'],
-                        dest: 'src/skin/external/fullcalendar/list/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/interaction/*'],
-                        dest: 'src/skin/external/fullcalendar/interaction/'
-                    },
-                    {
-                        expand: true,
-                        filter: 'isFile',
-                        flatten: true,
-                        src: ['node_modules/@fullcalendar/moment/*'],
-                        dest: 'src/skin/external/fullcalendar/moment/'
+                        src: ['node_modules/fullcalendar/locales/*'],
+                        dest: 'src/skin/external/fullcalendar/locales/'
                     },
                     {
                         expand: true,
@@ -142,7 +100,7 @@ module.exports = function (grunt) {
                     },
                     {
                         expand: true,
-                        cwd:'node_modules/ckeditor/',
+                        cwd:'node_modules/ckeditor4/',
                         src: ['*.js','*.css','*.json','lang/**/*','adapters/**/*','plugins/**/*','skins/**/*'],
                         dest: 'src/skin/external/ckeditor/'
                     },
@@ -451,6 +409,12 @@ module.exports = function (grunt) {
                     },
                     {
                         expand: true,
+                        cwd:'node_modules/datatables.net-plugins/i18n/',
+                        src: ['*.lang'],
+                        dest: 'src/locale/datatables/'
+                    },
+                    {
+                        expand: true,
                         cwd:'node_modules/eonasdan-bootstrap-datetimepicker/build/js/',
                         src: ['bootstrap-datetimepicker.min.js'],
                         dest: 'src/skin/external/bootstrap-datetimepicker/'
@@ -702,6 +666,7 @@ module.exports = function (grunt) {
                     jsFileContent = jsFileContent + '\n' + "try {" + fullCalendar + "} catch(e) {};\n";
                 }
             }*/
+
             if (enableDatePicker) {
                 tempFile = 'node_modules/bootstrap-datepicker/dist/locales/bootstrap-datepicker.'+languageCode+'.min.js';
                 var datePicker = grunt.file.read(tempFile);
@@ -784,3 +749,4 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-curl');
     grunt.loadNpmTasks('grunt-poeditor-ab');
 }
+
