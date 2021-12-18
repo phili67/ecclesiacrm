d160f666f84a107147502ad287109c3680017d60
phili67 <philippe.logel@gmail.com>
2021-12-18 20:47:54 +0000

    en_CA.sql update

--- src/mysql/install/languages/en_CA.sql
+++ src/mysql/install/languages/en_CA.sql
@@ -5,5 +5,5 @@ INSERT INTO `config_cfg` (`cfg_id`, `cfg_name`, `cfg_value`) VALUES
 (111, 'sPhoneFormatCell', '999 999 9999'),
 (112, 'sTimeFormat', '%H:%M'),
 (2050, 'bStateUnusefull', '1'),
-(2051, 'sCurrency', 'C$')
+(2051, 'sCurrency', '$')
 ON DUPLICATE KEY UPDATE cfg_name=VALUES(cfg_name),cfg_value=VALUES(cfg_value);
