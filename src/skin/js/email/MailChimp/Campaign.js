bbc4725819acb46d8d9fb4d48010b0accbf4be76
phili67 <philippe.logel@gmail.com>
2021-12-27 10:39:51 +0000

    mailchimp copyright

--- src/skin/js/email/MailChimp/Campaign.js
+++ src/skin/js/email/MailChimp/Campaign.js
@@ -1,3 +1,12 @@
+//
+//  This code is under copyright not under MIT Licence
+//  copyright   : 2018 Philippe Logel all right reserved.
+//                This code can't be included in another software.
+//
+//  Updated : 2020/06/18
+//
+
+
 $(document).ready(function () {
     var editor = null;
 
--- src/skin/js/email/MailChimp/Debug.js
+++ src/skin/js/email/MailChimp/Debug.js
@@ -1,8 +1,17 @@
+//
+//  This code is under copyright not under MIT Licence
+//  copyright   : 2018 Philippe Logel all right reserved.
+//                This code can't be included in another software.
+//
+//  Updated : 2020/06/18
+//
+
+
 $(document).ready(function () {
     window.CRM.APIRequest({
       method: 'POST',
       path: 'mailchimp/testConnection'
-    }).done(function(data) { 
+    }).done(function(data) {
       if (data.error == undefined) {
         $("#mailTest").html(data.result);
       } else {
