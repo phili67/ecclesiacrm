b064cc21db54ee6a8298f8afc088219fd68cb7c7
phili67 <philippe.logel@gmail.com>
2022-01-04 06:55:13 +0000

    v2/templates/email/mailchimp/campaign.php gui refactor

--- src/v2/templates/email/mailchimp/campaign.php
+++ src/v2/templates/email/mailchimp/campaign.php
@@ -16,7 +16,7 @@ require $sRootDocument . '/Include/Header.php';
 ?>
 
 <div class="row">
-    <div class="col-lg-12">
+    <div class="col-lg-9">
         <div class="card">
             <div class="card-header   with-border">
                 <h3 class="card-title"><i class="fa fa-newspaper-o"></i> <?= _('Email Campaign Management') ?></h3>
@@ -42,94 +42,124 @@ require $sRootDocument . '/Include/Header.php';
                 </p>
             </div>
         </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header with-border">
-                <h3 class="card-title"><?= _('Mail Subject') ?></h3>
-            </div>
-            <div class="card-body">
-                <input type="text" id="CampaignSubject" placeholder="<?= _("Your Mail Subject") ?>" size="30"
-                       maxlength="100" class="form-control input-sm" style="width: 100%"
-                       value="<?= $campaign['settings']['subject_line'] ?>">
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header   with-border">
-                <h3 class="card-title"><i class="fa fa-tag"></i> <?= _('Tags') ?></h3>
-            </div>
-            <div class="card-body">
-                <?= $campaign['recipients']['segment_text'] ?>
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header   with-border">
-                <?php
-                if ($campaign['status'] == "schedule") {
-                    ?>
-                    <h3 class="card-title"><input type="checkbox" id="checkboxaCampaignSchedule"
-                                                  name="checkboxaCampaignSchedule" checked> <i class="fa fa-calendar-check-o"></i> <label
-                            for="checkboxaCampaignSchedule"><?= _('Schedule') ?></label></h3>
-                    <?php
-                } else {
-                    ?>
-                    <h3 class="card-title"><input type="checkbox" id="checkboxaCampaignSchedule"
-                                                  name="checkboxaCampaignSchedule" <?= ($campaign['status'] == "sent" && !($campaign['status'] == "schedule")) ? "disabled" : "" ?>>
-                        <i class="fa fa-calendar-times-o"></i> <label for="checkboxaCampaignSchedule"><?= _('Schedule') ?></label></h3>
-                    <?php
-                }
-                ?>
-            </div>
-            <div class="card-body">
-                <div class="alert alert-warning"><i class="fa fa-warning" aria-hidden="true"></i>
-                    <?= _("You've first to create a content below to schedule a campaign.") ?>
-                </div>
-                <div class="row">
-                    <div class="col-md-12">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card">
+                    <div class="card-header with-border">
+                        <h3 class="card-title"><i class="fa fa-file-text-o"></i> <?= _('Mail Subject') ?> & <?= _("Content") ?></h3>
+                    </div>
+                    <div class="card-body">
                         <div class="row">
                             <div class="col-md-1"><span style="color: red">*</span>
-                                <label><?= _('Date') ?> :</label>
+                                <label><?= _('Subject') ?> :</label>
                             </div>
-                            <div class="form-group col-md-2">
-                                <div class="input-group mb-2">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                            <div class="col-md-9">
+                                <input type="text" id="CampaignSubject" placeholder="<?= _("Your Mail Subject") ?>" size="30"
+                                       maxlength="100" class="form-control input-sm" style="width: 100%"
+                                       value="<?= $campaign['settings']['subject_line'] ?>">
+                            </div>
+                        </div>
+                        <br/>
+                        <div class="row">
+                            <div class="col-lg-12" style="padding-left:15px;padding-right:15px;">
+                                <div class="alert alert-info"><i class="fa fa-info" aria-hidden="true"></i>
+                                    <?= _("You can use the button \"Merge Tags\" below, to customize your content") ?> : <img
+                                        src="<?= $sRootPath ?>/Images/merge_tags.png">.
+                                </div>
+                                <textarea name="campaignContent" cols="80" class="form-control input-sm campaignContent"
+                                          id="campaignContent" width="100%"
+                                          style="margin-top:0px;width: 100%;height: 14em;"></textarea></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-info">
+                    <div class="card-header   with-border">
+                        <h3 class="card-title"><i class="fa fa-tag"></i> <?= _('Tags') ?></h3>
+                        <div class="card-tools pull-right">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fa fa-minus"></i></button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <?= $campaign['recipients']['segment_text'] ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card card-gray">
+                    <div class="card-header   with-border">
+                        <?php
+                        if ($campaign['status'] == "schedule") {
+                            ?>
+                            <h3 class="card-title"><input type="checkbox" id="checkboxaCampaignSchedule"
+                                                          name="checkboxaCampaignSchedule" checked> <i class="fa fa-calendar-check-o"></i> <label
+                                    for="checkboxaCampaignSchedule"><?= _('Schedule') ?></label></h3>
+                            <?php
+                        } else {
+                            ?>
+                            <h3 class="card-title"><input type="checkbox" id="checkboxaCampaignSchedule"
+                                                          name="checkboxaCampaignSchedule" <?= ($campaign['status'] == "sent" && !($campaign['status'] == "schedule")) ? "disabled" : "" ?>>
+                                <i class="fa fa-calendar-times-o"></i> <label for="checkboxaCampaignSchedule"><?= _('Schedule') ?></label></h3>
+                            <?php
+                        }
+                        ?>
+                        <div class="card-tools pull-right">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fa fa-minus"></i></button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <div class="alert alert-warning"><i class="fa fa-warning" aria-hidden="true"></i>
+                            <?= _("You've first to create a content below to schedule a campaign.") ?>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col-md-3"><span style="color: red">*</span>
+                                        <label><?= _('Date') ?> :</label>
                                     </div>
-                                    <!-- Philippe Logel -->
-                                    <input class="form-control date-picker input-sm" type="text" id="dateCampaign"
-                                           name="dateCampaign"
-                                           value="<?= (isset($campaign['send_time'])) ? OutputUtils::change_date_for_place_holder($campaign['send_time']) : "" ?>"
-                                            maxlength="10" id="sel1" size="11"
-                                            placeholder="<?= SystemConfig::getValue('sDatePickerPlaceHolder') ?>
+                                    <div class="form-group col-md-9">
+                                        <div class="input-group mb-2">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-calendar"></i></span>
+                                            </div>
+                                            <!-- Philippe Logel -->
+                                            <input class="form-control date-picker input-sm" type="text" id="dateCampaign"
+                                                   name="dateCampaign"
+                                                   value="<?= (isset($campaign['send_time'])) ? OutputUtils::change_date_for_place_holder($campaign['send_time']) : "" ?>"
+                                                   maxlength="10" id="sel1" size="11"
+                                                   placeholder="<?= SystemConfig::getValue('sDatePickerPlaceHolder') ?>
                                     " <?= ($campaign['status'] == "schedule") ? "" : "disabled" ?>>
-                                </div>
-                            </div>
-                            <div class="form-group col-md-2">
-                                <div class="input-group mb-2">
-                                    <div class="input-group-prepend">
-                                        <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                        </div>
                                     </div>
-                                    <!-- Philippe Logel -->
-                                    <input type="text" class="form-control timepicker input-sm" id="timeCampaign"
-                                           name="timeCampaign"
-                                           value="<?= (isset($campaign['send_time'])) ? OutputUtils::change_time_for_place_holder($campaign['send_time']) : "00:00" ?>" <?= ($campaign['status'] == "schedule") ? "" : "disabled" ?>>
                                 </div>
-                            </div>
-                            <div class="col-md-7">
-                                <?= _("To validate, save your campaign with the <b>\"Save Campaign\"</b> button over.") ?>
+                                <div class="row">
+                                    <div class="col-md-3"><span style="color: red">*</span>
+                                        <label><?= _('Time') ?> :</label>
+                                    </div>
+                                    <div class="form-group col-md-9">
+                                        <div class="input-group">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <!-- Philippe Logel -->
+                                            <input type="text" class="form-control timepicker input-sm" id="timeCampaign"
+                                                   name="timeCampaign"
+                                                   value="<?= (isset($campaign['send_time'])) ? OutputUtils::change_time_for_place_holder($campaign['send_time']) : "00:00" ?>" <?= ($campaign['status'] == "schedule") ? "" : "disabled" ?>>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-md-12">
+                                        <?= _("To validate, save your campaign with the <b>\"Save Campaign\"</b> button over.") ?>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -139,27 +169,7 @@ require $sRootDocument . '/Include/Header.php';
     </div>
 </div>
 
-<div class="row">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-header   with-border">
-                <h3 class="card-title"><i class="fa fa-file-text-o"></i> <?= _("Content") ?> </h3>
-            </div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-12" style="padding-left:15px;padding-right:15px;">
-                        <div class="alert alert-info"><i class="fa fa-info" aria-hidden="true"></i>
-                            <?= _("You can use the button \"Merge Tags\" below, to customize your content") ?> : <img
-                                src="<?= $sRootPath ?>/Images/merge_tags.png">.
-                        </div>
-                        <textarea name="campaignContent" cols="80" class="form-control input-sm campaignContent"
-                                  id="campaignContent" width="100%"
-                                  style="margin-top:0px;width: 100%;height: 14em;"></textarea></div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
 
 <script src="<?= $sRootPath ?>/skin/js/email/MailChimp/AutomaticDarkMode.js"></script>
 <?php require $sRootDocument . '/Include/Footer.php'; ?>
