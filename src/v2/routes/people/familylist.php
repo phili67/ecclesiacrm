8cc2c94b812f1228897a1b3c1992043977324911
phili67 <philippe.logel@gmail.com>
2022-01-06 19:28:51 +0000

    v2 controllers : people

--- src/v2/routes/people/familylist.php
+++ src/v2/routes/people/familylist.php
@@ -3,176 +3,19 @@
 /*******************************************************************************
  *
  *  filename    : PastoralCare.php
- *  last change : 2019-06-16
+ *  last change : 2022-01-06
  *  website     : http://www.ecclesiacrm.com
  *  copyright   : 2019 Philippe Logel all right reserved not MIT licence
- *                This code can't be incoprorated in another software without any authorization
+ *                This code can't be included in another software without authorization
  *
  ******************************************************************************/
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\dto\SystemConfig;
-use EcclesiaCRM\FamilyQuery;
-use Propel\Runtime\ActiveQuery\Criteria;
-use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\SessionUser;
-
-use EcclesiaCRM\Map\FamilyTableMap;
-
-use Slim\Views\PhpRenderer;
+use EcclesiaCRM\VIEWControllers\VIEWFamilyListController;
 
 $app->group('/familylist', function (RouteCollectorProxy $group) {
-    $group->get('', 'renderFamilyList' );
-    $group->get('/', 'renderFamilyList' );
-    $group->get('/{mode}', 'renderFamilyList' );
+    $group->get('', VIEWFamilyListController::class . ':renderFamilyList' );
+    $group->get('/', VIEWFamilyListController::class . ':renderFamilyList' );
+    $group->get('/{mode}', VIEWFamilyListController::class . ':renderFamilyList' );
 });
-
-
-function renderFamilyList (Request $request, Response $response, array $args) {
-    $renderer = new PhpRenderer('templates/people/');
-
-    $sMode = $args['mode'];
-
-    if ( !( SessionUser::getUser()->isEditRecordsEnabled()
-      || (strtolower($sMode)  == 'gdrp' && SessionUser::getUser()->isGdrpDpoEnabled())
-      || (strtolower($sMode) == 'inactive' && SessionUser::getUser()->isEditRecordsEnabled())
-      || (strtolower($sMode) == 'empty' && SessionUser::getUser()->isEditRecordsEnabled())
-          )
-       ) {
-      return $response->withStatus(302)->withHeader('Location', SystemURLs::getRootPath() . '/v2/dashboard');
-    }
-
-    return $renderer->render($response, 'familylist.php', argumentsFamilyListArray($sMode));
-}
-
-function argumentsFamilyListArray ($sMode='Active')
-{
-    if (strtolower($sMode) == 'gdrp') {
-      $time = new \DateTime('now');
-      $newtime = $time->modify('-'.SystemConfig::getValue('iGdprExpirationDate').' year')->format('Y-m-d');
-
-      $subQuery = FamilyQuery::create()
-          ->withColumn('Family.Id','FamId')
-          ->leftJoinPerson()
-            ->withColumn('COUNT(Person.Id)','cnt')
-          ->filterByDateDeactivated($newtime, Criteria::LESS_THAN)
-          ->groupById(FamilyTableMap::COL_FAM_ID);
-
-      $families = FamilyQuery::create()
-           ->addSelectQuery($subQuery, 'res')
-           ->where('res.cnt>1 AND Family.Id=res.FamId')// only real family with more than one member will be showed here
-           ->find();
-
-    } else if (strtolower($sMode) == 'inactive') {
-      if (SystemConfig::getValue('bGDPR')) {
-        $time = new \DateTime('now');
-        $newtime = $time->modify('-'.SystemConfig::getValue('iGdprExpirationDate').' year')->format('Y-m-d');
-
-        $families = FamilyQuery::create()
-                  ->filterByDateDeactivated($newtime, Criteria::GREATER_THAN)// GDRP, when a person is completely deactivated, we only can see the person who are over a certain date
-                  ->orderByName()
-                  ->find();
-
-        $subQuery = FamilyQuery::create()
-          ->withColumn('Family.Id','FamId')
-          ->leftJoinPerson()
-            ->withColumn('COUNT(Person.Id)','cnt')
-          ->filterByDateDeactivated($newtime, Criteria::GREATER_THAN)
-          ->groupById(FamilyTableMap::COL_FAM_ID);
-
-        $families = FamilyQuery::create()
-           ->addSelectQuery($subQuery, 'res')
-           ->where('res.cnt>1 AND Family.Id=res.FamId')// only real family with more than one member will be showed here
-           ->find();
-      } else {// we're always inactiv
-        $time = new \DateTime('now');
-
-        $families = FamilyQuery::create()
-                  ->filterByDateDeactivated($time, Criteria::LESS_EQUAL)// GDRP, when a person is completely deactivated, we only can see the person who are over a certain date
-                  ->orderByName()
-                  ->find();
-
-        $subQuery = FamilyQuery::create()
-          ->withColumn('Family.Id','FamId')
-          ->leftJoinPerson()
-            ->withColumn('COUNT(Person.Id)','cnt')
-          ->filterByDateDeactivated($time, Criteria::LESS_EQUAL)
-          ->groupById(FamilyTableMap::COL_FAM_ID);
-
-        $families = FamilyQuery::create()
-           ->addSelectQuery($subQuery, 'res')
-           ->where('res.cnt>1 AND Family.Id=res.FamId')// only real family with more than one member will be showed here
-           ->find();
-
-      }
-    } else if (strtolower($sMode) == 'empty') {
-        $subQuery = FamilyQuery::create()
-            ->withColumn('Family.Id', 'FamId')
-            ->leftJoinPerson()
-            ->withColumn('COUNT(Person.Id)', 'cnt')
-            ->filterByDateDeactivated(NULL)
-            ->groupById();//groupBy('Family.Id');
-
-        $families = FamilyQuery::create()
-            ->addSelectQuery($subQuery, 'res')
-            ->where('res.cnt=0 AND Family.Id=res.FamId') // The emptied addresses
-            ->find();
-    } else if (strtolower($sMode) == 'single') {
-        $sMode = 'Single';
-        $subQuery = FamilyQuery::create()
-            ->withColumn('Family.Id','FamId')
-            ->leftJoinPerson()
-            ->usePersonQuery()
-            ->filterByDateDeactivated( null)
-            ->withColumn('COUNT(Person.Id)','cnt')
-            ->endUse()
-            ->filterByDateDeactivated(NULL)
-            ->groupById(FamilyTableMap::COL_FAM_ID);
-
-        $families = FamilyQuery::create()
-            ->addSelectQuery($subQuery, 'res')
-            ->where('res.cnt=1 AND Family.Id=res.FamId') // only real family with more than one member will be showed here
-            ->find();
-    } else {
-      $sMode = 'Active';
-      $subQuery = FamilyQuery::create()
-          ->withColumn('Family.Id','FamId')
-          ->leftJoinPerson()
-          ->usePersonQuery()
-            ->filterByDateDeactivated( null)
-            ->withColumn('COUNT(Person.Id)','cnt')
-          ->endUse()
-          ->filterByDateDeactivated(NULL)
-          ->groupById(FamilyTableMap::COL_FAM_ID);
-
-      $families = FamilyQuery::create()
-           ->addSelectQuery($subQuery, 'res')
-           ->where('res.cnt>1 AND Family.Id=res.FamId') // only real family with more than one member will be showed here
-           ->find();
-    }
-
-    if ($sMode == 'Single') {
-        $sPageTitle = _("Single Persons");
-    } else {
-        $sPageTitle = _(ucfirst(_($sMode))) . ' : ' . ((strtolower($sMode) == 'empty') ? _('Addresses') : _('Family List'));
-    }
-
-
-    $sRootDocument   = SystemURLs::getDocumentRoot();
-    $sDateFormatLong = SystemConfig::getValue('sDateFormatLong');
-    $sCSPNonce       = SystemURLs::getCSPNonce();
-
-    $paramsArguments = ['sRootPath'           => SystemURLs::getRootPath(),
-                       'sRootDocument'        => $sRootDocument,
-                       'sPageTitle'           => $sPageTitle,
-                       'sMode'                => $sMode,
-                       'sCSPNonce'            => $sCSPNonce,
-                       'families'             => $families,
-                       'bNotGDRPNotEmpty'     => SessionUser::getUser()->isAddRecordsEnabled() && strtolower($sMode) != 'gdrp' && strtolower($sMode) != 'empty'
-                       ];
-
-   return $paramsArguments;
-}
--- src/v2/routes/people/personlist.php
+++ src/v2/routes/people/personlist.php
@@ -10,100 +10,12 @@
  *
  ******************************************************************************/
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Routing\RouteCollectorProxy;
 
-use EcclesiaCRM\dto\SystemConfig;
-use EcclesiaCRM\PersonQuery;
-use Propel\Runtime\ActiveQuery\Criteria;
-use EcclesiaCRM\dto\SystemURLs;
-use EcclesiaCRM\SessionUser;
-
-
-use Slim\Views\PhpRenderer;
+use EcclesiaCRM\VIEWControllers\VIEWPersonListController;
 
 $app->group('/personlist', function (RouteCollectorProxy $group) {
-    $group->get('', 'renderPersonList' );
-    $group->get('/', 'renderPersonList' );
-    $group->get('/{mode}', 'renderPersonList' );
+    $group->get('', VIEWPersonListController::class . ':renderPersonList' );
+    $group->get('/', VIEWPersonListController::class . ':renderPersonList' );
+    $group->get('/{mode}', VIEWPersonListController::class . ':renderPersonList' );
 });
-
-
-function renderPersonList (Request $request, Response $response, array $args) {
-    $renderer = new PhpRenderer('templates/people/');
-
-    $sMode = $args['mode'];
-
-    if ( !( SessionUser::getUser()->isEditRecordsEnabled()
-      || (strtolower($sMode)  == 'gdrp' && SessionUser::getUser()->isGdrpDpoEnabled())
-      || (strtolower($sMode) == 'inactive' && SessionUser::getUser()->isEditRecordsEnabled())
-          )
-       ) {
-      return $response->withStatus(302)->withHeader('Location', SystemURLs::getRootPath() . '/v2/dashboard');
-    }
-
-    return $renderer->render($response, 'personlist.php', argumentsPersonListArray($sMode));
-}
-
-function argumentsPersonListArray ($sMode='Active')
-{
-    if (strtolower($sMode) == 'gdrp') {
-       $time = new \DateTime('now');
-       $newtime = $time->modify('-'.SystemConfig::getValue('iGdprExpirationDate').' year')->format('Y-m-d');
-
-       $persons = PersonQuery::create()
-                ->filterByDateDeactivated($newtime, Criteria::LESS_THAN)// GDRP, when a person is completely deactivated
-                ->_or() // or : this part is unusefull, it's only for debugging
-                ->useFamilyQuery()
-                  ->filterByDateDeactivated($newtime, Criteria::LESS_THAN)// GDRP, when a Family is completely deactivated
-                ->endUse()
-                ->orderByLastName()
-                ->find();
-
-    } else if (strtolower($sMode) == 'inactive') {
-      if (SystemConfig::getValue('bGDPR')) {
-        $time = new \DateTime('now');
-        $newtime = $time->modify('-'.SystemConfig::getValue('iGdprExpirationDate').' year')->format('Y-m-d');
-
-        $persons = PersonQuery::create()
-                ->filterByDateDeactivated($newtime, Criteria::GREATER_THAN)// GDRP, when a person isn't under GDRP but deactivated, we only can see the person who are over a certain date
-                ->_or()// this part is unusefull, it's only for debugging
-                ->useFamilyQuery()
-                  ->filterByDateDeactivated($newtime, Criteria::GREATER_THAN)// GDRP, when a Family is completely deactivated
-                ->endUse()
-                ->orderByLastName()
-                ->find();
-      } else {
-        $time = new \DateTime('now');
-
-        $persons = PersonQuery::create()
-                ->filterByDateDeactivated($time, Criteria::LESS_EQUAL)
-                ->orderByLastName()
-                ->find();
-      }
-    } else {
-      $sMode = 'Active';
-      $persons = PersonQuery::create()
-              ->filterByDateDeactivated(null)
-              ->orderByLastName()
-              ->find();
-    }
-
-    $sPageTitle = _(ucfirst(_($sMode))) . ' : ' . _('Person List');
-
-    $sRootDocument   = SystemURLs::getDocumentRoot();
-    $sDateFormatLong = SystemConfig::getValue('sDateFormatLong');
-    $sCSPNonce       = SystemURLs::getCSPNonce();
-
-    $paramsArguments = ['sRootPath'           => SystemURLs::getRootPath(),
-                       'sRootDocument'        => $sRootDocument,
-                       'sPageTitle'           => $sPageTitle,
-                       'sMode'                => $sMode,
-                       'sCSPNonce'            => $sCSPNonce,
-                       'persons'              => $persons,
-                       'bNotGDRP'             => SessionUser::getUser()->isAddRecordsEnabled() && strtolower($sMode) != 'gdrp'
-                       ];
-
-   return $paramsArguments;
-}
