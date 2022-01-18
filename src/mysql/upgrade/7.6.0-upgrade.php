5d5cc168c961baef3c0e483f04e9ce9cc4c32122
phili67 <philippe.logel@gmail.com>
2022-01-18 14:45:19 +0000

    7.6.0-upgrade.php : remove ListEvents.php + ListEvent.js + GetText.php

--- src/mysql/upgrade/7.6.0-upgrade.php
+++ src/mysql/upgrade/7.6.0-upgrade.php
@@ -14,6 +14,9 @@ $logger->info("Start to delete : all unusefull files");
 unlink(SystemURLs::getDocumentRoot()."/Include/GetGroupArray.php");
 unlink(SystemURLs::getDocumentRoot()."/RPCdummy.php");
 unlink(SystemURLs::getDocumentRoot()."/EcclesiaCRM/Reports/ChurchInfoReport.php");
+unlink(SystemURLs::getDocumentRoot()."/ListEvents.php");
+unlink(SystemURLs::getDocumentRoot()."/GetText.php");
+unlink(SystemURLs::getDocumentRoot()."/skin/js/event/ListEvent.js");
 
 $logger->info("End of delete :  all unusefull files");
 ?>
