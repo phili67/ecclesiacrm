a1d997c1ee574b735ea7e6ad56194aaeb7d5d5d1
phili67 <philippe.logel@gmail.com>
2022-01-18 14:14:55 +0000

    src/EcclesiaCRM/APIControllers/CalendarV2Controller.php : finish the part for

--- src/EcclesiaCRM/APIControllers/CalendarV2Controller.php
+++ src/EcclesiaCRM/APIControllers/CalendarV2Controller.php
@@ -47,7 +47,7 @@ class CalendarV2Controller
 
         $CalendarService = $this->container->get('CalendarService');
 
-        return $response->withJson($CalendarService->getEvents($params->start, $params->end, $params->isBirthdayActive, $params->isAnniversaryActive));;
+        return $response->withJson($CalendarService->getEvents($params->start, $params->end, $params->isBirthdayActive, $params->isAnniversaryActive)['EventsListResults']);;
     }
 
     public function getallCalendarEventsForEventsList (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
@@ -55,7 +55,7 @@ class CalendarV2Controller
 
         $CalendarService = $this->container->get('CalendarService');
 
-        return $response->withJson(['EventsListResults' => $CalendarService->getEvents($params->start, $params->end, $params->isBirthdayActive, $params->isAnniversaryActive)]);;
+        return $response->withJson($CalendarService->getEvents($params->start, $params->end, $params->isBirthdayActive, $params->isAnniversaryActive, $params->forEventslist));
     }
 
     public function numberOfCalendars (ServerRequestInterface $request, ResponseInterface $response, array $args): ResponseInterface {
