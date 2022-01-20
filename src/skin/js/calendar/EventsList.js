fa829addf5ba37bc07caed8d1e66d1a1f623b27b
phili67 <philippe.logel@gmail.com>
2022-01-20 08:18:51 +0000

    src/skin/js/calendar/EventsList.js : buttons enhancements

--- src/skin/js/calendar/EventsList.js
+++ src/skin/js/calendar/EventsList.js
@@ -69,18 +69,18 @@ $(document).ready(function () {
                     //full.backgroundColor
                     return '<table class="table-responsive" style="width:120px">\n' +
                         '                <tbody><tr class="no-background-theme">\n' +
-                        '                  <td style="width:38px;padding: 7px 2px;border:none;">\n' +
-                        '                    <button type="submit"  name="Action" data-id="' + full.eventID+ '" title="' + i18next.t('Edit') + '" style="color:blue" class="EditEvent btn btn-default btn-xs" '+ ((full.Rights)?'':'disabled') +'>\n' +
+                        '                  <td style="width:48px;padding: 7px 2px;border:none;text-align: right">\n' +
+                        '                    <button type="submit"  name="Action" data-id="' + full.eventID+ '" title="' + i18next.t('Edit') + '" style="color:' + ((full.Rights != "")?'blue':'gray') + '" class="EditEvent btn btn-default btn-xs" '+ ((full.Rights)?'':'disabled') +'>\n' +
                                                 data +
                         '                    </button>\n' +
                         '                  </td>\n' +
-                        '                  <td style="width:25px;padding: 7px 2px;border:none;">\n' +
-                        '                      <button type="submit" name="Action" data-dateStart="' + full.start + '" data-reccurenceid="' + full.reccurenceID + '" data-recurrent="' + full.recurrent + '" data-calendarid="' + full.calendarID + '" data-id="' + full.eventID+ '" title="' + i18next.t('Delete') + '"  style="color:red" class="DeleteEvent btn btn-default btn-xs" ' + ((full.Rights)?'':'disabled') + '>\n' +
+                        '                  <td style="width:18px;padding: 7px 2px;border:none;">\n' +
+                        '                      <button type="submit" name="Action" data-dateStart="' + full.start + '" data-reccurenceid="' + full.reccurenceID + '" data-recurrent="' + full.recurrent + '" data-calendarid="' + full.calendarID + '" data-id="' + full.eventID+ '" title="' + i18next.t('Delete') + '"  style="color:' + ((full.Rights != "")?'red':'gray') + '" class="DeleteEvent btn btn-default btn-xs" ' + ((full.Rights)?'':'disabled') + '>\n' +
                         '                        <i class="fa fa-trash"></i>\n' +
                         '                      </button>\n' +
                         '                  </td>\n' +
-                        '                  <td style="width:30px;padding: 7px 2px;border:none;">\n' +
-                        '                      <button type="submit" name="Action" data-id="' + full.eventID+ '" title="' + i18next.t('Info') + '" style="color:green" class="EventInfo btn btn-default btn-xs" ' + ((full.Text != "")?'':'disabled') + '>\n' +
+                        '                  <td style="width:18px;padding: 7px 2px;border:none;text-align: left">\n' +
+                        '                      <button type="submit" name="Action" data-id="' + full.eventID+ '" title="' + i18next.t('Info') + '" style="color:' + ((full.Text != "" && full.Rights)?'green':'gray') + '" class="EventInfo btn btn-default btn-xs" ' + ((full.Text != "")?'':'disabled') + '>\n' +
                         '                        <i class="fa fa-file"></i>\n' +
                         '                      </button>\n' +
                         '                  </td>\n' +
