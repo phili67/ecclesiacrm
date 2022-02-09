f63af8d8391d094404ba1a330afdeefd3b85c5ca
phili67 <philippe.logel@gmail.com>
2022-02-09 07:06:03 +0000

    v2 : 404 bug correction

--- src/v2/templates/error/404.php
+++ src/v2/templates/error/404.php
@@ -18,7 +18,7 @@ require $sRootDocument . '/Include/Header.php';
 
             <p>
                 <?= gettext("for")?> : <?= str_replace(' ', '/',$uri) ?>
-                <?= gettext("Meanwhile, you may")?> <a href="<?= SystemURLs::getRootPath() ?>/v2/dashboard"> <?= gettext("return to member dashboard") ?></a>
+                <?= gettext("Meanwhile, you may")?> <a href="<?= $sRootPath ?>/v2/dashboard"> <?= gettext("return to member dashboard") ?></a>
             </p>
         </div>
     </div>
