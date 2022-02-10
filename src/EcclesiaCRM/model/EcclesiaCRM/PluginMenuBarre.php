d15885d4c49f10271dae0099ec6d7d0466af9535
phili67 <philippe.logel@gmail.com>
2022-02-10 08:53:18 +0000

    New model : Plugin.php + PluginMenuBarre.php + User.php update

---
+++ src/EcclesiaCRM/model/EcclesiaCRM/PluginMenuBarre.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EcclesiaCRM;
+
+use EcclesiaCRM\Base\PluginMenuBarre as BasePluginMenuBarre;
+
+/**
+ * Skeleton subclass for representing a row from the 'plugin_menu_barre' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class PluginMenuBarre extends BasePluginMenuBarre
+{
+
+}
