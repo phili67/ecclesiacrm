60bfeeda26dd02a3648c48ba6ff0629b24f2a884
phili67 <philippe.logel@gmail.com>
2022-02-11 16:46:39 +0000

    v2 plugins routes

---
+++ src/v2/routes/plugins/plugins.php
@@ -0,0 +1,24 @@
+<?php
+
+/*******************************************************************************
+ *
+ *  filename    : route/backup.php
+ *  last change : 2019-11-21
+ *  description : manage the backup
+ *
+ *  http://www.ecclesiacrm.com/
+ *
+ *  This code is under copyright not under MIT Licence
+ *  copyright   : 2018 Philippe Logel all right reserved not MIT licence
+ *                This code can't be incorporated in another software authorization
+ *
+ ******************************************************************************/
+
+use Slim\Routing\RouteCollectorProxy;
+
+use EcclesiaCRM\VIEWControllers\VIEWPluginsController;
+
+$app->group('/plugins', function (RouteCollectorProxy $group) {
+    $group->get('', VIEWPluginsController::class . ':renderPlugins');
+    $group->get('/', VIEWPluginsController::class . ':renderPlugins');
+});
