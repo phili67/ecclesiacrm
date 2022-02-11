d9d099dccd94c013384ef6b54eba643136d3854a
phili67 <philippe.logel@gmail.com>
2022-02-11 16:44:29 +0000

    api/routes/plugins/plugins.php : new route

---
+++ src/api/routes/plugins/plugins.php
@@ -0,0 +1,15 @@
+<?php
+
+use Slim\Routing\RouteCollectorProxy;
+
+use EcclesiaCRM\APIControllers\PluginsController;
+
+$app->group('/plugins', function (RouteCollectorProxy $group) {
+
+    $group->post('/activate', PluginsController::class . ':activate' );
+    $group->post('/deactivate', PluginsController::class . ':deactivate' );
+    $group->delete('/', PluginsController::class . ':remove' );
+
+});
+
+
