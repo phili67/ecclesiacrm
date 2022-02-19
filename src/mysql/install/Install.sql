dd9166b446fc3ac21c76a65f41235c7eb162c30e
phili67 <philippe.logel@gmail.com>
2022-02-19 19:02:18 +0000

    install.sql : plugin_user_role

--- src/mysql/install/Install.sql
+++ src/mysql/install/Install.sql
@@ -1936,12 +1936,30 @@ CREATE TABLE `plugin_menu_barre` (
   `plgn_mb_plugin_Display_name` varchar(255) DEFAULT '',
   `plgn_mb_url` varchar(255) DEFAULT '' COMMENT 'URL Menubar',
   `plgn_bm_icon` varchar(255) DEFAULT '' COMMENT 'Icon MenuBar',
-  `plgn_bm_grp_sec` varchar(255) DEFAULT '' COMMENT 'In lower case : usr_AddRecords, usr_EditRecords, usr_DeleteRecords, usr_ShowCart, usr_ShowMap, usr_EDrive, usr_MenuOptions, usr_ManageGroups, usr_ManageCalendarResources, usr_HtmlSourceEditor, usr_Finance, usr_Notes, usr_EditSelf, usr_Canvasser, usr_Admin, usr_showMenuQuery, usr_CanSendEmail, usr_ExportCSV, usr_CreateDirectory, usr_ExportSundaySchoolPDF, usr_ExportSundaySchoolCSV, usr_MainDashboard, usr_SeePrivacyData, usr_MailChimp, usr_GDRP_DPO, usr_PastoralCare, usr_Meeting',
+  `plgn_bm_grp_sec` varchar(255) DEFAULT '' COMMENT 'In lower case : usr_AddRecords, usr_EditRecords, usr_DeleteRecords, usr_ShowCart, usr_ShowMap, usr_EDrive, usr_MenuOptions, usr_ManageGroups, usr_ManageCalendarResources, usr_HtmlSourceEditor, usr_Finance, usr_Notes, usr_EditSelf, usr_Canvasser, usr_Admin, usr_showMenuQuery, usr_CanSendEmail, usr_ExportCSV, usr_CreateDirectory, usr_ExportSundaySchoolPDF, usr_ExportSundaySchoolCSV, usr_MainDashboard, usr_SeePrivacyData, usr_MailChimp, usr_GDRP_DPO, usr_PastoralCare',
   PRIMARY KEY  (`plgn_mb_ID`)
 ) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;
 
 --
 -- Dumping data for table `plugin_menu_barre`
+--
+
+--
+-- Table structure for table `plugin_user_role`
+--
+
+CREATE TABLE `plugin_user_role` (
+    `plgn_usr_rl_ID` mediumint(8) unsigned NOT NULL auto_increment,
+    `plgn_usr_rl_user_id` mediumint(9) unsigned NOT NULL default '0',
+    `plgn_usr_rl_plugin_id` mediumint(8) unsigned NOT NULL default '0',
+    `plgn_usr_rl_role` enum('none', 'user', 'admin') NOT NULL default 'none' COMMENT 'user role : can be the thee enum parts',
+    PRIMARY KEY  (`plgn_usr_rl_ID`),
+    CONSTRAINT fk_plgn_usr_rl_user_id FOREIGN KEY (plgn_usr_rl_user_id) REFERENCES user_usr(usr_per_ID) ON DELETE CASCADE,
+    CONSTRAINT fkplgn_usr_rl_plugin_id FOREIGN KEY (plgn_usr_rl_plugin_id) REFERENCES plugin(plgn_ID) ON DELETE CASCADE
+) ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+--
+-- Dumping data for table `plugin_user_role`
 --
 
 -- method for dup emails
