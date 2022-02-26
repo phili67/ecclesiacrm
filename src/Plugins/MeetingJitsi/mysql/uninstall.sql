b00f0c87aaeaeb271bb4aa7b4995ed7284639515
phili67 <philippe.logel@gmail.com>
2022-02-26 06:18:30 +0000

    install.sql + uninstall.sql script

--- src/Plugins/MeetingJitsi/mysql/uninstall.sql
+++
@@ -1,6 +0,0 @@
-DELETE FROM `plugin` WHERE `plgn_Name`='MeetingJitsi';
-DELETE FROM `plugin_menu_barre` WHERE `plgn_mb_plugin_name`='MeetingJitsi';
-
-DROP TABLE `plugin_pref_jitsimeeting_pjmp`;
-DROP TABLE `personlastjitsimeeting_plm`;
-DROP TABLE `personjitsimeeting_pm`;
