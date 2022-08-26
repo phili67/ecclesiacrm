43882cc037ce3ed84ba91dd1dce3249c32a2345a
phili67 <philippe.logel@gmail.com>
2022-08-26 08:14:22 +0000

    BirthdayAnniversary delete : rename

--- src/Plugins/BirthdayAnniversary/mysql/Install.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `plugin` ( `plgn_Name`, `plgn_Description`, `plgn_Category`, `plgn_image`, `plgn_installation_path`, `plgn_activ`, `plgn_version`, `plgn_prefix`, `plgn_position`, `plgn_default_orientation`, `plgn_default_color`, `plgn_securities`)
-VALUES ('BirthdayAnniversary', 'Plugin birthday and anniversary', 'Dashboard', NULL, '', '0', '1.0', 'ba_', 'inside_category_menu', 'top', 'bg-gradient-blue text-white', 32);
--- src/Plugins/BirthdayAnniversary/mysql/Uninstall.sql
+++
@@ -1,1 +0,0 @@
-DELETE FROM `plugin` WHERE `plgn_Name`='BirthdayAnniversary';
