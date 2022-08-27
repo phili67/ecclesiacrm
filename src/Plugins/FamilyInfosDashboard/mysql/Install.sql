03d0ad9173dab3fc371e5e22aea296e8ede6eaf9
phili67 <philippe.logel@gmail.com>
2022-08-27 08:37:25 +0000

    Plugins are activ by default

--- src/Plugins/FamilyInfosDashboard/mysql/Install.sql
+++ src/Plugins/FamilyInfosDashboard/mysql/Install.sql
@@ -1,2 +1,2 @@
 INSERT INTO `plugin` ( `plgn_Name`, `plgn_Description`, `plgn_Category`, `plgn_image`, `plgn_installation_path`, `plgn_activ`, `plgn_version`, `plgn_prefix`, `plgn_position`, `plgn_default_orientation`, `plgn_default_color`, `plgn_securities`)
-VALUES ('FamilyInfosDashboard', 'Dashboard Plugin for family infos', 'Dashboard', NULL, '', '0', '1.0', 'pid_', 'inside_category_menu', 'right', 'bg-gradient-blue text-white',32);
+VALUES ('FamilyInfosDashboard', 'Dashboard Plugin for family infos', 'Dashboard', NULL, '', '1', '1.0', 'pid_', 'inside_category_menu', 'right', 'bg-gradient-blue text-white',32);
