4dcba35bdf449dd91a188847557ff90237ec10a0
phili67 <philippe.logel@gmail.com>
2022-08-29 19:02:10 +0000

    Setup + db upgrade

--- src/Plugins/PersonInfosDashboard/mysql/Install.sql
+++ src/Plugins/PersonInfosDashboard/mysql/Install.sql
@@ -1,2 +1,3 @@
 INSERT INTO `plugin` ( `plgn_Name`, `plgn_Description`, `plgn_Category`, `plgn_image`, `plgn_installation_path`, `plgn_activ`, `plgn_version`, `plgn_prefix`, `plgn_position`, `plgn_default_orientation`, `plgn_default_color`, `plgn_securities`)
-VALUES ('PersonInfosDashboard', 'Plugin person Infos', 'Dashboard', NULL, '', '1', '1.0', 'pc_', 'inside_category_menu', 'pip', 'bg-gradient-red text-white',32);
+VALUES ('PersonInfosDashboard', 'Plugin person Infos', 'Dashboard', NULL, '', '1', '1.0', 'pip_', 'inside_category_menu', 'left', 'bg-gradient-red text-white',32);
+
