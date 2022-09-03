f448f7b1cfafaf080eb5d415850a2d719b3b2286
phili67 <philippe.logel@gmail.com>
2022-09-03 14:13:12 +0000

    src/v2/templates/dashboard/maindashboard.php : to collapse a plugin

--- src/v2/templates/dashboard/maindashboard.php
+++ src/v2/templates/dashboard/maindashboard.php
@@ -321,12 +321,20 @@ if (!$load_Elements) {
                 if ( !(SessionUser::getUser()->isSecurityEnableForPlugin($plugin->getName(), $security)) )
                     continue;
 
+                $userPluginStatus = PluginUserRoleQuery::create()
+                    ->filterByUserId(SessionUser::getId())
+                    ->findOneByPluginId($plugin->getId());
+
+                $is_collapsed = $userPluginStatus->isCollapsed();
 
                 echo $this->fetch("../../../Plugins/" . $plugin->getName() . "/v2/templates/View.php",[
                     'sRootPath'     => $sRootPath,
                     'sRootDocument' => $sRootDocument,
                     'CSPNonce'      => $CSPNonce,
-                    'PluginId'      => $plugin->getId()
+                    'PluginId'      => $plugin->getId(),
+                    'Card_collapsed'  => ($is_collapsed?'collapsed-card':''),
+                    'Card_body'       => ($is_collapsed?'display: none':'display: block'),
+                    'Card_collapsed_button' => ($is_collapsed?'fa-plus':'fa-minus')
                 ])
                 ?>
             <?php } ?>
@@ -357,143 +365,23 @@ if (!$load_Elements) {
                 if ( !(SessionUser::getUser()->isSecurityEnableForPlugin($plugin->getName(), $security)) )
                     continue;
 
+                $userPluginStatus = PluginUserRoleQuery::create()
+                    ->filterByUserId(SessionUser::getId())
+                    ->findOneByPluginId($plugin->getId());
+
+                $is_collapsed = $userPluginStatus->isCollapsed();
+
                 echo $this->fetch("../../../Plugins/" . $plugin->getName() . "/v2/templates/View.php",[
                     'sRootPath'     => $sRootPath,
                     'sRootDocument' => $sRootDocument,
                     'CSPNonce'      => $CSPNonce,
-                    'PluginId'      => $plugin->getId()
+                    'PluginId'      => $plugin->getId(),
+                    'Card_collapsed'  => ($is_collapsed?'collapsed-card':''),
+                    'Card_body'       => ($is_collapsed?'display: none':'display: block'),
+                    'Card_collapsed_button' => ($is_collapsed?'fa-plus':'fa-minus')
                 ])
                 ?>
             <?php } ?>
-
-            <div class="card">
-                <div class="card-header ui-sortable-handle" style="cursor: move;">
-                    <h3 class="card-title">
-                        <i class="ion ion-clipboard mr-1"></i>
-                        To Do List
-                    </h3>
-                    <div class="card-tools">
-                        <ul class="pagination pagination-sm">
-                            <li class="page-item"><a href="#" class="page-link">«</a></li>
-                            <li class="page-item"><a href="#" class="page-link">1</a></li>
-                            <li class="page-item"><a href="#" class="page-link">2</a></li>
-                            <li class="page-item"><a href="#" class="page-link">3</a></li>
-                            <li class="page-item"><a href="#" class="page-link">»</a></li>
-                        </ul>
-                    </div>
-                </div>
-
-                <div class="card-body">
-                    <ul class="todo-list ui-sortable" data-widget="todo-list">
-                        <li>
-
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo1" id="todoCheck1">
-                                <label for="todoCheck1"></label>
-                            </div>
-
-                            <span class="text">Design a nice theme</span>
-
-                            <small class="badge badge-danger"><i class="far fa-clock"></i> 2 mins</small>
-
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                        <li class="done">
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo2" id="todoCheck2" checked="">
-                                <label for="todoCheck2"></label>
-                            </div>
-                            <span class="text">Make the theme responsive</span>
-                            <small class="badge badge-info"><i class="far fa-clock"></i> 4 hours</small>
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                        <li>
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo3" id="todoCheck3">
-                                <label for="todoCheck3"></label>
-                            </div>
-                            <span class="text">Let theme shine like a star</span>
-                            <small class="badge badge-warning"><i class="far fa-clock"></i> 1 day</small>
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                        <li>
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo4" id="todoCheck4">
-                                <label for="todoCheck4"></label>
-                            </div>
-                            <span class="text">Let theme shine like a star</span>
-                            <small class="badge badge-success"><i class="far fa-clock"></i> 3 days</small>
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                        <li>
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo5" id="todoCheck5">
-                                <label for="todoCheck5"></label>
-                            </div>
-                            <span class="text">Check your messages and notifications</span>
-                            <small class="badge badge-primary"><i class="far fa-clock"></i> 1 week</small>
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                        <li>
-<span class="handle ui-sortable-handle">
-<i class="fas fa-ellipsis-v"></i>
-<i class="fas fa-ellipsis-v"></i>
-</span>
-                            <div class="icheck-primary d-inline ml-2">
-                                <input type="checkbox" value="" name="todo6" id="todoCheck6">
-                                <label for="todoCheck6"></label>
-                            </div>
-                            <span class="text">Let theme shine like a star</span>
-                            <small class="badge badge-secondary"><i class="far fa-clock"></i> 1 month</small>
-                            <div class="tools">
-                                <i class="fas fa-edit"></i>
-                                <i class="fas fa-trash-o"></i>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="card-footer clearfix">
-                    <button type="button" class="btn btn-primary float-right"><i class="fas fa-plus"></i> Add item</button>
-                </div>
-            </div>
-
         </section>
 
 
@@ -519,14 +407,25 @@ if (!$load_Elements) {
                 if ( !(SessionUser::getUser()->isSecurityEnableForPlugin($plugin->getName(), $security)) )
                     continue;
 
+                $userPluginStatus = PluginUserRoleQuery::create()
+                    ->filterByUserId(SessionUser::getId())
+                    ->findOneByPluginId($plugin->getId());
+
+                $is_collapsed = $userPluginStatus->isCollapsed();
+
                 echo $this->fetch("../../../Plugins/" . $plugin->getName() . "/v2/templates/View.php",[
                     'sRootPath'     => $sRootPath,
                     'sRootDocument' => $sRootDocument,
                     'CSPNonce'      => $CSPNonce,
-                    'PluginId'      => $plugin->getId()
+                    'PluginId'      => $plugin->getId(),
+                    'Card_collapsed'  => ($is_collapsed?'collapsed-card':''),
+                    'Card_body'       => ($is_collapsed?'display: none':'display: block'),
+                    'Card_collapsed_button' => ($is_collapsed?'fa-plus':'fa-minus')
                 ])
                 ?>
-            <?php } ?>
+            <?php
+            }
+            ?>
         </section>
 
     </div>
@@ -542,10 +441,6 @@ if (!$load_Elements) {
 </script>
 
 <script src="<?= SystemURLs::getRootPath() ?>/skin/external/jquery-ui/jquery-ui.min.js"  type="text/javascript"></script>
-
-<script>
-    $('.todo-list').sortable({placeholder:'sort-highlight',handle:'.handle',forcePlaceholderSize:true,zIndex:999999});
-</script>
 
 <?php require $sRootDocument . '/Include/Footer.php'; ?>
 
