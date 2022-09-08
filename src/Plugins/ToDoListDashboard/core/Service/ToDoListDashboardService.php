cdbbe3f1e11dde09520e477868334e65c91526e2
phili67 <philippe.logel@gmail.com>
2022-09-08 16:27:29 +0000

    src/Plugins/ToDoListDashboard/core/Service/ToDoListDashboardService.php : service update

--- src/Plugins/ToDoListDashboard/core/Service/ToDoListDashboardService.php
+++ src/Plugins/ToDoListDashboard/core/Service/ToDoListDashboardService.php
@@ -10,46 +10,60 @@ class ToDoListDashboardService
     {
         $now = new \DateTime('now', new \DateTimeZone(SystemConfig::getValue('sTimeZone')) );
 
-        $interval = $date->diff($now);
+        $interval = $now->diff($date);
 
         $days = $interval->format('%d');
         $hours   = $interval->format('%H');
         $minutes = $interval->format('%i');
 
+        $after = $interval->format('%R');
+
         $time = $days * 1440 + $hours * 60 + $minutes;
 
-        $color = '';
 
-        if ($time < 60) {
-            $color = 'danger';
-        } elseif ($time < 60*60) {
-            $color = 'warning';
-        } elseif ($time < 60*60*24) {
-            $color = 'info';
-        } elseif ($time < 60*60*24*7) {
-            $color = 'primary';
-        } elseif ($time < 60*60*24*30) {
-            $color = 'success';
-        } else {
-            $color = 'secondary';
-        }
 
         $period = '';
 
-        if ($time < 60*60) {
+        if ($time < 60) {
             $period = $minutes . ' ' . dgettext("messages-ToDoListDashboard","mins");
-        } else if ($time < 60*60*24) {
+        } else if ($time < 60*24) {
             $period = $hours . ' ' . dgettext("messages-ToDoListDashboard","hours");
-        } else if ($time < 60*60*24*30) {
+        } else if ($time < 60*24*30) {
             $period = $days . ' ' . dgettext("messages-ToDoListDashboard","days");
         } else {
             $period = (int)($days/30) . ' ' . dgettext("messages-ToDoListDashboard","months");
         }
 
+        $period = $after. ' ' .$period;
+
+        $after = ($after == "+")?true:false;
+
+        $color = '';
+
+        if ($time < 1) {
+            $color = 'danger';
+        } elseif ($time < 60) {
+            $color = 'warning';
+        } elseif ($time < 60*24) {
+            $color = 'info';
+        } elseif ($time < 60*24*7) {
+            $color = 'primary';
+        } elseif ($time < 60*24*30) {
+            $color = 'success';
+        } else {
+            $color = 'secondary';
+        }
+
+        if ($after == false) {
+            $color = 'secondary';
+        }
+
         return [
             'time' => $time,
             'period' => $period,
-            'color' => $color
+            'color' => $color,
+            'date' => $date->format('Y-m-d H:i:s'),
+            'after' => $after
         ];
     }
 }
