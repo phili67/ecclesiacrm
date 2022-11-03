5821be1f0d7e622f87f9b9fe8d425c6ee40e3781
phili67 <philippe.logel@gmail.com>
2022-11-03 19:35:21 +0000

    Upgrade script

--- src/EcclesiaCRM/Service/UpgradeService.php
+++ src/EcclesiaCRM/Service/UpgradeService.php
@@ -24,45 +24,74 @@ class UpgradeService
         if ($db_version == $_SESSION['sSoftwareInstalledVersion']) {
             return true;
         }
-        
+
         try {
-          $_SESSION['updateDataBase'] = true;
-                
-          //the database isn't at the current version.  Start the upgrade
-          $dbUpdatesFile = file_get_contents(SystemURLs::getDocumentRoot() . '/mysql/upgrade.json');
-          $dbUpdates = json_decode($dbUpdatesFile, true);
-        
-          $errorFlag = false;
-          $connection = Propel::getConnection();
-          foreach ($dbUpdates as $dbUpdate) {
-              if (in_array(SystemService::getDBVersion(), $dbUpdate['versions'])) {
-                  $version = new Version();
-                  $version->setVersion($dbUpdate['dbVersion']);
-                  $version->setUpdateStart(new \DateTime());
-                  $logger->info("New Version: " .$version->getVersion());
-                  foreach ($dbUpdate['scripts'] as $dbScript) {
-                      $scriptName = SystemURLs::getDocumentRoot() . $dbScript;
-                      $logger->info("Upgrade DB - " . $scriptName);
-                      if (pathinfo($scriptName, PATHINFO_EXTENSION) == "sql") {
-                          SQLUtils::sqlImport($scriptName, $connection);
-                      } else {
-                          require_once ($scriptName);
-                      }
-                  }
-                  if (!$errorFlag) {
-                      $version->setUpdateEnd(new \DateTime());
-                      $version->save();
-                  }
-              }
-          }
+            $_SESSION['updateDataBase'] = true;
 
-          unset($_SESSION['updateDataBase']);
+            //the database isn't at the current version.  Start the upgrade
+            $dbUpdatesFile = file_get_contents(SystemURLs::getDocumentRoot() . '/mysql/upgrade.json');
+            $dbUpdates = json_decode($dbUpdatesFile, true);
 
-          return true;
-        
+            $errorFlag = false;
+            $connection = Propel::getConnection();
+            foreach ($dbUpdates as $dbUpdate) {
+                if (in_array(SystemService::getDBVersion(), $dbUpdate['versions'])) {
+                    $version = new Version();
+                    $version->setVersion($dbUpdate['dbVersion']);
+                    $version->setUpdateStart(new \DateTime());
+                    $logger->info("New Version: " .$version->getVersion());
+                    foreach ($dbUpdate['scripts'] as $dbScript) {
+                        $scriptName = SystemURLs::getDocumentRoot() . $dbScript;
+                        $logger->info("Upgrade DB - " . $scriptName);
+                        if (pathinfo($scriptName, PATHINFO_EXTENSION) == "sql") {
+                            SQLUtils::sqlImport($scriptName, $connection);
+                        } else {
+                            require_once ($scriptName);
+                        }
+                    }
+                    if (!$errorFlag) {
+                        $version->setUpdateEnd(new \DateTime());
+                        $version->save();
+                    }
+                }
+            }
+
+            unset($_SESSION['updateDataBase']);
+
+            return true;
+
         } catch (\Exception $exc){
-           $logger->error(gettext("Databse upgrade failed").": " . $exc->getMessage());
-           throw $exc; //allow the method requesting the upgrade to handle this failure also.
+            $logger->error(gettext("Databse upgrade failed").": " . $exc->getMessage());
+            throw $exc; //allow the method requesting the upgrade to handle this failure also.
+        }
+    }
+
+    static public function preUpgradePHPScriptFrom($db_version)
+    {
+        $logger = LoggerUtils::getAppLogger();
+
+        try {
+            //the database isn't at the current version.  Start the upgrade
+            $dbUpdatesFile = file_get_contents(SystemURLs::getDocumentRoot() . '/mysql/upgrade.json');
+            $dbUpdates = json_decode($dbUpdatesFile, true);
+
+            foreach ($dbUpdates as $dbUpdate) {
+                if (in_array($db_version, $dbUpdate['versions'])) {
+                    foreach ($dbUpdate['prescripts'] as $dbScript) {
+                        $scriptName = SystemURLs::getDocumentRoot() . $dbScript;
+                        $logger->info("Upgrade php - " . $scriptName);
+                        if (pathinfo($scriptName, PATHINFO_EXTENSION) == "php") {
+                            require_once ($scriptName);
+                        }
+                    }
+                }
+            }
+
+            return true;
+
+        } catch (\Exception $exc){
+            $logger->error(gettext("Databse upgrade failed").": " . $exc->getMessage());
+            throw $exc; //allow the method requesting the upgrade to handle this failure also.
         }
     }
 
