4c9646b936f8570817e5c9ed4346f84c4cc927db
EcclesiaCRM <philippe.logel@gmail.com>
2022-12-24 16:25:19 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,28 +3,30 @@
 # Dans le cas de php 7.2
 #cp -rf propel/vendor/* src/vendor/propel
 #
+
+## the first run you've to install :
+
 # npm install node-sass
 # npm install i18next-extract-gettext -g
 # npm install i18next-conv -g
 # npm install grunt -g
-
 # npm install --global strip-json-comments-cli
+
+# cd src/
+# sudo composer i
+# cd ..
+# sudo cp BuildConfig.json.example BuildConfig.json
 
 # npm install grunt --save-dev
 
 # rebuild the nodes-sass base
+
+## end of first run
+
 npm rebuild node-sass
 
 #  it's time to install all the node js files
 npm install --unsafe-perm --legacy-peer-deps
-
-cd src/
-
-sudo composer i
-
-cd ..
-
-sudo cp BuildConfig.json.example BuildConfig.json
 
 # to run the installation
 npm run install
