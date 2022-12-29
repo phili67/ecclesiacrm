dd56a7d9a9583369d2da0aac98d8c334d2b56d8b
phili67 <philippe.logel@gmail.com>
2022-12-29 17:03:12 +0000

    slim-doc.php : mise à jour pour les nouvelles apis

--- Documentation/slim-doc.php
+++ Documentation/slim-doc.php
@@ -1,53 +1,67 @@
 #!/usr/bin/php
 <?php
-    $options = getopt("i:o:n::h");
-    $doc_cache = array();
-    $doc_temp = emptyTemp();
+$options = getopt("i:o:n::h");
+$doc_cache = array();
+$doc_temp = emptyTemp();
 
-    $use_template = array_key_exists("t", $options) ? $options["t"] : "markdown";
-    $title = array_key_exists("n", $options) ? $options["n"] : "API Description";
+$use_template = array_key_exists("t", $options) ? $options["t"] : "markdown";
+$title = array_key_exists("n", $options) ? $options["n"] : "API Description";
 
-    //echo "Title ${title}\n";
+//echo "Title ${title}\n";
 
-    if ($argc < 3 || array_key_exists("h", $options)) {
-        printUsage();
-        die();
-    }
+if ($argc < 3 || array_key_exists("h", $options)) {
+    printUsage();
+    die();
+}
 
-    if (!file_exists($options["i"])) {
-        die("Input file does not exist !\n");
-    }
+if (!file_exists($options["i"])) {
+    die("Input file does not exist !\n");
+}
 
-    $route = str_replace("../src","",$options["i"]);
+$route = str_replace("../src", "", $options["i"]);
 
-    $out_file = fopen($options["o"], "w");
-    if (!$out_file) {
-        die("Could not create output file. \n");
-    }
+$out_file = fopen($options["o"], "w");
+if (!$out_file) {
+    die("Could not create output file. \n");
+}
 
-    $src_file = fopen($options["i"], 'r');
-    if (!empty($src_file)){
-        while(($line = fgets($src_file)) !== false){
+$src_file = fopen($options["i"], 'r');
+if (!empty($src_file)) {
+    $desc_found = false;
+    while (($line = fgets($src_file)) !== false) {
+        if ($desc_found == false) {
             preg_match("/\*\s+@!\s+(.*)/", $line, $description);
-            if ($description){
-                $doc_temp["description"] = $description[1];
-                continue;
-            }
+        }
+        if ($description and $desc_found == false) {
+            $doc_temp["description"] = $description[1];
+            $desc_found = true;
+            continue;
+        }
 
-            /* Matches route parameters ( #! ) */
-            preg_match("/\*\s+#!\s+param:\s+([a-zA-Z_1-2]*)->([a-z]*)\s+::\s+(.*)/", $line, $params);
-            if($params){
-                array_push($doc_temp["params"], $params);
-                continue;
-            }
+        /* Matches route parameters ( #! ) */
+        preg_match("/\*\s+#!\s+param:\s+([a-zA-Z_1-2]*)->([a-z]*)\s+::\s+(.*)/", $line, $params);
+        if ($params) {
+            array_push($doc_temp["params"], $params);
+            continue;
+        }
 
-            /* Matches route path ( src ) */
-            //preg_match("/[a-z]*->(get|post|put|delete)\(/", $line, $route_data);
-              // slash at the end is the end of the reg search
-              // extract this->get( this->post( etc ...
-            preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', \'(.*)\' \)/", $line, $route_data);
-              // extract this->get('coucou', 'toto' )
+        /* Matches route path ( src ) */
+        //preg_match("/[a-z]*->(get|post|put|delete)\(/", $line, $route_data);
+        // slash at the end is the end of the reg search
+        // extract this->get( this->post( etc ...
+        preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', \'(.*)\' \)/", $line, $route_data);
+        // extract this->get('coucou', 'toto' )
 
+
+        if ($route_data) {
+            $doc_cache[] = array(
+                "route_data" => $route_data,
+                "doc" => $doc_temp
+            );
+            $doc_temp = emptyTemp();
+            $desc_found = false;
+        } else {
+            preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', \"(.*)\" \)/", $line, $route_data);
 
             if ($route_data) {
                 $doc_cache[] = array(
@@ -55,23 +69,16 @@
                     "doc" => $doc_temp
                 );
                 $doc_temp = emptyTemp();
+                $desc_found = false;
             } else {
-              preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', \"(.*)\" \)/", $line, $route_data);
-
-              if ($route_data) {
-                $doc_cache[] = array(
-                    "route_data" => $route_data,
-                    "doc" => $doc_temp
-                );
-                $doc_temp = emptyTemp();
-              } else {
                 preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', (.*) \{/", $line, $route_data);
                 if ($route_data) {
-                  $doc_cache[] = array(
-                      "route_data" => $route_data,
-                      "doc" => $doc_temp
-                  );
-                  $doc_temp = emptyTemp();
+                    $doc_cache[] = array(
+                        "route_data" => $route_data,
+                        "doc" => $doc_temp
+                    );
+                    $doc_temp = emptyTemp();
+                    $desc_found = false;
                 } else {
                     preg_match("/\\$[a-z]*->(get|post|put|delete)\(\'(.*)\', (.*) \)/", $line, $route_data);
                     if ($route_data) {
@@ -80,53 +87,58 @@
                             "doc" => $doc_temp
                         );
                         $doc_temp = emptyTemp();
+                        $desc_found = false;
                     }
                 }
-              }
             }
         }
-
-        //print_r($doc_cache)."\n";
-
-        //echo $route;
-
-        fwrite($out_file,
-            template(dirname(__FILE__) . "/templates/". $use_template .".php",
-                array(
-                    "title" => $title,
-                    "route" => $route,
-                    "contents" => $doc_cache)));
-    } else {
-        die("Could not open input file");
     }
 
-    fclose($out_file);
-    fclose($src_file);
+    //print_r($doc_cache)."\n";
+
+    //echo $route;
+
+    fwrite($out_file,
+        template(dirname(__FILE__) . "/templates/" . $use_template . ".php",
+            array(
+                "title" => $title,
+                "route" => $route . "\"",
+                "contents" => $doc_cache)));
+} else {
+    die("Could not open input file");
+}
+
+fclose($out_file);
+fclose($src_file);
 
 
-    /* Declarations */
+/* Declarations */
 
-    function template($filename, $template_data){
-        ob_start();
-        if (file_exists($filename)){
-            include($filename);
-        }
-
-        return ob_get_clean();
+function template($filename, $template_data)
+{
+    ob_start();
+    if (file_exists($filename)) {
+        include($filename);
     }
 
-    function emptyTemp(){
-        return array(
-            "description" => "No description",
-            "params" => Array()
-        );
-    }
+    return ob_get_clean();
+}
 
-    function printUsage(){
-        print("Slim simple documentation generator.\n");
-        print("Usage: slim_doc.php -i[input_file] -o[output_file]\n");
-        print("Optional Parameters: \n\t-t[template] ( default : markdown )\n");
-        print("\t-n[name] ( API Name header )\n");
-        print("\t-h ( show help )\n");
-    }
+function emptyTemp()
+{
+    return array(
+        "description" => "No description",
+        "params" => array()
+    );
+}
+
+function printUsage()
+{
+    print("Slim simple documentation generator.\n");
+    print("Usage: slim_doc.php -i[input_file] -o[output_file]\n");
+    print("Optional Parameters: \n\t-t[template] ( default : markdown )\n");
+    print("\t-n[name] ( API Name header )\n");
+    print("\t-h ( show help )\n");
+}
+
 ?>
