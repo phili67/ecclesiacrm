976e6afde0d05b3f0e1b66cc65e15733b7ee46c6
Philippe Logel <philippelogel@free.fr>
2023-04-20 09:30:09 +0000

    new class file from schema

---
+++ src/EcclesiaCRM/model/EcclesiaCRM/SendNewsLetterUserUpdate.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace EcclesiaCRM;
+
+use EcclesiaCRM\Base\SendNewsLetterUserUpdate as BaseSendNewsLetterUserUpdate;
+
+/**
+ * Skeleton subclass for representing a row from the 'send_news_letter_user_update' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SendNewsLetterUserUpdate extends BaseSendNewsLetterUserUpdate
+{
+
+}
