5d7b76a485e622567393b25620e703486536e598
Philippe Logel <philippelogel@free.fr>
2023-05-11 16:28:29 +0000

    new routes

--- src/v2/routes/user/user.php
+++ src/v2/routes/user/user.php
@@ -15,7 +15,9 @@ $app->group('/users', function (RouteCollectorProxy $group) {
 
     $group->get('/editor/{PersonID:[0-9]+}', VIEWUserController::class . ':renderUserEditor' );
 
-    $group->get('/editor/{PersonID:[0-9]+}/{errorMsg}', VIEWUserController::class . ':renderUserEditorErrorMsg' );
-    $group->post('/editor/new/{NewPersonID:[0-9]+}/{errorMsg}', VIEWUserController::class . ':renderNewUserEditorErrorMsg' );
+    $group->get('/editor/{PersonID:[0-9]+}/errormessage/{errorMsg}', VIEWUserController::class . ':renderUserEditorErrorMsg' );
 
+    $group->get('/editor/new', VIEWUserController::class . ':renderNewUserEditorErrorMsg' );
+    $group->get('/editor/new/{NewPersonID:[0-9]+}', VIEWUserController::class . ':renderNewUserEditorErrorMsg' );
+    $group->get('/editor/new/{NewPersonID:[0-9]+}/errormessage/{errorMsg}', VIEWUserController::class . ':renderNewUserEditorErrorMsg' );
 });
