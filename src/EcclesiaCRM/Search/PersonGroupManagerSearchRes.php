0cc0ac579c9003045c55d04c410c678f156a42e2
Philippe Logel <philippelogel@free.fr>
2023-06-14 12:59:30 +0000

    search update for v2

--- src/EcclesiaCRM/Search/PersonGroupManagerSearchRes.php
+++ src/EcclesiaCRM/Search/PersonGroupManagerSearchRes.php
@@ -122,7 +122,7 @@ class PersonGroupManagerSearchRes extends BaseSearchRes
                                 }
                             }
                             if (SessionUser::getUser()->isShowCartEnabled()) {
-                                $res .= '&nbsp;<a href="' . SystemURLs::getRootPath() . '/PrintView.php?PersonID=' . $per->getPerson()->getId() . '"  data-toggle="tooltip" data-placement="top" title="' . _('Print') . '">';
+                                $res .= '&nbsp;<a href="' . SystemURLs::getRootPath() . '/v2/people/person/print/' . $per->getPerson()->getId() . '"  data-toggle="tooltip" data-placement="top" title="' . _('Print') . '">';
                             }
                             $res .= '<span class="fa-stack">'
                                 . '<i class="fas fa-square fa-stack-2x"></i>'
