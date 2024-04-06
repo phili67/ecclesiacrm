b0ccc3a1c6f4bd41eb9499628fa3c0fb01bf8dd1
Philippe Logel <philippelogel@free.fr>
2024-04-06 07:48:34 +0000

    Header security for 8x8

--- src/Include/Header-Security.php
+++ src/Include/Header-Security.php
@@ -13,14 +13,14 @@ $actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https"
 
 $csp = array(
     "default-src 'self'",
-    "script-src 'self' 'unsafe-eval' 'nonce-".SystemURLs::getCSPNonce()."' sidecar.gitter.im browser-update.org maps.googleapis.com www.bing.com  dev.virtualearth.net t.ssl.ak.dynamic.tiles.virtualearth.net cdnjs.cloudflare.com meet.jit.si", /*'nonce-".SystemURLs::getCSPNonce()."' // replacement of : 'unsafe-inline' */
+    "script-src 'self' 'unsafe-eval' 'nonce-".SystemURLs::getCSPNonce()."' sidecar.gitter.im browser-update.org maps.googleapis.com www.bing.com  dev.virtualearth.net t.ssl.ak.dynamic.tiles.virtualearth.net cdnjs.cloudflare.com meet.jit.si 8x8.vc", /*'nonce-".SystemURLs::getCSPNonce()."' // replacement of : 'unsafe-inline' */
     "object-src 'self' 'nonce-".SystemURLs::getCSPNonce()."'",
     "style-src 'self' 'unsafe-inline' fonts.googleapis.com www.bing.com r.bing.com",
     "img-src 'self' 'nonce-".SystemURLs::getCSPNonce()."' www.google.com maps.gstatic.com maps.googleapis.com r.bing.com a.tile.openstreetmap.org b.tile.openstreetmap.org c.tile.openstreetmap.org www.bing.com t.ssl.ak.dynamic.tiles.virtualearth.net data:",
     "media-src 'self'",
-    "frame-src 'self' www.youtube.com",
+    "frame-src 'self' www.youtube.com 8x8.vc",
     "font-src 'self' fonts.gstatic.com",
-    "connect-src 'self' www.bing.com  nominatim.openstreetmap.org cke4.ckeditor.com",
+    "connect-src 'self' www.bing.com  nominatim.openstreetmap.org cke4.ckeditor.com maps.googleapis.com",
     "report-uri ".$actual_link.SystemURLs::getRootPath()."/api/system/csp-report"
 );
 if (SystemConfig::getBooleanValue("bHSTSEnable")) {
