2ca90930053e846290aefc0e1a7f72a356a12047
Philippe Logel <philippelogel@free.fr>
2025-04-17 13:10:35 +0000

    minor update of src/mysql/upgrade/8.0.0-upgrade.php

--- src/mysql/upgrade/8.0.0-upgrade.php
+++ src/mysql/upgrade/8.0.0-upgrade.php
@@ -1,8 +1,8 @@
 <?php
 // pour le debug on se met au bon endroit : http://192.168.151.205/mysql/upgrade/8.0.0-upgrade.php
 // et il faut décommenter
-define("webdav", "1");
-require '../../Include/Config.php';
+/*define("webdav", "1");
+require '../../Include/Config.php';*/
 
 use EcclesiaCRM\Utils\LoggerUtils;
 use EcclesiaCRM\dto\SystemURLs;
