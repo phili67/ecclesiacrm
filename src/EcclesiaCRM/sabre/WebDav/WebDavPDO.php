7db2002bb01c6bcf77ceb5c2874b9a592a8ac743
Philippe Logel <philippelogel@free.fr>
2025-05-17 19:26:55 +0000

    src/EcclesiaCRM/sabre/WebDav/WebDavPDO.php : share implementation

--- src/EcclesiaCRM/sabre/WebDav/WebDavPDO.php
+++ src/EcclesiaCRM/sabre/WebDav/WebDavPDO.php
@@ -10,12 +10,23 @@
 
 namespace EcclesiaCRM\MyPDO;
 
+use Collator;
 use EcclesiaCRM\Bootstrapper;
 
 use Sabre\DAV\Xml\Element\Sharee;
 use Sabre\WebDAV\Backend as SabreWebDavBase;
 
+use EcclesiaCRM\Utils\MiscUtils;
+
 use EcclesiaCRM\UserQuery;
+use EcclesiaCRM\Collections;
+use EcclesiaCRM\CollectionsQuery;
+use EcclesiaCRM\Collectionsinstances;
+use EcclesiaCRM\CollectionsinstancesQuery;
+use EcclesiaCRM\dto\SystemURLs;
+
+use Sabre\DAV\Sharing\Plugin as SPlugin;
+
 
 class WebDavPDO extends SabreWebDavBase\PDO
 {
@@ -27,6 +38,44 @@ class WebDavPDO extends SabreWebDavBase\PDO
 
         parent::__construct($pdo);
     }
+
+    /**
+     * Returns the 'access level' for the instance of this shared resource.
+     *
+     * The value should be one of the Sabre\DAV\Sharing\Plugin::ACCESS_
+     * constants.
+     * const ACCESS_NOTSHARED = 0;
+     * const ACCESS_SHAREDOWNER = 1;
+     * const ACCESS_READ = 2;
+     * const ACCESS_READWRITE = 3;
+     * const ACCESS_NOACCESS = 4;
+     *
+     * @return int
+     */
+    public function getShareAccess($mycol)
+    { 
+        $path = $mycol->getPath();
+
+        $collections = CollectionsQuery::create()
+            ->findOneByOwnerpath($path);
+
+        if (!is_null($collections)) {
+            $collectionsInstances = CollectionsinstancesQuery::create()
+                ->findOneByCollectionsId($collections->getId());
+            
+            
+            if (!is_null($collectionsInstances))
+            {
+                return $collectionsInstances->getAccess();
+            } else {// there is no more shared file or folder
+                $collections->delete();
+            }
+        }
+        
+
+        return SPlugin::ACCESS_NOTSHARED;
+    }
+    
 
     /**
      * This function must return a URI that uniquely identifies the shared
@@ -41,9 +90,25 @@ class WebDavPDO extends SabreWebDavBase\PDO
      */
     public function getShareResourceUri($mycol):string
     {
-        $coucou = "titi";
+        $path = $mycol->getPath();
 
-        return $coucou;
+        $collections = CollectionsQuery::create()
+            ->findOneByOwnerpath($path);
+
+        if (!is_null($collections)) {
+            $collectionsInstances = CollectionsinstancesQuery::create()
+                ->findOneByCollectionsId($collections->getId());
+            
+            
+            if (!is_null($collectionsInstances))
+            {
+                return '/ns/share/collection/' . $collections->getId();
+            } else {// there is no more shared file or folder
+                $collections->delete();
+            }
+        }
+
+        return "None";        
     }
 
     /**
@@ -55,6 +120,32 @@ class WebDavPDO extends SabreWebDavBase\PDO
      */
     public function updateInvites($mycol, array $sharees)
     {
+        $ownerPrinpals = $mycol->getOwner();
+        $login = explode("/", $ownerPrinpals)[1];
+        
+        $user = UserQuery::create()            
+                ->findOneByUserName($login);
+            
+        $ownerPaths = $mycol->getPath();
+        $personId  = $user->getPerson()->getId();
+        $ownerNameCollection = basename($mycol->getPath());
+
+        // we create the root file or directory
+        $collections = CollectionsQuery::create()
+            ->filterByOwnerid($personId)
+            ->filterByOwnerpath($ownerPaths)
+            ->findOneByPrincipaluri($ownerPrinpals);
+
+        if (is_null($collections)) {
+            $collections = new Collections();
+            
+            $collections->setOwnerid($personId);
+            $collections->setOwnerpath($ownerPaths);            
+            $collections->setPrincipaluri($ownerPrinpals);
+
+            $collections->save();
+        }   
+
         foreach($sharees as $sharee) {
             $res = explode(':', $sharee->href);
             if (count($res) != 2) {
@@ -62,101 +153,73 @@ class WebDavPDO extends SabreWebDavBase\PDO
             }
             $email = $res[1];
 
-            $user = UserQuery::create()
+            $guestUser = UserQuery::create()
                 ->usePersonQuery()
                     ->filterByWorkEmail($email)
                     ->_or()
                     ->filterByEmail($email)
                 ->endUse()
-                ->findOne();
+                ->findOne();    
+                
+            $guestPath = $guestUser->getUserDir() . "/". $ownerNameCollection;
+            
+            $guestPrincipals = "principals/".$guestUser->getUserName();
+            $access = $sharee->access;
 
-            if (is_null($user)) {
+            $collectionsInstance = CollectionsinstancesQuery::create()
+                ->filterByCollectionsId($collections->getId())
+                ->findOneByPrincipaluri($guestPrincipals);
+
+
+            if ($sharee->access === \Sabre\DAV\Sharing\Plugin::ACCESS_NOACCESS) {
+                // if access was set no NOACCESS, it means access for an
+                // existing sharee was removed.
+                if (!is_null($collectionsInstance)){
+                    // we have to purge the link
+                    $guestPath = SystemURLs::getDocumentRoot()."/".$guestPath;
+
+                    if (is_link($guestPath)) {
+                        unlink($guestPath);
+                    }
+                    $collectionsInstance->delete();
+                }                        
                 continue;
             }
 
-            $username = $user->getUserName();
+            if (is_null($sharee->principal)) {
+                // If the server could not determine the principal automatically,
+                // we will mark the invite status as invalid.
+                $sharee->inviteStatus = \Sabre\DAV\Sharing\Plugin::INVITE_INVALID;
+            } else {
+                // Because sabre/dav does not yet have an invitation system,
+                // every invite is automatically accepted for now.
+                $sharee->inviteStatus = \Sabre\DAV\Sharing\Plugin::INVITE_ACCEPTED;
+            }
 
-            $removeStmt = $this->pdo->prepare("DELETE FROM " . $this->collectionsTableName . " WHERE ownerPath = ? AND ownerPath = ? AND access IN (2,3)");
-            $updateStmt = $this->pdo->prepare("UPDATE " . $this->collectionsTableName . " SET access = ?, ownerPath = ?, share_href = ? WHERE ownerPath = ? AND ownerId = ?");
+            $ownerPaths = SystemURLs::getDocumentRoot()."/".$ownerPaths;
 
-            $insertStmt = $this->pdo->prepare('
-                INSERT INTO ' . $this->collectionsTableName . '
-                    (
-                        uri,
-                        email,
-                        ownerId,
-                        ownerPath,
-                        access,
-                        share_invitestatus
-                    )
-                    SELECT
-                        ?,
-                        ?,
-                        ?,
-                        displayname,
-                        grpid,
-                        cal_type,
-                        ?,
-                        description,
-                        calendarorder,
-                        calendarcolor,
-                        timezone,
-                        1,
-                        ?,
-                        ?,
-                        ?
-                    FROM ' . $this->collectionsTableName . ' WHERE id = ?');
+            if (is_null($collectionsInstance)) {
+                $collectionsInstance = new Collectionsinstances();
+                
+                $collectionsInstance->setCollectionsId($collections->getId());
+                $collectionsInstance->setPrincipaluri($guestPrincipals);
+                $collectionsInstance->setShareInvitestatus($sharee->inviteStatus);
+                $collectionsInstance->setGuestpath($guestPath);
+                $collectionsInstance->setGuestid($guestUser->getPerson()->getId());
+                $collectionsInstance->setUri(MiscUtils::gen_uuid());
+                $collectionsInstance->setAccess($access);            
 
-            foreach ($sharees as $sharee) {
+                $guestPath = SystemURLs::getDocumentRoot()."/".$guestPath;
 
-                if ($sharee->access === \Sabre\DAV\Sharing\Plugin::ACCESS_NOACCESS) {
-                    // if access was set no NOACCESS, it means access for an
-                    // existing sharee was removed.
-                    $removeStmt->execute([$calendarId, $sharee->href]);
-                    continue;
+                if (is_link($guestPath)) {
+                    unlink($guestPath);
                 }
+                                           
+                symlink($ownerPaths , $guestPath);
 
-                if (is_null($sharee->principal)) {
-                    // If the server could not determine the principal automatically,
-                    // we will mark the invite status as invalid.
-                    $sharee->inviteStatus = \Sabre\DAV\Sharing\Plugin::INVITE_INVALID;
-                } else {
-                    // Because sabre/dav does not yet have an invitation system,
-                    // every invite is automatically accepted for now.
-                    $sharee->inviteStatus = \Sabre\DAV\Sharing\Plugin::INVITE_ACCEPTED;
-                }
-
-                foreach ($currentInvites as $oldSharee) {
-
-                    if ($oldSharee->href === $sharee->href or $oldSharee->principal === $sharee->principal) {
-                        // This is an update
-                        $sharee->properties = array_merge(
-                            $oldSharee->properties,
-                            $sharee->properties
-                        );
-                        $updateStmt->execute([
-                            $sharee->access,
-                            isset($sharee->properties['{DAV:}displayname']) ? $sharee->properties['{DAV:}displayname'] : null,
-                            $sharee->inviteStatus ?: $oldSharee->inviteStatus,
-                            $sharee->href,
-                            $calendarId,
-                            $sharee->principal
-                        ]);
-                        continue 2;
-                    }
-
-                }
-                // If we got here, it means it was a new sharee
-                $insertStmt->execute([
-                    $calendarId,
-                    $sharee->principal,
-                    $sharee->access,
-                    \Sabre\DAV\UUIDUtil::getUUID(),
-                    $sharee->href,
-                    isset($sharee->properties['{DAV:}displayname']) ? $sharee->properties['{DAV:}displayname'] : null,
-                    $sharee->inviteStatus ?: \Sabre\DAV\Sharing\Plugin::INVITE_NORESPONSE,
-                    $instanceId
-                ]);
+                $collectionsInstance->save();
+            } else {
+                // we update the $collectionsInstance
 
             }
         }
@@ -180,44 +243,39 @@ class WebDavPDO extends SabreWebDavBase\PDO
      */
     public function getInvites($mycol)
     {
-        return [new Sharee([
-            'href' => 'admin',
-            'access' => \Sabre\DAV\Sharing\Plugin::ACCESS_NOACCESS,
-        ])];
+        $ownerPah = $mycol->getPath();
 
-        if (!is_array($calendarId)) {
-            throw new \InvalidArgumentException('The value passed to getInvites() is expected to be an array with a calendarId and an instanceId');
-        }
-        list($calendarId, $instanceId) = $calendarId;
-
-        $query = <<<SQL
-SELECT
-    principaluri,
-    access,
-    share_href,
-    share_displayname,
-    share_invitestatus
-FROM {$this->collectionsTableName}
-WHERE
-    calendarid = ?
-SQL;
-
-        $stmt = $this->pdo->prepare($query);
-        $stmt->execute([$calendarId]);
+        $collections = CollectionsQuery::create()
+            ->findOneByOwnerpath($ownerPah);
 
         $result = [];
-        while ($row = $stmt->fetch(\PDO::FETCH_ASSOC)) {
-            $result[] = new Sharee([
-                'href' => isset($row['share_href']) ? $row['share_href'] : \Sabre\HTTP\encodePath($row['principaluri']),
-                'access' => (int) $row['access'],
-                /// Everyone is always immediately accepted, for now.
-                'inviteStatus' => (int) $row['share_invitestatus'],
-                'properties' => !empty($row['share_displayname'])
-                    ? ['{DAV:}displayname' => $row['share_displayname']]
-                    : [],
-                'principal' => $row['principaluri'],
-            ]);
+
+        if (!is_null($collections)) {
+            $collectionsInstances = CollectionsinstancesQuery::create()
+                ->findByCollectionsId($collections->getId());
+
+            
+            foreach ($collectionsInstances as $collectionsInstance) {
+                $ret = explode("/", $collectionsInstance->getPrincipaluri());
+                
+                if (count($ret) < 2) continue;
+                
+                $username = $ret[1];
+                $user = UserQuery::create()->findOneByUserName($username);
+
+                if (is_null($user)) continue;
+                
+                $result[] = new Sharee([
+                    'href' => "mailto:".$user->getPerson()->getEmail(),
+                    'access' => $collectionsInstance->getAccess(),
+                    /// Everyone is always immediately accepted, for now.
+                    'inviteStatus' => (int) $collectionsInstance->getShareInvitestatus(),
+                    'properties' => ['{DAV:}displayname' => $user->getPerson()->getFullName()],
+                    'principal' => $collectionsInstance->getPrincipaluri(),
+                ]);
+            }
         }
+
 
         return $result;
     }
