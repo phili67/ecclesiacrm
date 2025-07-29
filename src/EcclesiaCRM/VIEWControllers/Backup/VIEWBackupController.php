e29d3d41af804fa1432e53f5df768f8afc89e9d4
Philippe Logel <philippelogel@free.fr>
2025-07-29 09:12:01 +0000

    Views are rewritten too

--- src/EcclesiaCRM/VIEWControllers/Backup/VIEWBackupController.php
+++ src/EcclesiaCRM/VIEWControllers/Backup/VIEWBackupController.php
@@ -2,11 +2,12 @@
 
 //
 //  This code is under copyright not under MIT Licence
-//  copyright   : 2022 Philippe Logel all right reserved not MIT licence
+//  copyright   : 2021 Philippe Logel all right reserved not MIT licence
 //                This code can't be included in another software
 //
-//  Updated : 2022/01/05
+//  Updated : 2025/07/28
 //
+
 
 namespace EcclesiaCRM\VIEWControllers;
 
@@ -40,13 +41,44 @@ class VIEWBackupController {
     }
 
     public function argumentsBackupArray ()
-    {
-        $paramsArguments = [ 'sRootPath'   => SystemURLs::getRootPath(),
+    {        
+        $BackupDone = $RemoteBackup = $Backup_In_Progress = false;
+        $message = "";
+        $Backup_Result_Datas = [];
+
+        $backup_result_url = SystemURLs::getDocumentRoot().'/tmp_attach/backup_result.json';
+        
+        if (file_exists(SystemURLs::getDocumentRoot().'/tmp_attach/backup_result.json')) {
+            $BackupDone = true;
+            $message = _("Backup Complete, Ready for Download.");
+            $content = file_get_contents($backup_result_url);
+            $Backup_Result_Datas =  json_decode($content,true); 
+            
+        }        
+
+        if (file_exists(SystemURLs::getDocumentRoot().'/tmp_attach/backup_in_progress.txt')) {
+            $Backup_In_Progress = true;
+            $message = _("Background backup In progress");
+        }
+
+        if (SystemConfig::getBooleanValue('bEnableExternalBackupTarget') && SystemConfig::getValue('sExternalBackupAutoInterval') > 0) {
+            $RemoteBackup = true;
+            
+            $message = _("Backup Generated and copied to remote server");
+        }
+
+        $paramsArguments = [ 
+            'sRootPath'   => SystemURLs::getRootPath(),
             'sRootDocument' => SystemURLs::getDocumentRoot(),
-            'sPageTitle'  => _('Backup Database'),
+            'sPageTitle'  => _('Backup Database')."/CRM",
             'hasGZIP' => SystemConfig::getBooleanValue('bGZIP'),
             'hasZIP' => SystemConfig::getBooleanValue('bZIP'),
-            'encryptionMethod' => SystemConfig::getValue('sPGP')
+            'encryptionMethod' => SystemConfig::getValue('sPGP'),
+            'BackupDone' => $BackupDone,
+            'Backup_In_Progress' => $Backup_In_Progress,
+            'RemoteBackup' => $RemoteBackup,
+            'message' => $message,
+            'Backup_Result_Datas' => $Backup_Result_Datas
         ];
 
         return $paramsArguments;
