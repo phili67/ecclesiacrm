faca0135ed0829ede709978705aa8e5980bf2597
Philippe Logel <philippelogel@free.fr>
2025-07-30 15:27:56 +0000

    Include/slim/settings.php : deleted

--- src/api/index.php
+++ src/api/index.php
@@ -34,17 +34,6 @@ AppFactory::setContainer($container);
 
 $app = AppFactory::create();
 
-/**
- * Add Error Middleware
- *
- * @param bool                  $displayErrorDetails -> Should be set to false in production
- * @param bool                  $logErrors -> Parameter is passed to the default ErrorHandler
- * @param bool                  $logErrorDetails -> Display error details in error log
- * @param LoggerInterface|null  $logger -> Optional PSR-3 Logger  
- *
- * Note: This middleware should be added last. It will not handle any exceptions/errors
- * for middleware added after it.
- */
 if (SystemConfig::getValue('sLogLevel') == 0) {
     $errorMiddleware = $app->addErrorMiddleware(false, false, false);
 } else {
