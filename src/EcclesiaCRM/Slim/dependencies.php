904a93f75397e331b89cd96ac14df9bdd7f6f451
Philippe Logel <philippelogel@free.fr>
2025-08-02 18:52:05 +0000

    Dependencies : with session

--- src/EcclesiaCRM/Slim/dependencies.php
+++ src/EcclesiaCRM/Slim/dependencies.php
@@ -20,49 +20,82 @@ class dependencies
     static public function install(\DI\Container $container, BOOL $loggeronly = false): void
     {
         $container->set('Logger', function () {
-            return LoggerUtils::getAppLogger();
+            if (!isset($_SESSION['Logger'])) {
+                    $_SESSION['Logger'] = LoggerUtils::getAppLogger();
+            }
+            return $_SESSION['Logger'];
         });
 
         // DIC configuration
         if ($loggeronly == false) {
             $container->set('MailChimpService', function () {
-                return new MailChimpService();
+                if (!isset($_SESSION['MailChimpService'])) {
+                    $_SESSION['MailChimpService'] = new MailChimpService();
+                }
+                return $_SESSION['MailChimpService'];
             });
 
             $container->set('PersonService', function () {
-                return new PersonService();
+                if (!isset($_SESSION['PersonService'])) {
+                    $_SESSION['PersonService'] = new PersonService();
+                }
+                return $_SESSION['PersonService'];
             });
 
             $container->set('GroupService', function () {
-                return new GroupService();
+                if (!isset($_SESSION['GroupService'])) {
+                    $_SESSION['GroupService'] = new GroupService();
+                }
+                return $_SESSION['GroupService'];                
             });
 
             $container->set('FinancialService', function () {
-                return new FinancialService();
+                if (!isset($_SESSION['FinancialService'])) {
+                    $_SESSION['FinancialService'] = new GroupService();
+                }
+                return $_SESSION['FinancialService'];                   
             });
 
             $container->set('ReportingService', function () {
-                return new ReportingService();
+                if (!isset($_SESSION['ReportingService'])) {
+                    $_SESSION['ReportingService'] = new ReportingService();
+                }
+                return $_SESSION['ReportingService'];
             });
 
             $container->set('SystemService', function () {
-                return new SystemService();
+                if (!isset($_SESSION['SystemService'])) {
+                    $_SESSION['SystemService'] = new SystemService();
+                }
+                return $_SESSION['SystemService'];                
             });
             
             $container->set('CalendarService', function () {
-                return new CalendarService();
+                if (!isset($_SESSION['CalendarService'])) {
+                    $_SESSION['CalendarService'] = new CalendarService();
+                }
+                return $_SESSION['CalendarService'];                
             });
 
             $container->set('SundaySchoolService', function () {
-                return new SundaySchoolService();
+                if (!isset($_SESSION['SundaySchoolService'])) {
+                    $_SESSION['SundaySchoolService'] = new SundaySchoolService();
+                }
+                return $_SESSION['SundaySchoolService'];                
             });
 
             $container->set('CacheProvider', function () {
-                return new CacheProvider();
+                if (!isset($_SESSION['CacheProvider'])) {
+                    $_SESSION['CacheProvider'] = new CacheProvider();
+                }
+                return $_SESSION['CacheProvider'];
             });
 
             $container->set('PastoralCareService', function () {
-                return new PastoralCareService();
+                if (!isset($_SESSION['PastoralCareService'])) {
+                    $_SESSION['PastoralCareService'] = new PastoralCareService();
+                }
+                return $_SESSION['PastoralCareService'];                   
             });
         }
     }
