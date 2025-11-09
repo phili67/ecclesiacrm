c991dfe6c0cb64729db2ac05708fa6f2cdeafddb
Philippe Logel <philippelogel@free.fr>
2025-11-09 22:10:38 +0000

    comments

--- src/api/routes/sidebar/sidebar-systemsettings.php
+++ src/api/routes/sidebar/sidebar-systemsettings.php
@@ -8,7 +8,8 @@ $app->group('/systemsettings', function (RouteCollectorProxy $group) {
 
     /*
      * @! save system settings
-     * 
+     * #! param: ref->array  :: new_value
+     * #! param: ref->array  :: type
      */
     $group->post('/saveSettings', SidebarSystemSettingsController::class . ':saveSettings' );
 
