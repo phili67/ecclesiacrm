dba7bc94f2e6cfe857f83b8a834e5eea0e0170c4
Philippe Logel <philippelogel@free.fr>
2025-11-18 15:32:49 +0000

    the php code for hierarchical

--- src/v2/templates/volunteer/volunteerview.php
+++ src/v2/templates/volunteer/volunteerview.php
@@ -8,7 +8,20 @@
  ******************************************************************************/
 
 require $sRootDocument . '/Include/Header.php';
+
+use EcclesiaCRM\Service\VolunteerService;
+
+$hier = VolunteerService::getHirearchicalView($volID, $volID);
+
 ?>
+<div class="clt">
+    <ul>
+        <li>
+            <?= $hier ?>
+        </li>
+    </ul>
+</div>
+
 
 <?php if ( $isVolunteerOpportunityEnabled ) {// only an admin can modify the options
 ?>
@@ -19,10 +32,17 @@ require $sRootDocument . '/Include/Header.php';
     <div class="alert alert-warning"><i class="fas fa-exclamation-triangle" aria-hidden="true"></i>   <?= _('Only an admin can modify or delete this records.') ?></div>
 <?php
 }
+
+
 ?>
 <div class="card card-body">
-  <table class="table table-striped table-bordered" id="VolunteerOpportunityTable" cellpadding="5" cellspacing="0"  width="100%"></table>
+  <table class="table table-striped table-bordered" id="VolunteerOpportunityTableMembers" cellpadding="5" cellspacing="0"  width="100%"></table>
 </div>
+
+<script nonce="<?= $CSPNonce ?>">
+    window.CRM.volID = <?= $volID ?>;
+    var isShowable = true;
+</script>
 
 <script type="module" src="<?= $sRootPath ?>/skin/js/volunteer/VolunteerOpportunityView.js" ></script>
 
