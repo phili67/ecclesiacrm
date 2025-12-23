8c80282c68abe0a7f0e23c6b4ebb1f986c8026e6
Philippe Logel <philippelogel@free.fr>
2025-12-23 17:52:49 +0000

    src/EcclesiaCRM/Service/VolunteerService.php : manager detection

--- src/EcclesiaCRM/Service/VolunteerService.php
+++ src/EcclesiaCRM/Service/VolunteerService.php
@@ -11,30 +11,34 @@ namespace EcclesiaCRM\Service;
 
 use EcclesiaCRM\VolunteerOpportunityQuery;
 use EcclesiaCRM\dto\SystemURLs;
+use EcclesiaCRM\PersonVolunteerOpportunityQuery;
 
 class VolunteerService
 {
-    static private function renderHierarchicalView($voldId, $orig_selected_VolId) : string {
+    static private function renderHierarchicalView($voldId, $orig_selected_VolId): string
+    {
         $res = "";
 
         $opportunity = VolunteerOpportunityQuery::create()->findOneById($voldId);
-        $res .= '<a '. (($opportunity->getId() == $orig_selected_VolId)?'style="color:red"':'') .' href="'. SystemURLs::getRootPath() .'/v2/volunteeropportunity/'. $opportunity->getId() . '/view">'.$opportunity->getName().'</a>';
-        
+        $res .= '<a ' . (($opportunity->getId() == $orig_selected_VolId) ? 'style="color:red"' : '') . ' href="' . SystemURLs::getRootPath() . '/v2/volunteeropportunity/' . $opportunity->getId() . '/view">' . $opportunity->getName() . '</a>';
+
         $opportunities = VolunteerOpportunityQuery::create()->findByParentId($voldId);
-        
+
         if ($opportunities->count() > 0) {
             $res .= "<ul>";
             foreach ($opportunities as $opportunity) {
-                $res .= "<li>";   
-                $res .= self::renderHierarchicalView($opportunity->getId(), $orig_selected_VolId);            
-                $res .= "</li>";   
-            }        
+                $res .= "<li>";
+                $res .= self::renderHierarchicalView($opportunity->getId(), $orig_selected_VolId);
+                $res .= "</li>";
+            }
             $res .= "</ul>";
         }
 
         return $res;
     }
-    static public function getHirearchicalView($voId, $orig_selected_VolId) : string {
+
+    static public function getHirearchicalView($voId, $orig_selected_VolId): string
+    {
 
         $res = "";
         $opportunity = VolunteerOpportunityQuery::create()->findOneById($voId);
@@ -43,6 +47,54 @@ class VolunteerService
             $res = self::renderHierarchicalView($opportunity->getId(), $orig_selected_VolId);
         } else {
             $res = self::getHirearchicalView($opportunity->getParentId(), $orig_selected_VolId);
+        }
+
+        return $res;
+    }
+
+    static private function loopHierarchicalManager($personId, $voldId, $orig_selected_VolId): string
+    {
+        $res = False;
+
+        $opportunities = VolunteerOpportunityQuery::create()->findByParentId($voldId);
+
+        foreach ($opportunities as $opportunity) {  
+            $person = PersonVolunteerOpportunityQuery::create()
+                ->filterByVolunteerOpportunityId($opportunity->getId())
+                ->findOneByPersonId($personId);
+
+            if (!is_null($person) and $opportunity->isManagers()) {
+                return True;
+            }
+
+            $res = self::loopHierarchicalManager($personId, $opportunity->getId(), $orig_selected_VolId);
+
+            if ($res == True) {
+                return $res;
+            }
+        }
+
+        return $res;
+    }
+
+    static public function getHirearchicalManager($personId, $voId, $orig_selected_VolId): Bool
+    {
+        $opportunity = VolunteerOpportunityQuery::create()->findOneById($voId);
+
+        $person = PersonVolunteerOpportunityQuery::create()
+                ->filterByVolunteerOpportunityId($voId)
+                ->findOneByPersonId($personId);
+
+        if (!is_null($person) and $opportunity->isManagers()) {
+            return True;
+        }
+
+        $res = False;
+
+        if ($opportunity->getParentId() == null) {
+            $res = self::loopHierarchicalManager($personId, $opportunity->getId(), $orig_selected_VolId);
+        } else {
+            $res = self::getHirearchicalManager($personId, $opportunity->getParentId(), $orig_selected_VolId);
         }
 
         return $res;
