effb96de05f4e382f8b99cd1a4ab5d6b61c6a261
Philippe Logel <philippelogel@free.fr>
2026-01-04 18:02:37 +0000

    Menulink + fundlist gui update

--- src/v2/templates/sidebar/menulinklist.php
+++ src/v2/templates/sidebar/menulinklist.php
@@ -1,4 +1,5 @@
 <?php
+
 /*******************************************************************************
  *
  *  filename    : MenuLinksList.php
@@ -12,28 +13,19 @@
 use EcclesiaCRM\dto\SystemURLs;
 
 require $sRootDocument . '/Include/Header.php';
-
-if ( $addCustomLink ) {
-?>
-    <p align="center"><button class="btn btn-primary" id="add-new-menu-links"><?= _("Add Custom Menu Link") ?></button></p>
-<?php
-} else {
-?>
-    <div class="alert alert-warning"><i class="fas fa-exclamation-triangle" aria-hidden="true"></i>   <?= _('Only an admin can modify or delete this records.') ?></div>
-<?php
-}
 ?>
 
 <div class="card card-body">
-
-<table class="table table-striped table-bordered" id="menulinksTable" cellpadding="5" cellspacing="0"  width="100%"></table>
-
+  <?php if ($addCustomLink) { ?>
+    <p align="center"><button class="btn btn-primary" id="add-new-menu-links"><?= _("Add Custom Menu Link") ?></button></p>
+  <?php } ?>
+  <table class="table table-hover dt-responsive dataTable no-footer dtr-inline" id="menulinksTable" cellpadding="5" cellspacing="0" width="100%"></table>
 </div>
 
 <script nonce="<?= SystemURLs::getCSPNonce() ?>">
-  window.CRM.personId  = <?= $personId ?>;
+  window.CRM.personId = <?= $personId ?>;
 </script>
 
-<script type="module" src="<?= $sRootPath ?>/skin/js/sidebar/MenuLinksList.js" ></script>
+<script type="module" src="<?= $sRootPath ?>/skin/js/sidebar/MenuLinksList.js"></script>
 
 <?php require $sRootDocument . '/Include/Footer.php'; ?>
