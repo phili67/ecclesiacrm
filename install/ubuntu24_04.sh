c20b21e75bfd06dd265d416e3afb78623a37155d
EcclesiaCRM <philippe.logel@gmail.com>
2026-03-22 14:08:28 +0100

    Update ubuntu24_04.sh

--- install/ubuntu24_04.sh
+++ install/ubuntu24_04.sh
@@ -65,6 +65,49 @@ sudo apt install certbot python3-certbot-apache
 sudo a2enmod ssl
 sudo a2enmod rewrite
 
+
+# create Apache vhost for HTTP (always)
+VHOST_Default_CONF="/etc/apache2/sites-available/000-default.conf "
+sudo tee "$VHOST_Default_CONF" > /dev/null <<EOF    
+<VirtualHost *:80>
+    # The ServerName directive sets the request scheme, hostname and port that
+    # the server uses to identify itself. This is used when creating
+    # redirection URLs. In the context of virtual hosts, the ServerName
+    # specifies what hostname must appear in the request's Host: header to
+    # match this virtual host. For the default virtual host (this file) this
+    # value is not decisive as it is used as a last resort host regardless.
+    # However, you must set it for any further virtual host explicitly.
+    #ServerName www.example.com
+
+    ServerAdmin webmaster@localhost
+    DocumentRoot /var/www/html
+
+    # Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+    # error, crit, alert, emerg.
+    # It is also possible to configure the loglevel for particular
+    # modules, e.g.
+    #LogLevel info ssl:warn
+
+    ErrorLog ${APACHE_LOG_DIR}/error.log
+    CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+    # For most configuration files from conf-available/, which are
+    # enabled or disabled at a global level, it is possible to
+    # include a line for only one particular virtual host. For example the
+    # following line enables the CGI configuration for this host only
+    # after it has been globally disabled with "a2disconf".
+    #Include conf-available/serve-cgi-bin.conf
+
+    <Directory /var/www/html>
+        Options Indexes FollowSymLinks
+        AllowOverride All
+        Require all granted
+    </Directory>
+
+    RewriteEngine on
+EOF
+
+
 read -p "Activate HTTPS and Let's Encrypt ? (y/N) [N]: " ENABLE_HTTPS
 ENABLE_HTTPS=${ENABLE_HTTPS:-N}
 ENABLE_HTTPS=$(echo "$ENABLE_HTTPS" | tr '[:upper:]' '[:lower:]')
