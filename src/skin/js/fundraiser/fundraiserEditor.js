a408c215c60d8624b14823ba3be47607699ecdc9
Philippe Logel <philippelogel@free.fr>
2026-04-08 13:11:52 +0000

    update: src/skin/js/fundraiser/fundraiserEditor.js

--- src/skin/js/fundraiser/fundraiserEditor.js
+++ src/skin/js/fundraiser/fundraiserEditor.js
@@ -1,28 +1,58 @@
-$(function() {
-    $('body').on('click',".deleteDonatedItem", function () {
+$(function () {
+    var locale = window.CRM.locale.replace('_', '-');
+    var $countBadge = $('#donatedItemsCountBadge');
+
+    function formatAmount(value) {
+        return Number(value || 0).toLocaleString(locale);
+    }
+
+    function cleanPictureUrl(value) {
+        if (!value) {
+            return '';
+        }
+
+        var data = String(value).trim();
+        if (data.charAt(0) === "'") {
+            data = data.substring(1);
+        }
+        if (data.charAt(data.length - 1) === "'") {
+            data = data.substring(0, data.length - 1);
+        }
+
+        return data;
+    }
+
+    function updateCountBadge() {
+        if ($countBadge.length && window.CRM.donatedItemsTable) {
+            $countBadge.text(window.CRM.donatedItemsTable.rows().count());
+        }
+    }
+
+    $('body').on('click', '.deleteDonatedItem', function (event) {
+        event.preventDefault();
         var donatedItem = $(this).data('donatedid');
 
         bootbox.confirm({
-            message: i18next.t ("You're about to delete the item !!!"),
+            message: i18next.t("You're about to delete the item !!!"),
             buttons: {
                 confirm: {
-                    label: '<i class="fas fa-times"></i> ' + i18next.t ('Yes'),
+                    label: '<i class="fas fa-times"></i> ' + i18next.t('Yes'),
                     className: 'btn-danger'
                 },
                 cancel: {
-                    label: '<i class="fas fa-check"></i> ' + i18next.t ('No'),
+                    label: '<i class="fas fa-check"></i> ' + i18next.t('No'),
                     className: 'btn-primary'
                 }
             },
             callback: function (result) {
                 if (result) {
                     window.CRM.APIRequest({
-                        method: "DELETE",
-                        path: "fundraiser/donateditem",
-                        data: JSON.stringify({"DonatedItemID":donatedItem,"FundRaiserID": window.CRM.fundraiserID})
-                    },function (data) {
-                        if (data.status == "success") {
-                            window.CRM.donatedItemsTable.ajax.reload();
+                        method: 'DELETE',
+                        path: 'fundraiser/donateditem',
+                        data: JSON.stringify({ DonatedItemID: donatedItem, FundRaiserID: window.CRM.fundraiserID })
+                    }, function (data) {
+                        if (data.status === 'success') {
+                            window.CRM.donatedItemsTable.ajax.reload(updateCountBadge);
                         }
                     });
                 }
@@ -30,98 +60,100 @@ $(function() {
         });
     });
 
-    var  dataTableConfig = {
+    var dataTableConfig = {
         ajax: {
-            url: window.CRM.root + "/api/fundraiser/" + window.CRM.fundraiserID,
+            url: window.CRM.root + '/api/fundraiser/' + window.CRM.fundraiserID,
             type: 'POST',
-            contentType: "application/json",
-            dataSrc: "DonatedItems",
-            "beforeSend": function (xhr) {
-                xhr.setRequestHeader('Authorization',
-                    "Bearer " +  window.CRM.jwtToken
-                );
+            contentType: 'application/json',
+            dataSrc: 'DonatedItems',
+            beforeSend: function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + window.CRM.jwtToken);
             }
         },
-        "language": {
-            "url": window.CRM.plugin.dataTable.language.url
+        language: {
+            url: window.CRM.plugin.dataTable.language.url
         },
-        "searching": false,
+        searching: false,
+        lengthChange: false,
+        pageLength: 10,
+        autoWidth: false,
+        dom: 'rtip',
         columns: [
             {
                 width: 'auto',
                 title: i18next.t('Item'),
                 data: 'di_Item',
-                render: function (data, type, full, meta) {
-                    return '<a href="' + window.CRM.root + '/v2/fundraiser/donatedItemEditor/' + full.di_ID + '/' +  window.CRM.fundraiserID +'"><i class="fas fa-pencil-alt" aria-hidden="true"></i>&nbsp;' + data + '</a>';
+                className: 'align-middle',
+                render: function (data, type, full) {
+                    return '<a class="font-weight-semibold" href="' + window.CRM.root + '/v2/fundraiser/donatedItemEditor/' + full.di_ID + '/' + window.CRM.fundraiserID + '"><i class="fas fa-pencil-alt mr-1" aria-hidden="true"></i>' + data + '</a>';
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Preview'),
                 data: 'di_picture',
-                render: function (data, type, full, meta) {
-                    let l = data.length;
-                    if (data[l-1] == "'") data = data.substring(0, l-1);                        
-                    if (data[0] == "'") data = data.substring(1);                    
-                    if (data.length > 4)
-                        return '<img src="' + data +'" width="80">';
-                    else
-                        return '';
+                className: 'align-middle text-center',
+                orderable: false,
+                render: function (data) {
+                    var imageUrl = cleanPictureUrl(data);
+                    if (imageUrl.length > 4) {
+                        return '<img src="' + imageUrl + '" width="64" class="img-fluid rounded" alt="preview">';
+                    }
+                    return '';
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Multiple'),
                 data: 'di_multibuy',
-                render: function (data, type, full, meta) {
-                    return (data == "1")?'x':'';
+                className: 'align-middle text-center',
+                render: function (data) {
+                    return data === '1' ? '<span class="badge badge-info">x</span>' : '';
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Donor'),
                 data: 'donorFirstName',
-                render: function (data, type, full, meta) {
-                    res = '';
-                    if (data != null)
-                        res += data;
-                    if (full.donorLastName != null)
-                        res += ' ' + full.donorLastName;
-
-                    return res;
+                className: 'align-middle',
+                render: function (data, type, full) {
+                    var result = '';
+                    if (data != null) {
+                        result += data;
+                    }
+                    if (full.donorLastName != null) {
+                        result += (result ? ' ' : '') + full.donorLastName;
+                    }
+                    return result;
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Buyer'),
                 data: 'di_multibuy',
-                render: function (data, type, full, meta) {
-                    if (data == "1") {
+                className: 'align-middle',
+                render: function (data, type, full) {
+                    if (data === '1') {
                         return i18next.t('Multiple');
                     }
 
-                    res = '';
-                    if (full.buyerFirstName != null)
-                        res += full.buyerFirstName;
-                    if (full.buyerLastName != null)
-                        res += ' ' + full.buyerLastName;
+                    var result = '';
+                    if (full.buyerFirstName != null) {
+                        result += full.buyerFirstName;
+                    }
+                    if (full.buyerLastName != null) {
+                        result += (result ? ' ' : '') + full.buyerLastName;
+                    }
 
-                    return res;
+                    return result;
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Title'),
                 data: 'di_title',
-                render: function (data, type, full, meta) {
-                    return data;
-                }
-            },
-            {
-                width: 'auto',
-                title: i18next.t('Title'),
-                data: 'di_title',
-                render: function (data, type, full, meta) {
+                className: 'align-middle',
+                render: function (data) {
                     return data;
                 }
             },
@@ -129,50 +161,60 @@ $(function() {
                 width: 'auto',
                 title: i18next.t('Sale Price'),
                 data: 'di_sellprice',
-                render: function (data, type, full, meta) {
-                    return Number(data).toLocaleString(window.CRM.locale.replace("_","-"));
+                className: 'align-middle text-right',
+                render: function (data) {
+                    return formatAmount(data);
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Estimated value'),
                 data: 'di_estprice',
-                render: function (data, type, full, meta) {
-                    return Number(data).toLocaleString(window.CRM.locale.replace("_","-"));
+                className: 'align-middle text-right',
+                render: function (data) {
+                    return formatAmount(data);
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Material Value'),
                 data: 'di_materialvalue',
-                render: function (data, type, full, meta) {
-                    return Number(data).toLocaleString(window.CRM.locale.replace("_","-"));
+                className: 'align-middle text-right',
+                render: function (data) {
+                    return formatAmount(data);
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Minimum Price'),
                 data: 'di_minimum',
-                render: function (data, type, full, meta) {
-                    return Number(data).toLocaleString(window.CRM.locale.replace("_","-"));
+                className: 'align-middle text-right',
+                render: function (data) {
+                    return formatAmount(data);
                 }
             },
             {
                 width: 'auto',
                 title: i18next.t('Delete'),
                 data: 'di_ID',
-                render: function (data, type, full, meta) {
-                    return '<a href="#" class="deleteDonatedItem" data-donatedid="' + data + '"><i class="far fa-trash-alt" aria-hidden="true" style="color:red" data-donatedid="' + data + '"></i>';
+                className: 'align-middle text-center',
+                orderable: false,
+                render: function (data) {
+                    return '<a href="#" class="btn btn-xs btn-outline-danger deleteDonatedItem" data-donatedid="' + data + '"><i class="far fa-trash-alt" aria-hidden="true"></i></a>';
                 }
             }
         ],
         responsive: true,
-        createdRow: function (row, data, index) {
-            $(row).addClass("paymentRow");
+        createdRow: function (row) {
+            $(row).addClass('paymentRow');
+        },
+        drawCallback: function () {
+            updateCountBadge();
         }
     };
 
-    $.extend(dataTableConfig,window.CRM.plugin.dataTable);
+    $.extend(dataTableConfig, window.CRM.plugin.dataTable);
 
-    window.CRM.donatedItemsTable = $("#fundraiser-table").DataTable(dataTableConfig);
+    window.CRM.donatedItemsTable = $('#fundraiser-table').DataTable(dataTableConfig);
+    updateCountBadge();
 });
