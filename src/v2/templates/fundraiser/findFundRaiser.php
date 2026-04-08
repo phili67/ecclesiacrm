23b8966ef184c0e8678f1540b30cd10d235badc6
Philippe Logel <philippelogel@free.fr>
2026-04-08 13:11:54 +0000

    update: src/v2/templates/fundraiser/findFundRaiser.php

--- src/v2/templates/fundraiser/findFundRaiser.php
+++ src/v2/templates/fundraiser/findFundRaiser.php
@@ -13,65 +13,70 @@ use EcclesiaCRM\dto\SystemURLs;
 
 require $sRootDocument . '/Include/Header.php';
 ?>
-<div class="card">
-    <div class="card-header border-1">
-        <h3 class="card-title"><?= _('Filters') ?></h3>
+<div class="card card-outline card-primary shadow-sm">
+    <div class="card-header py-2 d-flex justify-content-between align-items-center">
+        <h3 class="card-title mb-0"><i class="fas fa-filter mr-1"></i><?= _('Filters') ?></h3>
+        <span class="badge badge-light border"><?= _('Fundraiser search') ?></span>
     </div>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-lg-2">
-                <?= _('Number') ?>:
+    <div class="card-body py-3">
+        <div class="form-row align-items-center mb-2">
+            <div class="col-lg-2 col-md-3 font-weight-semibold mb-1 mb-md-0">
+                <?= _('Number') ?>
             </div>
-            <div class="col-lg-2">
+            <div class="col-lg-3 col-md-4">
                 <input type="text" name="ID" id="ID" value="" class="form-control form-control-sm">
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-lg-2">
-                <?= _('Date Start') ?>:
+        <div class="form-row align-items-center mb-2">
+            <div class="col-lg-2 col-md-3 font-weight-semibold mb-1 mb-md-0">
+                <?= _('Date Start') ?>
             </div>
-            <div class="col-lg-2">
+            <div class="col-lg-3 col-md-4">
                 <input type="text" name="DateStart" maxlength="10" id="DateStart" size="11"
                        value=""
                        class="date-picker form-control form-control-sm"
                        placeholder="<?= SystemConfig::getValue("sDatePickerPlaceHolder") ?>">
             </div>
-            <div class="col-lg-1">
-            </div>
-            <div class="col-lg-3">
-                <input type="submit" class="btn btn-primary btn-sm" id="submitFilter" value="<?= _('Apply Filters') ?>"
-                       name="FindFundRaiserSubmit">
+            <div class="col-lg-7 col-md-5 d-none d-md-block">
+                <small class="text-muted"><?= _('Use a date range to narrow down fundraiser records.') ?></small>
             </div>
         </div>
-        <div class="row">
-            <div class="col-lg-2">
-                <?= _('Date End') ?>:
+
+        <div class="form-row align-items-center mb-0">
+            <div class="col-lg-2 col-md-3 font-weight-semibold mb-1 mb-md-0">
+                <?= _('Date End') ?>
             </div>
-            <div class="col-lg-2">
+            <div class="col-lg-3 col-md-4">
                 <input type="text" name="DateEnd" maxlength="10" id="DateEnd" size="11"
                        value=""
                        class="date-picker form-control form-control-sm"
                        placeholder="<?= SystemConfig::getValue("sDatePickerPlaceHolder") ?>">
             </div>
-            <div class="col-lg-1">
-            </div>
-            <div class="col-lg-3">
-                <input type="submit" class="btn btn-danger btn-sm" id="clearFiltersSubmit" value="<?= _('Clear Filters') ?>"
-                       name="FilterClear">
-            </div>
+        </div>
+
+        <div class="pt-2 mt-3 border-top d-flex flex-wrap align-items-center">
+            <button type="button" class="btn btn-sm btn-primary mr-2 mb-1" id="submitFilter" name="FindFundRaiserSubmit">
+                <i class="fas fa-search mr-1"></i><?= _('Apply Filters') ?>
+            </button>
+            <button type="button" class="btn btn-sm btn-outline-secondary mb-1" id="clearFiltersSubmit" name="FilterClear">
+                <i class="fas fa-eraser mr-1"></i><?= _('Clear Filters') ?>
+            </button>
+            <small class="text-muted ml-md-3 ml-0 mb-1"><?= _('Tip: press Enter in a field to apply filters.') ?></small>
         </div>
     </div>
 </div>
 
-<div class="card">
-    <div class="card-header border-1">
-        <h3 class="card-title"><?= _('Fundraisers') ?></h3>
+<div class="card card-outline card-secondary shadow-sm">
+    <div class="card-header py-2 d-flex justify-content-between align-items-center">
+        <h3 class="card-title mb-0"><i class="fas fa-hand-holding-usd mr-1"></i><?= _('Fundraisers') ?></h3>
+        <span class="badge badge-light border" id="fundraiserCountBadge">0</span>
     </div>
-    <div class="card-body">
-        <br>
-        <table cellpadding='4' align='center' cellspacing='0' width='100%' id='fundraiser-listing-table'
-               class="table table-striped table-bordered dataTable no-footer dtr-inline"></table>
+    <div class="card-body py-2">
+        <div class="table-responsive">
+            <table cellpadding='4' align='center' cellspacing='0' width='100%' id='fundraiser-listing-table'
+                   class="table table-striped table-hover table-bordered mb-0 dataTable no-footer dtr-inline"></table>
+        </div>
     </div>
 </div>
 
