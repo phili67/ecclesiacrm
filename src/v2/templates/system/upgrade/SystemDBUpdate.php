32c218959b85b69b4153016032916d4454e4828b
Philippe Logel <philippelogel@free.fr>
2026-04-08 13:11:56 +0000

    update: src/v2/templates/system/upgrade/SystemDBUpdate.php

--- src/v2/templates/system/upgrade/SystemDBUpdate.php
+++ src/v2/templates/system/upgrade/SystemDBUpdate.php
@@ -5,50 +5,62 @@ $bSuppressSessionTests = true; // DO NOT MOVE
 // Set the page title and include HTML header
 require $sRootDocument . '/Include/HeaderNotLoggedIn.php'; ?>
 
-<p></br></p>
-
-<div class="error-page">
-    <div class="row">
-        <div class="col-3"><h1 class="headline text-green" style="font-size:60px">426</h1></div>
-        <div class="col-6">
-            <div class="error-content">
-                <div class="row">
-                    <h3><i class="fas fa-exclamation-triangle text-green"></i> <?= _('Upgrade Required') ?></h3>
-                    <p>
-                        <?= _("Current DB Version") . ": " . $dbVersion ?> <br/>
-                        <?= _("Current Software Version") . ": " . $InstalledVersion ?> <br/>
-                    </p>
-                    <h5><?= _("Update and clean up the database and files") ?></h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-3"></div>
-    </div>
-    <?php if (empty($errorMessage)) {
-    ?>
+<section class="content pt-4">
+    <div class="container-fluid px-0">
         <div class="row">
             <div class="col-12">
-                <p></br></p>                
-                <form action="<?= $sRootPath ?>/v2/system/database/update/1" method="post">
-                    <input type="hidden" name="upgrade" value="true"/>
-                    <button type="submit" class="btn btn-primary btn-block btn-lg">
-                        <i class="fas fa-database"></i> <i class="fas fa-file"></i> <i class="fas fa-folder"></i> 
-                        <?= _("Complete the update") ?>
-                    </button>
-                </form>
+
+                <div class="card card-outline card-warning shadow-sm">
+                    <div class="card-header d-flex align-items-center justify-content-between">
+                        <h3 class="card-title mb-0">
+                            <i class="fas fa-tools text-warning mr-2"></i><?= _('Upgrade Required') ?>
+                        </h3>
+                        <span class="badge badge-warning">426</span>
+                    </div>
+                    <div class="card-body">
+
+                        <div class="alert alert-light border mb-4">
+                            <div class="row">
+                                <div class="col-md-6 mb-2 mb-md-0">
+                                    <div class="text-muted small"><?= _('Current DB Version') ?></div>
+                                    <div class="font-weight-bold"><?= $dbVersion ?></div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="text-muted small"><?= _('Current Software Version') ?></div>
+                                    <div class="font-weight-bold"><?= $InstalledVersion ?></div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <p class="mb-4 text-secondary">
+                            <?= _('Update and clean up the database and files') ?>
+                        </p>
+
+                        <?php if (empty($errorMessage)) { ?>
+                            <form action="<?= $sRootPath ?>/v2/system/database/update/1" method="post">
+                                <input type="hidden" name="upgrade" value="true"/>
+                                <button type="submit" class="btn btn-primary btn-lg btn-block">
+                                    <i class="fas fa-database mr-1"></i>
+                                    <i class="fas fa-file mr-1"></i>
+                                    <i class="fas fa-folder mr-2"></i>
+                                    <?= _('Complete the update') ?>
+                                </button>
+                            </form>
+                        <?php } else { ?>
+                            <div class="main-box-body clearfix" id="globalMessage">
+                                <div class="alert alert-danger mb-0" id="globalMessageAlert">
+                                    <i class="fas fa-exclamation-triangle fa-lg mr-2"></i>
+                                    <span id="globalMessageText"><?= $errorMessage ?></span>
+                                </div>
+                            </div>
+                        <?php } ?>
+                    </div>
+                </div>
+
             </div>
         </div>
-    <?php
-} else {
-        ?>
-        <div class="main-box-body clearfix" id="globalMessage">
-            <div class="alert alert-danger" id="globalMessageAlert">
-                <i class="fas fa-exclamation-triangle  fa-lg"></i> <?= $errorMessage ?>
-            </div>
-        </div>
-    <?php
-    } ?>
-</div>
+    </div>
+</section>
 
 
 <?php require $sRootDocument . '/Include/FooterNotLoggedIn.php'; ?>
