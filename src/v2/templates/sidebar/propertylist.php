10a5f0f260fa6582babc431846f3e473743a185b
Philippe Logel <philippelogel@free.fr>
2026-04-13 09:43:30 +0000

    update: src/v2/templates/sidebar/propertylist.php

--- src/v2/templates/sidebar/propertylist.php
+++ src/v2/templates/sidebar/propertylist.php
@@ -12,17 +12,26 @@
 //Include the function library
 require $sRootDocument . '/Include/Header.php'; ?>
 
-<div class="card">
-  <div class="card-header border-1 d-flex justify-content-between align-items-center">
-    <h3 class="card-title"><i class="fas fa-person mr-2" aria-hidden="true"></i> <?= _('Properties') ?></h3>
-    <?php if ($isMenuOption) { ?>
-    <a href="#" class="btn btn-success btn-lg shadow-sm font-weight-bold py-2 px-4 ml-auto" id="add-new-prop">
-        <i class="fas fa-plus-circle mr-2"></i> <?= _('Add a New') ?> <?= $sTypeName?> <?= _('Property') ?>
-    </a>  
-    <?php } ?>
-  </div>
-  <div class="card-body">    
-    <table class='table table-hover dt-responsive dataTable no-footer dtr-inline' id="property-listing-table-v2" width="100%"></table>
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="card card-outline card-primary shadow-sm rounded-4">
+      <div class="card-header border-1 d-flex justify-content-between align-items-center">
+        <h3 class="card-title mb-0">
+          <i class="fas fa-list-alt text-primary me-2"></i> <?= _('Properties') ?>
+        </h3>
+        <?php if ($isMenuOption) { ?>
+        <a href="#" class="btn btn-success btn-lg shadow-sm font-weight-bold py-2 px-4 ml-auto" id="add-new-prop">
+          <i class="fas fa-plus-circle me-2"></i> <?= _('Add a New') ?> <?= $sTypeName?> <?= _('Property') ?>
+        </a>
+        <?php } ?>
+      </div>
+      <div class="card-body">
+        <div class="table-responsive">
+          <table class="table table-striped table-bordered data-table dataTable no-footer dtr-inline" id="property-listing-table-v2" style="width:100%"></table>
+        </div>
+      </div>
+    </div>
   </div>
 </div>
 
