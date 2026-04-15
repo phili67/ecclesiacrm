90a627308200f1cf70716a56b1b9ff794e062746
Philippe Logel <philippelogel@free.fr>
2026-04-15 19:37:50 +0000

    script + sql update

---
+++ src/mysql/upgrade/8.1.0-upgrade.sql
@@ -0,0 +1,3 @@
+UPDATE `userconfig_ucfg` SET `ucfg_value` = 'light' WHERE `userconfig_ucfg`.`ucfg_name` = 'sStyleBrandLinkColor';
+UPDATE `userconfig_ucfg` SET `ucfg_value` = 'light' WHERE `userconfig_ucfg`.`ucfg_name` = 'sStyleSideBar';
+UPDATE `userconfig_ucfg` SET `ucfg_value` = 'light' WHERE `userconfig_ucfg`.`ucfg_name` = 'sStyleNavBarColor';
