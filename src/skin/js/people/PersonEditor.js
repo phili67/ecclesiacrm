585bf5e0b3b828bc44033fd582ec007218769e77
Philippe Logel <philippelogel@free.fr>
2026-04-18 16:58:07 +0000

    update: src/skin/js/people/PersonEditor.js

--- src/skin/js/people/PersonEditor.js
+++ src/skin/js/people/PersonEditor.js
@@ -1,6 +1,87 @@
 $(function() {
   $("[data-mask]").inputmask();
 
+  var familyValueInput = $("#familySelectionValue");
+  var familyPicker = $("#existingFamilyPicker");
+  var familySelect = $("#optionFamily");
+
+  function showPersonAddress() {
+    familyPicker.hide();
+    $("#familyAddress").fadeOut(50);
+    $("#personAddress").fadeIn(1000);
+  }
+
+  function prepareNewFamily() {
+    familyPicker.hide();
+    $('#famcountry-input').val(window.CRM.sEntityCountry).trigger('change');
+    $('#famstate-input').val('').trigger('change');
+    $('#FamName').val('');
+    $('#FamAddress1').val('');
+    $('#FamAddress2').val('');
+    $('#FamCity').val('');
+    $('#FamZip').val('');
+    $('#FamStateTextbox').val('');
+
+    $("#familyAddress").fadeIn(1000);
+    $("#personAddress").fadeOut(50);
+    $('#FamName').focus();
+  }
+
+  function loadExistingFamily(famID) {
+    if (!(famID > 0)) {
+      return;
+    }
+
+    familyPicker.show();
+
+    window.CRM.APIRequest({
+       method: 'POST',
+       path: 'families/info',
+       data: JSON.stringify({"familyId": famID})
+    }, function(data) {
+       $('#famcountry-input').val(data.Country).trigger('change');
+       $('#famstate-input').val(data.State).trigger('change');
+       $('#FamName').val(data.Name);
+       $('#FamAddress1').val(data.Address1);
+       $('#FamAddress2').val(data.Address2);
+       $('#FamCity').val(data.City);
+       $('#FamZip').val(data.Zip);
+       $('#FamStateTextbox').val(data.State);
+    });
+
+    $("#familyAddress").fadeIn(1000);
+    $("#personAddress").fadeOut(50);
+  }
+
+  function applyFamilyMode(mode) {
+    if (mode === 'existing') {
+      familyPicker.show();
+
+      if (!(parseInt(familySelect.val(), 10) > 0)) {
+        var firstFamilyOption = familySelect.find('option:first').val();
+        if (firstFamilyOption) {
+          familySelect.val(firstFamilyOption);
+        }
+      }
+
+      familyValueInput.val(familySelect.val() || '0');
+      if (parseInt(familyValueInput.val(), 10) > 0) {
+        loadExistingFamily(parseInt(familyValueInput.val(), 10));
+      }
+
+      return;
+    }
+
+    if (mode === 'new') {
+      familyValueInput.val('-1');
+      prepareNewFamily();
+      return;
+    }
+
+    familyValueInput.val('0');
+    showPersonAddress();
+  }
+
   // we hide by default the familyAddress
   if (!window.CRM.bShowAddress) {
     $("#familyAddress").hide();
@@ -8,65 +89,28 @@ $(function() {
     $("#personAddress").hide();
   }
 
-  // This scroll the family at the right place
-  var selectedItem = $("#optionFamily option:selected").val();
+  $('input[name="familyAssignmentMode"]').on('change', function() {
+    applyFamilyMode($(this).val());
+  });
 
-  $('#optionFamily').val(1).on('change');
-  $('#optionFamily').val(selectedItem).on('change');
-
-
-  $('#optionFamily').on('change',function(data) {
-    var famID = $(this).val();
+  familySelect.on('change', function() {
+    var famID = parseInt($(this).val(), 10) || 0;
+    familyValueInput.val(famID);
 
     if (famID > 0) {
-      window.CRM.APIRequest({
-         method: 'POST',
-         path: 'families/info',
-         data: JSON.stringify({"familyId":famID})
-      },function(data) {
-         $('#famcountry-input').val(data.Country).trigger('change');
-         $('#famstate-input').val(data.State).trigger('change');
-         $('#FamName').val(data.Name);
-         $('#FamAddress1').val(data.Address1);
-         $('#FamAddress2').val(data.Address2);
-         $('#FamCity').val(data.City);
-         $('#FamZip').val(data.Zip);
-         $('#FamStateTextbox').val(data.State);
-      })
-
-      $('#optionFamily').attr('size', '8');
-      $("#familyAddress").fadeIn(1000);
-      $("#personAddress").fadeOut(50);
-
-    } else {
-      if (this.value == -1) {// we create a new family
-        // we fields are blank
-        $('#famcountry-input').val(window.CRM.sEntityCountry).trigger('change');
-        $('#famstate-input').val('').trigger('change');
-        $('#FamName').val('');
-        $('#FamAddress1').val('');
-        $('#FamAddress2').val('');
-        $('#FamCity').val('');
-        $('#FamZip').val('');
-        $('#FamStateTextbox').val('');
-
-        // next the fields will appear
-        $('#optionFamily').attr('size', '8');
-        $("#familyAddress").fadeIn(1000);
-        $("#personAddress").fadeOut(50);
-      } else {
-        $('#optionFamily').attr('size', '2');
-        $("#familyAddress").fadeOut(50);
-        $("#personAddress").fadeIn(1000);
-      }
-
-      $('#FamName').focus();
+      loadExistingFamily(famID);
     }
   });
+
+  applyFamilyMode($('input[name="familyAssignmentMode"]:checked').val());
 
   $("#famcountry-input").select2();
   $("#famstate-input").select2();
   $("#country-input").select2();
   $("#state-input").select2();
+  familySelect.select2({
+    width: '100%',
+    placeholder: window.i18next ? i18next.t('Search for a family or address') : 'Search for a family or address'
+  });
 
 });
