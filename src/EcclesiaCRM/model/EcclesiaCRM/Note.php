45ab1610bec1d0f7d5d16f426e3a26164dc50258
Philippe Logel <philippelogel@free.fr>
2026-04-29 20:38:19 +0000

    Note.php  NoteShare.php : update

--- src/EcclesiaCRM/model/EcclesiaCRM/Note.php
+++ src/EcclesiaCRM/model/EcclesiaCRM/Note.php
@@ -4,7 +4,7 @@ namespace EcclesiaCRM;
 
 use EcclesiaCRM\Base\Note as BaseNote;
 use EcclesiaCRM\NoteShareQuery;
-use EcclesiaCRM\dto\SystemURLs;
+use EcclesiaCRM\dto\SystemConfig;
 
 /**
  * Skeleton subclass for representing a row from the 'note_nte' table.
@@ -22,6 +22,29 @@ class Note extends BaseNote
         $this->setDateEntered(new \DateTime());
         $this->setEnteredBy($enteredBy);
     }
+
+    public function currentEditbyUserName() : ?string
+    {       
+        if ($this->getCurrentEditedBy() > 0) {
+            $currentDate = new \DateTime();
+            $since_start = $currentDate->diff($this->getCurrentEditedDate());
+            $min = ($since_start->days * 24 * 60) + ($since_start->h * 60) + $since_start->i;
+            $timeLeft = SystemConfig::getValue('iDocumentTimeLeft') - $min;
+
+            if ($timeLeft > 0) {
+                $editor = PersonQuery::create()->findPk($this->getCurrentEditedBy());
+                if ($editor) {
+                    return _( "This document is opened by" ) . " : " . $editor->getFullName() . " (" . $timeLeft . " " . _( "Minutes left" ) . ")";
+                }
+            } else {
+                // reset the count
+                $this->setCurrentEditedDate(null);
+                $this->setCurrentEditedBy(0);
+                $this->save();
+            }
+        }
+        return "";
+    }
     
     public function isShared ()
     {
@@ -30,25 +53,44 @@ class Note extends BaseNote
 
     public function getEditLink()
     {
-        $url = '<a href="#" data-id="' . $this->getId() . '" data-perid="';
-
-        if ($this->getPerId() != '') {
-            $url .= $this->getPerId().'" data-famid="0" class="editDocument btn btn-primary btn-sm"';
-        } else {
-            $url .= '0" data-famid="' . $this->getFamId() . '" class="editDocument btn btn-primary btn-sm"';
+        if ($this->currentEditbyUserName() != "") {
+            return '<button class="btn btn-outline-info btn-sm" disabled><i class="fas fa-lock"></i></button>';
         }
 
-        $url .= 'data-toggle="tooltip" data-placement="bottom" title="'. _("Edit this document") .'">';
+        $noteId = htmlspecialchars($this->getId(), ENT_QUOTES, 'UTF-8');
+        $perId = ($this->getPerId() != '') ? htmlspecialchars($this->getPerId(), ENT_QUOTES, 'UTF-8') : '0';
+        $famId = ($this->getPerId() != '') ? '0' : htmlspecialchars($this->getFamId(), ENT_QUOTES, 'UTF-8');
+        $title = htmlspecialchars(_("Edit this document"), ENT_QUOTES, 'UTF-8');
+
+        $url = sprintf(
+            '<button data-id="%s" data-perid="%s" data-famid="%s" class="editDocument btn btn-outline-primary btn-sm" data-toggle="tooltip" data-placement="bottom" title="%s"><i class="fas fa-edit"></i></button>',
+            $noteId,
+            $perId,
+            $famId,
+            $title
+        );
 
         return $url;
     }
 
     public function getDeleteLink()
     {
-        return '<a href="#" data-id="' . $this->getId() . '" data-perid="' . $this->getPerId() .'" 
-            data-famid="' . $this->getFamId() . '" 
-            data-toggle="tooltip" data-placement="bottom" title="'. _("Delete this document") .'"
-            class="btn btn-danger btn-sm deleteDocument">';
+        if ($this->currentEditbyUserName() != "") {
+            return '<button class="btn btn-outline-danger btn-sm deleteDocument" disabled><i class="fas fa-lock"></i></button>';
+        }
+
+        $noteId = htmlspecialchars($this->getId(), ENT_QUOTES, 'UTF-8');
+        $perId = htmlspecialchars($this->getPerId(), ENT_QUOTES, 'UTF-8');
+        $famId = htmlspecialchars($this->getFamId(), ENT_QUOTES, 'UTF-8');
+        $title = htmlspecialchars(_("Delete this document"), ENT_QUOTES, 'UTF-8');
+
+        return sprintf(
+            '<button data-id="%s" data-perid="%s" data-famid="%s" data-toggle="tooltip" data-placement="bottom" title="%s" class="btn btn-outline-danger btn-sm deleteDocument"><i class="fas fa-trash-alt"></i></button>',
+            $noteId,
+            $perId,
+            $famId,
+            $title
+        );
     }
 
     public function getDisplayEditedDate($format = 'Y-m-d H:i:s')// you have to set the time to 0-23, if not all the time are set to AM.
